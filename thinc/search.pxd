49e56b48fa193cc62f022f16d1a50e08a2775fbc
Matthew Honnibal <honnibal@gmail.com>
2014-11-14 00:51:30 +1100

    * Rewrite beam class, while doing NER parsing stuff.

--- thinc/search.pxd
+++ thinc/search.pxd
@@ -14,25 +14,47 @@ ctypedef pair[weight_t, Candidate] Entry
 ctypedef priority_queue[Entry] Queue
 
 
+ctypedef int (*trans_func_t)(void* dest, void* src, class_t clas, void* x) except -1
+
+ctypedef int (*init_func_t)(Pool mem, void* state, void* extra_args) except -1
+
+ctypedef int (*finish_func_t)(void* state, void* extra_args) except -1
+
+cdef struct _State:
+    void* content
+    class_t* hist
+    weight_t score
+    int loss
+    int i
+    int t
+    bint is_done
+
+
 cdef class Beam:
     cdef Pool mem
     cdef class_t nr_class
     cdef class_t width
     cdef class_t size
     cdef Queue q
-    cdef void** parents
-    cdef void** states
+    cdef _State* _parents
+    cdef _State* _states
 
-    cdef int fill(self, weight_t** scores) except -1
-    cpdef pair[size_t, size_t] pop(self) except *
+    cdef int _fill(self, weight_t** scores, bint** is_valid) except -1
+
+    cdef void* at(self, int i)
+    cdef int initialize(self, init_func_t init_func, int n, void* extra_args) except -1
+    cdef int advance(self, weight_t** scores, bint** is_valid, int** costs,
+                     trans_func_t transition_func, void* extra_args) except -1
+    cdef int check_done(self, finish_func_t finish_func, void* extra_args) except -1
+ 
 
 
 cdef class MaxViolation:
+    cdef Pool mem
     cdef int cost
     cdef weight_t delta
     cdef class_t n
-    cdef void* pred
-    cdef void* gold
+    cdef readonly list p_hist
+    cdef readonly list g_hist
 
-    cdef weight_t check(self, int cost, weight_t p_score, weight_t g_score,
-                         void* p, void* g, class_t n) except -1
+    cpdef int check(self, Beam pred, Beam gold) except -1
--- thinc/search.pyx
+++ thinc/search.pyx
@@ -2,33 +2,68 @@ from cymem.cymem cimport Pool
 
 
 cdef class Beam:
-    def __init__(self, class_t nr_class, class_t width, class_t state_size=0):
+    def __init__(self, class_t nr_class, class_t width):
         self.nr_class = nr_class
         self.width = width
         self.mem = Pool()
-        self.parents = <void**>self.mem.alloc(self.width, sizeof(void*))
-        self.states = <void**>self.mem.alloc(self.width, sizeof(void*))
+        self._parents = <_State*>self.mem.alloc(self.width, sizeof(_State))
+        self._states = <_State*>self.mem.alloc(self.width, sizeof(_State))
         self.size = 1
-        cdef class_t i
-        if state_size != 0:
-            for i in range(self.width):
-                self.parents[i] = self.mem.alloc(1, state_size)
 
     property score:
         def __get__(self):
-            return self.q.top().first
+            return self.state[0].score
+    
+    cdef void* at(self, int i):
+        return self._states[i].content
 
-    def fill_from_list(self, list scores):
-        mem = Pool()
-        c_scores = <weight_t**>mem.alloc(len(scores), sizeof(weight_t*))
-        for i, clas_scores in enumerate(scores):
-            c_scores[i] = <weight_t*>mem.alloc(len(clas_scores), sizeof(weight_t))
-            for j, score in enumerate(clas_scores):
-                c_scores[i][j] = score
-        self.fill(c_scores)
-        return self.q.top().first
+    cdef int initialize(self, init_func_t init_func, int n, void* extra_args) except -1:
+        for i in range(self.width):
+            init_func(self.mem, self._states[i].content, extra_args)
 
-    cdef int fill(self, weight_t** scores) except -1:
+    cdef int advance(self, weight_t** scores, bint** is_valid, int** costs,
+                     trans_func_t transition_func, void* extra_args) except -1:
+        cdef _State* parent
+        cdef _State* state
+        self._fill(scores, is_valid)
+        # For a beam of width k, we only ever need 2k state objects. How?
+        # Each transition takes a parent and a class and produces a new state.
+        # So, we don't need the whole history --- just the parent. So at
+        # each step, we take a parent, and apply one or more extensions to
+        # it.
+        self._parents, self._states = self._states, self._parents
+        cdef int i = 0
+        while i < self.width:
+            score, (p_i, clas) = self.q.top()
+            self.q.pop()
+            # Indicates terminal state reached; i.e. state is done
+            if clas == 0:
+                # Now parent will not be changed, so we don't have to copy.
+                assert self._parents[p_i].is_done
+                self._states[i] = self._parents[p_i]
+                self._states[i].score = score
+                continue
+            parent = &self._parents[p_i]
+            state = &self._states[i]
+            # The supplied transition function should adjust the destination
+            # state to be the result of applying the class to the source state
+            transition_func(state.content, parent.content, clas, extra_args)
+            state.score += scores[i][clas]
+            state.loss += costs[i][clas]
+            state.hist[state.t] = clas
+            state.t += 1
+            i += 1
+        self.size = i
+
+    cdef int check_done(self, finish_func_t finish_func, void* extra_args) except -1:
+        cdef int i
+        self.is_done = True
+        for i in range(self.size):
+            self._states[i].is_done = finish_func(&self._states[i].content, extra_args)
+            if not self._states[i].is_done:
+                self.is_done = False
+
+    cdef int _fill(self, weight_t** scores, bint** is_valid) except -1:
         """Populate the queue from a k * n matrix of scores, where k is the
         beam-width, and n is the number of classes.
         """
@@ -37,21 +72,17 @@ cdef class Beam:
         cdef weight_t score
         while not self.q.empty():
             self.q.pop()
+        cdef _State* s
         for i in range(self.width):
+            s = &self._states[i]
+            if s.is_done:
+                # Update score by path average, following TACL '13 paper.
+                entry = Entry(s.score + (s.score / s.t), Candidate(i, 0))
+                continue
             for j in range(self.nr_class):
-                entry = Entry(scores[i][j], Candidate(i, j))
-                self.q.push(entry)
-
-    cpdef pair[size_t, size_t] pop(self) except *:
-        """Pop the current top candidate from the beam, returning the parent
-        and class.
-        """
-        if self.q.empty():
-            raise StopIteration
-        cdef weight_t score
-        score, (parent, clas) = self.q.top()
-        self.q.pop()
-        return pair[size_t, size_t](parent, clas)
+                if is_valid[i][j]:
+                    entry = Entry(scores[i][j], Candidate(i, j))
+                    self.q.push(entry)
 
 
 cdef class MaxViolation:
@@ -59,18 +90,15 @@ cdef class MaxViolation:
         self.delta = -1
         self.n = 0
         self.cost = 0
-        self.pred = NULL
-        self.gold = NULL
+        self.p_hist = []
+        self.g_hist = []
 
-    cdef weight_t check(self, int cost, weight_t p_score, weight_t g_score,
-                         void* p, void* g, class_t n) except -1:
-        cdef weight_t d = (p_score + 1) - g_score
-        if cost >= 1 and d > self.delta:
-            self.cost = cost
+    cpdef int check(self, Beam pred, Beam gold) except -1:
+        cdef _State* p = &pred._states[0]
+        cdef _State* g = &gold._states[0]
+        cdef weight_t d = (p.score + 1) - g.score
+        if p.loss and d > self.delta:
+            self.loss = p.loss
             self.delta = d
-            self.pred = p
-            self.gold = g
-            self.n = n
-            return d
-        else:
-            return 0
+            self.p_hist = [p.hist[i] for i in range(p.t)]
+            self.g_hist = [g.hist[i] for i in range(g.t)]
