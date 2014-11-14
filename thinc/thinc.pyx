193a5ccd35f9e2d39f04cf34f5012051630f6d4d
Matthew Honnibal <honnibal@gmail.com>
2014-11-14 17:58:34 +1100

    * Tmp work on integration class

--- thinc/thinc.pyx
+++ thinc/thinc.pyx
@@ -12,17 +12,16 @@ cdef class Brain:
         self.mem = Pool()
         self._extr = Extractor(templates, [ConjFeat] * len(templates))
         self._model = LinearModel(n_classes)
-        if load_from is not None:
-            assert path.exists(load_from)
+        if load_from is not None and path.exists(load_from):
             self._model.load(path.join(load_from))
         self.n_classes = self._model.nr_class
         self.n_atoms = n_atoms
         self.feats = <feat_t*>self.mem.alloc(self._extr.n, sizeof(feat_t))
         self.values = <weight_t*>self.mem.alloc(self._extr.n, sizeof(weight_t))
-        self.scores = <weight_t*>self.mem.alloc(self.model.nr_class, sizeof(weight_t))
+        self.scores = <weight_t*>self.mem.alloc(self._model.nr_class, sizeof(weight_t))
 
-        self._is_valid = <bint*>self.mem.alloc(self.model.nr_class, sizeof(bint))
-        self._is_gold = <bint*>self.mem.alloc(self.model.nr_class, sizeof(bint))
+        self._is_valid = <bint*>self.mem.alloc(self._model.nr_class, sizeof(bint))
+        self._is_gold = <bint*>self.mem.alloc(self._model.nr_class, sizeof(bint))
 
     cdef void score(self, weight_t* scores, atom_t* atoms):
         self._extr.extract(self.feats, self.values, atoms, NULL)
@@ -49,6 +48,8 @@ cdef class Brain:
         return best_i + 1
 
     cdef tuple learn(self, atom_t* atoms, list valid_classes, list gold_classes):
+        assert valid_classes
+        assert gold_classes
         self.score(self.scores, atoms)
         cdef int best_p = -1
         cdef int best_g = -1
@@ -57,20 +58,27 @@ cdef class Brain:
         memset(self._is_valid, False, sizeof(bint) * self.n_classes)
         cdef int clas
         for clas in valid_classes:
-            self._is_valid[clas] = True
+            self._is_valid[clas-1] = True
         memset(self._is_gold, False, sizeof(bint) * self.n_classes)
         for clas in gold_classes:
-            self._is_gold[clas] = True
+            self._is_gold[clas-1] = True
         cdef weight_t* scores = self.scores
         for i in range(self.n_classes):
+            # Classes offset by 1, to reserve 0 as a missing value
             if self._is_valid[i] and scores[i] > p_score:
-                best_p = i
+                best_p = i + 1
                 p_score = scores[i]
-            if self._is_gold[i] and scores[i] > g_score:
-                best_g = i
+            if self._is_gold[i] and (best_g == -1 or scores[i] > g_score):
+                best_g = i + 1
                 g_score = scores[i]
-        # Classes offset by 1, to reserve 0 as a missing value
-        return (best_p + 1, best_g + 1)
+        if best_p == best_g:
+            upd = {}
+        else:
+            upd = {best_g: {}, best_p: {}}
+            self._extr.count(upd[best_g], self.feats, 1.0)
+            self._extr.count(upd[best_p], self.feats, -1.0)
+        self._model.update(upd)
+        return (best_p, best_g)
 
     property feats:
         def __get__(self):
