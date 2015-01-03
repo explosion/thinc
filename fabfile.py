0045b012c23edee2c11b8e68b38c2e5735f218fb
Matthew Honnibal <honnibal@gmail.com>
2015-01-03 14:27:22 +1100

    * Use fabfile to claw back some packaging sanity

--- fabfile.py
+++ fabfile.py
@@ -7,14 +7,28 @@ PWD = path.dirname(__file__)
 VENV_DIR = path.join(PWD, '.env')
 
 
+def sdist():
+    local('rm dist/*')
+    with virtualenv(VENV_DIR):
+        local('python setup.py sdist')
+
+
+def install():
+    local('virtualenv .env')
+    with virtualenv(VENV_DIR):
+        local('pip install --upgrade setuptools')
+        local('pip install dist/*.tar.gz')
+        local('pip install pytest')
+
+
 def make():
     with virtualenv(VENV_DIR):
         with lcd(path.dirname(__file__)):
-            local('python setup.py build_ext --inplace')
+            local('python dev_setup.py build_ext --inplace')
 
 def clean():
     with lcd(path.dirname(__file__)):
-        local('python setup.py clean --all')
+        local('python dev_setup.py clean --all')
 
 def test():
     with virtualenv(VENV_DIR):
