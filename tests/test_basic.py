a99ad22c72f272bc38b2bfe4925cc216ea85569e
Matthew Honnibal <honnibal@gmail.com>
2015-01-03 20:07:53 +1100

    * Add basic tests

---
+++ tests/test_basic.py
@@ -0,0 +1,12 @@
+from __future__ import division
+
+import StringIO
+import pytest
+
+from thinc.learner import LinearModel
+
+
+def test_basic():
+    model = LinearModel(5, 4)
+    model.update({1: {(1, 1): 1, (3, 3): -5}, 2: {(2, 2): 4, (3, 3): 5}})
+    #scores = model([1, 2, 3])
