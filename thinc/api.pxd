5dfc33366275dba6c7eff52ce2c01fb0b20ee9f2
Matthew Honnibal <honnibal@gmail.com>
2015-06-28 22:36:27 +0200

    * Redo Example class, to allow raw C arrays

--- thinc/api.pxd
+++ thinc/api.pxd
@@ -4,22 +4,49 @@ from .typedefs cimport weight_t, atom_t
 from .features cimport Feature
 
 
+cdef struct ExampleC:
+    int* is_valid
+    int* costs
+    atom_t* atoms
+    Feature* features
+    weight_t* embeddings
+    weight_t* scores
+
+    int nr_class
+    int nr_atom
+    int nr_feat
+    int nr_embed
+    
+    int guess
+    int best
+    int cost
+    weight_t loss
+
+
 cdef class Example:
     cdef Pool mem
-
-    cdef int n_classes
-    cdef int n_atoms
-    cdef int n_features
-
-
+    cdef ExampleC c
     cdef int[:] is_valid
     cdef int[:] costs
-    cdef weight_t[:] scores
-    
     cdef atom_t[:] atoms
     cdef weight_t[:] embeddings
+    cdef weight_t[:] scores
 
-    cdef int guess
-    cdef int best
-    cdef int cost
-    cdef weight_t loss
+    @staticmethod
+    cdef inline ExampleC init(Pool mem, int nr_class, int nr_atom,
+            int nr_feat, int nr_embed) except *:
+        return ExampleC(
+            is_valid = <int*>mem.alloc(nr_class, sizeof(int)),
+            costs = <int*>mem.alloc(nr_class, sizeof(int)),
+            scores = <weight_t*>mem.alloc(nr_class, sizeof(weight_t)),
+            atoms = <atom_t*>mem.alloc(nr_atom, sizeof(atom_t)),
+            features = <Feature*>mem.alloc(nr_feat, sizeof(Feature)),
+            embeddings = <weight_t*>mem.alloc(nr_embed, sizeof(weight_t)),
+            nr_class = nr_class,
+            nr_atom = nr_atom,
+            nr_feat = nr_feat,
+            nr_embed = nr_embed,
+            guess = 0,
+            best = 0,
+            cost = 0,
+            loss = 0)
--- thinc/api.pyx
+++ thinc/api.pyx
@@ -1,32 +1,28 @@
 from libc.string cimport memset
+from cymem.cymem cimport Pool
+
+from .typedefs cimport weight_t, atom_t
 
 
 cdef class Example:
-    def __init__(self, int n_classes, int n_atoms, int n_embeddings):
+    def __init__(self, int nr_class, int nr_atom, int nr_feat, int nr_embed):
         self.mem = Pool()
 
-        self.n_classes = n_classes
-        self.n_atoms = n_atoms
-        self.n_features = n_embeddings
-        
-        self.atoms      = <atom_t[:n_atoms]>self.mem.alloc(n_atoms, sizeof(atom_t))
-        self.is_valid   = <int[:n_classes]>self.mem.alloc(n_classes, sizeof(int))
-        self.costs      = <int[:n_classes]>self.mem.alloc(n_classes, sizeof(int))
-        self.scores     = <weight_t[:n_classes]>self.mem.alloc(n_classes, sizeof(weight_t))
-        self.embeddings = <weight_t[:n_embeddings]>self.mem.alloc(n_embeddings, sizeof(float))
+        self.c = Example.init(self.mem, nr_class, nr_atom, nr_feat, nr_embed)
 
-        self.guess = 0
-        self.best = 0
-        self.cost = 0
-        self.loss = 0
+        self.is_valid = <int[:nr_class]>self.c.is_valid
+        self.costs = <int[:nr_class]>self.c.costs
+        self.atoms = <atom_t[:nr_atom]>self.c.atoms
+        self.embeddings = <weight_t[:nr_embed]>self.c.embeddings
+        self.scores = <weight_t[:nr_class]>self.c.scores
 
     def wipe(self):
         cdef int i
-        for i in range(self.n_classes):
-            self.is_valid[i] = 0
-            self.costs[i] = 0
-            self.scores[i] = 0
-        for i in range(self.n_atoms):
-            self.atoms[i] = 0
-        for i in range(self.n_features):
-            self.embeddings[i] = 0
+        for i in range(self.c.nr_class):
+            self.c.is_valid[i] = 0
+            self.c.costs[i] = 0
+            self.c.scores[i] = 0
+        for i in range(self.c.nr_atom):
+            self.c.atoms[i] = 0
+        for i in range(self.c.nr_feat):
+            self.c.embeddings[i] = 0
