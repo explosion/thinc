7d100ecdfcecba2c5e4834e6d527d6127b707777
Matthew Honnibal <honnibal@gmail.com>
2015-11-06 19:47:06 +1100

    * Update features.pyx

--- thinc/features.pxd
+++ thinc/features.pxd
@@ -1,29 +1,13 @@
 from cymem.cymem cimport Pool
 
-from .typedefs cimport *
-
-DEF MAX_TEMPLATE_LEN = 10
-
-
-cdef struct Template:
-    int[MAX_TEMPLATE_LEN] indices
-    int length
-    atom_t[MAX_TEMPLATE_LEN] atoms
+from .structs cimport TemplateC, FeatureC
+from .typedefs cimport atom_t
 
 
-cdef struct Feature:
-    int i
-    feat_t key
-    weight_t value
-
-
-cdef class Extractor:
+cdef class ConjunctionExtracter:
     cdef Pool mem
-    cdef Template* templates
-    cdef Feature* feats
-    cdef readonly int n_templ
-    cdef Feature* get_feats(self, atom_t* atoms, int* length) nogil
-    cdef int set_feats(self, Feature* feats, atom_t* atoms) nogil
+    cdef TemplateC* templates
+    cdef readonly int nr_templ
+    cdef readonly int nr_atom
 
-
-cdef int count_feats(dict counts, Feature* feats, int n_feats, weight_t inc) except -1
+    cdef int set_features(self, FeatureC* feats, const atom_t* atoms) nogil
--- thinc/features.pyx
+++ thinc/features.pyx
@@ -3,22 +3,24 @@ from cymem.cymem cimport Pool
 
 from murmurhash.mrmr cimport hash64
 
+from .api cimport Example
+
 
 DEF MAX_TEMPLATE_LEN = 10
 
 
-cdef class Extractor:
+cdef class ConjunctionExtracter:
     """Extract composite features from a sequence of atomic values, according to
     the schema specified by a list of templates.
     """
-    def __init__(self, templates):
+    def __init__(self, nr_atom, templates):
         self.mem = Pool()
+        self.nr_atom = nr_atom
         # Value that indicates the value has been "masked", e.g. it was pruned
         # as a rare word. If a feature contains any masked values, it is dropped.
         templates = tuple(sorted(set([tuple(sorted(f)) for f in templates])))
-        self.n_templ = len(templates) + 1
-        self.templates = <Template*>self.mem.alloc(len(templates), sizeof(Template))
-        self.feats = <Feature*>self.mem.alloc(self.n_templ, sizeof(Feature))
+        self.nr_templ = len(templates) + 1
+        self.templates = <TemplateC*>self.mem.alloc(len(templates), sizeof(TemplateC))
         # Sort each feature, and sort and unique the set of them
         cdef int i, j, idx
         for i, indices in enumerate(templates):
@@ -27,21 +29,19 @@ cdef class Extractor:
                 self.templates[i].indices[j] = idx
             self.templates[i].length = len(indices)
 
-    cdef Feature* get_feats(self, atom_t* atoms, int* length) nogil:
-        length[0] = self.set_feats(self.feats, atoms)
-        return self.feats
+    def __call__(self, Example eg):
+        eg.c.nr_feat = self.set_features(eg.c.features, eg.c.atoms)
 
-    cdef int set_feats(self, Feature* feats, atom_t* atoms) nogil:
-        cdef Template* templ
-        cdef Feature* feat
-        feats[0].i = 0
+    cdef int set_features(self, FeatureC* feats, const atom_t* atoms) nogil:
+        cdef TemplateC* templ
+        cdef FeatureC* feat
         feats[0].key = 1
-        feats[0].value = 1
+        feats[0].val = 1
         cdef bint seen_non_zero
         cdef int templ_id
         cdef int n_feats = 1
         cdef int i
-        for templ_id in range(self.n_templ-1):
+        for templ_id in range(self.nr_templ-1):
             templ = &self.templates[templ_id]
             seen_non_zero = False
             for i in range(templ.length):
@@ -49,18 +49,8 @@ cdef class Extractor:
                 seen_non_zero = seen_non_zero or templ.atoms[i]
             if seen_non_zero:
                 feat = &feats[n_feats]
-                feat.i = templ_id
-                feat.key = hash64(templ.atoms, templ.length * sizeof(atom_t), templ_id)
-                feat.value = 1
+                feat.key = hash64(templ.atoms, templ.length * sizeof(templ.atoms[0]),
+                                  templ_id)
+                feat.val = 1
                 n_feats += 1
         return n_feats
-
-
-cdef int count_feats(dict counts, const Feature* feats, int n_feats, weight_t inc) except -1:
-    cdef int i
-    cdef feat_t f
-    for i in range(n_feats):
-        f = feats[i].key
-        key = (feats[i].i, f)
-        counts.setdefault(key, 0)
-        counts[key] += inc
