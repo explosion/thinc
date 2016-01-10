fe56a7464ed87c487c910011c48d2f73baf8e286
Matthew Honnibal <honnibal@gmail.com>
2016-01-10 14:59:35 +0100

    * Refactor tagger

--- examples/tagger.py
+++ examples/tagger.py
@@ -35,9 +35,47 @@ def pad_tokens(tokens):
     tokens.append('ROOT')
 
 
-class Tagger(object):
-    def __init__(self, classes=None, load=False):
+class FeatureExtractor(object):
+    def __init__(self, char_width=5, tag_width=5, word_width=10, chars_per_word=10,
+            word_context=(-2, -1, 0, 1, 2), tag_context=(-2, -1)):
+        self.char_width = char_width
+        self.tag_width = tag_width
+        self.word_width = word_width
+        self.chars_per_word = chars_per_word
+        self.word_context = word_context
+        self.tag_context = tag_context
         self.strings = {}
+        self.tables = (self.char_width, self.word_width, self.tag_width)
+        self.slots = (0,) * chars_per_word + (1,) * len(word_context) + (2,) * len(tag_context)
+
+    @property
+    def input_length(self):
+        return sum(self.tables[slot] for slot in self.slots)
+
+    def __call__(self, i, word, context, prev_tags):
+        i += len(START)
+        features = []
+        word = word.ljust(self.chars_per_word, ' ')
+        # Character features
+        features = [(0, self._intify(c), 1.0) for c in word]
+        for position in self.word_context:
+            features.append((1, self._intify(context[i+position]), 1.0))
+        for position in self.tag_context:
+            features.append((2, self._intify(prev_tags[position]), 1.0))
+        return features
+    
+    def _intify(self, string):
+        if string in self.strings:
+            return self.strings[string]
+        else:
+            i = len(self.strings) + 1
+            self.strings[string] = i
+            return i
+
+
+class Tagger(object):
+    def __init__(self, extractor, classes=None, load=False):
+        self.ex = extractor
         self.tagdict = {}
         if classes:
             self.classes = classes
@@ -46,33 +84,23 @@ class Tagger(object):
         self.model = None
 
     def start_training(self, sentences):
-        suffix_width = 5
-        prefix_width = 5
-        tags_width = 10
-        words_width = 10
-        #tables = (suffix_width, prefix_width, tags_width, words_width)
-        #slots = (0, 1, 2, 2, 3, 3, 0, 3, 3, 0, 3)
-        tables = (words_width,tags_width)
-        slots = (0,0,0,0,0,1,1)
-        input_length = sum(tables[slot] for slot in slots)
         self._make_tagdict(sentences)
+        input_length = self.ex.input_length
         self.model = NeuralNet(
             (input_length, input_length, len(self.classes)),
-            embed=(tables, slots),
-        rho=1e-7, eta=0.001, update_step='adam')
+            embed=(self.ex.tables, self.ex.slots),
+        rho=1e-7, eta=0.01, update_step='adam')
+        print(self.model.widths)
     
     def tag(self, words):
-        prev, prev2 = START
         tags = DefaultList('') 
         context = START + [self._normalize(w) for w in words] + END
         inverted_classes = {i: tag for tag, i in self.classes.items()}
         for i, word in enumerate(words):
-            features = self._get_features(i, word, context, prev, prev2)
+            features = self.ex(i, word, context, tags)
             eg = self.model.predict_sparse(features)
             tag = inverted_classes[eg.guess]
             tags.append(tag)
-            prev2 = prev
-            prev = tag
         return tags
     
     def train(self, sentences, save_loc=None, nr_iter=5):
@@ -86,7 +114,7 @@ class Tagger(object):
         self.end_training(save_loc)
     
     def train_one(self, words, tags):
-        prev, prev2 = START
+        tag_history = DefaultList('') 
         context = START + [self._normalize(w) for w in words] + END
         Xs = []
         ys = []
@@ -95,11 +123,11 @@ class Tagger(object):
         grad_l1 = 0.0
         for i, word in enumerate(words):
             if tags[i] in ('ROOT', '<start>', None):
-                prev2, prev = prev, tags[i]
+                tag_history.append(tags[i])
                 continue
-            features = self._get_features(i, word, context, prev, prev2)
+            features = self.ex(i, word, context, tag_history)
             eg = self.model.train_sparse(features, self.classes[tags[i]])
-            prev2, prev = prev, inverted_classes[eg.guess]
+            tag_history.append(inverted_classes[eg.guess])
             grad_l1 += self.model.l1_gradient
             loss += eg.loss
         return loss, grad_l1
@@ -123,29 +151,6 @@ class Tagger(object):
             return '!DIGITS'
         else:
             return word.lower()
-
-    def _get_features(self, i, word, context, prev, prev2):
-        '''Map tokens into a feature representation, implemented as a
-        {(slot, id): float} dict. If the features change, a new model must be
-        trained.'''
-        def _intify(string):
-            if string in self.strings:
-                return self.strings[string]
-            else:
-                i = len(self.strings) + 1
-                self.strings[string] = i
-                return i
-
-        i += len(START)
-        features = {}
-        features[(0, _intify(word))] = 1
-        features[(1, _intify(context[i-1]))] = 1
-        features[(2, _intify(context[i+1]))] = 1
-        features[(3, _intify(context[i-2]))] = 1
-        features[(4, _intify(context[i+2]))] = 1
-        features[(5, _intify(prev))] = 1 # Previous tag
-        features[(6, _intify(prev2))] = 1 # Prev prev tag
-        return features
 
     def _make_tagdict(self, sentences):
         '''Make a tag dictionary for single-tag words.'''
@@ -192,7 +197,6 @@ def train(tagger, sentences, nr_iter):
         random.shuffle(train_sents)
 
 
-
 def read_conll(loc):
     for sent_str in open(loc).read().strip().split('\n\n'):
         lines = [line.split() for line in sent_str.split('\n')]
@@ -214,7 +218,7 @@ def main(model_dir, train_loc, heldout_gold):
         os.mkdir(model_dir)
 
     input_sents = [words for words, tags, labels, heads in read_conll(heldout_gold)]
-    tagger = Tagger(load=False)
+    tagger = Tagger(FeatureExtractor(), load=False)
     sentences = list(read_conll(train_loc))
     train(tagger, sentences, nr_iter=100)
 
