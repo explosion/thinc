bdd5f6693272a8c099ad58c64e0d892019fe097b
Matthew Honnibal <honnibal@gmail.com>
2016-01-28 15:36:52 +0100

    * Add about.py

---
+++ thinc/about.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '4.2.0'
+version = '4.2.0'
+full_version = '4.2.0'
+git_revision = '6b07491ce694a21da428f93878308b4ae9811c07'
+release = True
+if not release:
+    version = full_version
