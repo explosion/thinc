c482046c314fe4f7d615057283c9b4663c7a2f61
Matthew Honnibal <honnibal@gmail.com>
2016-01-30 19:14:03 +0100

    * Move and delete files

--- thinc/about.py
+++ thinc/about.py
@@ -1,8 +1,8 @@
 # THIS FILE IS GENERATED FROM SETUP.PY
 short_version = '5.0.0'
 version = '5.0.0'
-full_version = '5.0.0'
-git_revision = 'd8c1ccdfc30e5160eae9774cefaa90f7bacb7a3e'
-release = True
+full_version = '5.0.0.dev0+d980881'
+git_revision = 'd98088182636c0cf24842de71fc62204b7290075'
+release = False
 if not release:
     version = full_version
