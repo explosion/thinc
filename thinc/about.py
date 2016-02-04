b5480116445a08c1a4d66b2a40c80385a294f751
Matthew Honnibal <honnibal+gh@gmail.com>
2016-02-04 20:14:02 +0100

    * Update about.py

--- thinc/about.py
+++ thinc/about.py
@@ -1,8 +1,8 @@
 # THIS FILE IS GENERATED FROM SETUP.PY
 short_version = '5.0.0'
 version = '5.0.0'
-full_version = '5.0.0.dev0+3681565'
-git_revision = '3681565cc40a10f56fd8fc9e7d217bde02eb7549'
+full_version = '5.0.0.dev0+3c22590'
+git_revision = '3c225903c1f99c3d07e2675fbc0331d9418d6804'
 release = False
 if not release:
     version = full_version
