edfb2a13aec8213e263d45ecd932b1a76e3a4ada
Matthew Honnibal <honnibal+gh@gmail.com>
2016-02-26 21:16:30 +1100

    Create _deserialize.py

---
+++ bin/_deserialize.py
@@ -0,0 +1,16 @@
+'''Load a spaCy model
+
+1. Download and extract https://index.spacy.io/models/en_default-1.0.8/archive.gz
+2. Find the model file deps/model
+3. Pass the path to this file to this script
+'''
+import sys
+
+
+def main(loc):
+    model = thinc.linear.avgtron.AveragedPerceptron([])
+    model.load(loc)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
