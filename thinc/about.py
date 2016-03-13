875c5b7a72e40830ec75f94098fdd9bf511d31a3
Henning Peters <pete@dexterslab.de>
2016-03-13 14:54:24 +0100

    cleanup

--- thinc/about.py
+++ thinc/about.py
@@ -3,11 +3,10 @@
 # https://python-packaging-user-guide.readthedocs.org/en/latest/single_source_version/
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
-__name__ = 'thinc'
+__title__ = 'thinc'
 __version__ = '5.0.6'
 __summary__ = 'Learn sparse linear models'
 __uri__ = 'https://github.com/spacy-io/thinc'
 __author__ = 'Matthew Honnibal'
 __email__ = 'matt@spacy.io'
 __license__ = 'MIT'
-__release__ = True
