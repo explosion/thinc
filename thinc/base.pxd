4bae1952254aa60762d3535af9fa56bf25a6ef89
Matthew Honnibal <honnibal+gh@gmail.com>
2016-08-23 13:12:27 +0200

    Support dense features in base.pyx

--- thinc/base.pxd
+++ thinc/base.pxd
@@ -6,11 +6,11 @@ from .structs cimport ExampleC, FeatureC
 
 cdef class Model:
     cdef void set_scoresC(self, weight_t* scores,
-        const FeatureC* feats, int nr_feat) nogil
+        const void* feats, int nr_feat, int is_sparse) nogil
  
     cpdef int update_weight(self, feat_t feat_id, class_t clas, weight_t upd) except -1
 
     cdef void set_featuresC(self, ExampleC* eg, const void* state) nogil 
 
-    cdef void dropoutC(self, FeatureC* feats, weight_t drop_prob,
-            int nr_feat) nogil
+    cdef void dropoutC(self, void* feats, weight_t drop_prob,
+            int nr_feat, int is_sparse) nogil
--- thinc/base.pyx
+++ thinc/base.pyx
@@ -32,16 +32,20 @@ cdef class Model:
     cpdef int update_weight(self, feat_t feat_id, class_t clas, weight_t upd) except -1:
         pass
     cdef void set_scoresC(self, weight_t* scores,
-            const FeatureC* feats, int nr_feat) nogil:
+            const void* feats, int nr_feat, int is_sparse) nogil:
         pass
 
     cdef void set_featuresC(self, ExampleC* eg, const void* state) nogil: 
         pass
 
-    cdef void dropoutC(self, FeatureC* feats, weight_t keep_prob,
-            int nr_feat) nogil:
+    cdef void dropoutC(self, void* _feats, weight_t keep_prob,
+            int nr_feat, int is_sparse) nogil:
         if keep_prob == 1.0:
             return
+        if is_sparse:
+            sparse = <FeatureC*>_feats
+        else:
+            dense = <weight_t*>_feats
         for i in range(nr_feat):
             if prng.get_uniform() < keep_prob:
                 # Preserve the mean activation, by increasing the activation
@@ -50,6 +54,12 @@ cdef class Model:
                 # I think I read this somewhere.
                 # If not...well, it makes sense right?
                 # Right?
-                feats[i].value *= 1.0 / keep_prob
+                if is_sparse:
+                    sparse[i].value *= 1.0 / keep_prob
+                else:
+                    dense[i] *= 1.0 / keep_prob
             else:
-                feats[i].value = 0
+                if is_sparse:
+                    sparse[i].value = 0
+                else:
+                    dense[i] = 0
