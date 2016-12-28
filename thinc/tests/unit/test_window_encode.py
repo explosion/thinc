f5a04b7c0820e48407328ed752b0ec25797321e2
Matthew Honnibal <honnibal+gh@gmail.com>
2016-12-28 18:56:06 +0100

    Update test for window encode. Not sure about last test

--- thinc/tests/unit/test_window_encode.py
+++ thinc/tests/unit/test_window_encode.py
@@ -1,5 +1,9 @@
 import pytest
+import numpy
+
 from ...ids2vecs import WindowEncode
+from ...optimizers import SGD
+
 
 class MyWindowEncode(WindowEncode):
     device = 'cpu'
@@ -18,30 +22,26 @@ def model():
 def test_init():
     model = MyWindowEncode(device='cpu', nr_out=10, nr_in=2, vectors={})
     assert model.nr_out == 10
-    assert model.W.shape == (10, 3, 2)
+    assert model.W.shape == (10, 3, 5, 2)
 
 
-@pytest.mark.xfail
 def test_get_ids(model):
     x = [[1, 10], [20, 20]]
-    ids = model._get_ids(x)
+    ids = model._get_positions(x)
     assert ids == {1: [(0, 0)], 10: [(0, 1)], 20: [(1, 0), (1, 1)]}
 
 
-@pytest.mark.xfail
 def test_dot_ids(model):
     x = [[1, 10], [20, 20]]
-    ids = model._get_ids(x)
+    ids = model._get_positions(x)
     assert ids == {1: [(0, 0)], 10: [(0, 1)], 20: [(1, 0), (1, 1)]}
     model._dot_ids(ids, [2, 2])
 
-@pytest.mark.xfail
 def test_predict_batch(model):
     x = [[1, 10], [20, 20]]
     scores = model.predict_batch(x)
 
 
-@pytest.mark.xfail
 def test_update(model):
     x = [[1, 10], [20, 20]]
     scores, update = model.begin_update(x)
@@ -50,30 +50,14 @@ def test_update(model):
 
 
 def set_vector(model, key, values):
-    model.add_param(key, (len(values),))
-    vector = model.get_param(key)
+    model.add_vector(key, (len(values),))
+    vector = model.get_vector(key)
     for i, value in enumerate(values):
         vector[i] = value
  
 
-@pytest.mark.xfail
-def test_init():
-    model = WindowEncode('encode', (10,), (5,), tuple())
-    assert model.nr_out == 10
-
-
-@pytest.mark.xfail
-def test_add_param():
-    model = WindowEncode('encode', (2,), (2,), [])
-    model.add_param(1, (2,))
-    vector = model.get_param(1)
-    assert vector.shape == (2,)
-    assert vector.sum() != 0
-
-
-@pytest.mark.xfail
 def test_predict_shapes():
-    model = WindowEncode('encode', (5,), (2,), [])
+    model = WindowEncode('encode', nr_out=5, nr_in=2)
     set_vector(model, 1, [1., -1.])
     set_vector(model, 2, [0.25, -0.25])
 
@@ -83,29 +67,16 @@ def test_predict_shapes():
     assert output[1].shape == (2, 5)
 
 
-@pytest.mark.xfail
 def test_update():
-    for side in [0, 1]:
-        model = WindowEncode('encode', (2,), (2,), [])
-        model.W.fill(1)
-        model.b.fill(0)
-        if side == 0:
-            model.b[0, 0] = 1.0
-            model.b[1, 0] = 0.25
-        else:
-            model.b[0, 1] = 0.25
-            set_vector(model, 1, [1., -1.])
-        set_vector(model, 2, [0.25, -0.25])
-        model.is_initialized = True
-
-        output, finish_update = model.begin_update(numpy.asarray([[2]]))
-        finish_update(numpy.asarray([[[0.25, 0.25]]]), SGD(1.0))
-        vector = model.get_param(2)
-        assert vector[0] == -0.25
-        assert vector[1] == -0.75
-        if side == 0:
-            assert list(model.b[0]) == [0.75, 0.0]
-            assert list(model.b[1]) == [0.0, 0.0]
-        else:
-            assert list(model.b[0]) == [0.0, 0.0]
-            assert list(model.b[1]) == [0.0, 0.75]
+    model = WindowEncode(nr_out=2, nr_in=2, nr_piece=2)
+    model.W.fill(2)
+    model.b.fill(0) 
+    set_vector(model, 2, [0.25, -0.25])
+    
+    output, finish_update = model.begin_update(numpy.asarray([[2]]))
+    finish_update(numpy.asarray([[[0.25, 0.25]]]), SGD(model.ops, 1.0))
+    vector = model.get_vector(2)
+    # Not positive these are correct?
+    assert vector[0] == -0.75
+    assert vector[1] == -1.25
+    assert list(model.b[0]) == [0.0, 0.0]
