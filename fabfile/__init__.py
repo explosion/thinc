43cb0191a5067687fdee466defb606d26700bf41
Matthew Honnibal <honnibal+gh@gmail.com>
2016-12-30 18:23:23 +0100

    Put basic repo commands in fabfile/__init__.py

---
+++ fabfile/__init__.py
@@ -0,0 +1,37 @@
+from fabric.api import task, local, run, lcd, cd, env
+from os.path import exists as file_exists
+from fabtools.python import virtualenv
+from os import path
+
+
+PWD = path.join(path.dirname(__file__), '..')
+VENV_DIR = path.join(PWD, '.env')
+
+
+
+@task
+def env():
+    if file_exists('.env'):
+        local('rm -rf .env')
+    local('virtualenv .env')
+    with virtualenv(VENV_DIR):
+        local('pip install --upgrade setuptools')
+        local('pip install -r requirements.txt')
+        local('pip install pytest')
+
+@task
+def make():
+    with virtualenv(VENV_DIR):
+        with lcd(PWD):
+            local('python setup.py build_ext --inplace')
+
+@task
+def clean():
+    with lcd(PWD):
+        local('python setup.py clean --all')
+
+@task
+def test():
+    with virtualenv(VENV_DIR):
+        with lcd(PWD):
+            local('python -m pytest -x thinc')
