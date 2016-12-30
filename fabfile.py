0d9c81996e436f5f717801e8abdd6c12e9c39084
Matthew Honnibal <honnibal+gh@gmail.com>
2016-12-30 18:16:02 +0100

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import local, run, lcd, cd, env
+from fabric.api import task, local, run, lcd, cd, env
 from os.path import exists as file_exists
 from fabtools.python import virtualenv
 from os import path
@@ -6,19 +6,17 @@ from os import path
 
 PWD = path.dirname(__file__)
 VENV_DIR = path.join(PWD, '.env')
-DEV_ENV_DIR = path.join(PWD, '.denv')
 
 
-def dev():
-    # Allow this to persist, since we aren't as rigorous about keeping state clean
-    if not file_exists('.denv'):
-        local('virtualenv .denv')
- 
-    with virtualenv(DEV_ENV_DIR):
-        local('pip install -r requirements.txt')
+#def dev():
+#    # Allow this to persist, since we aren't as rigorous about keeping state clean
+#    if not file_exists('.denv'):
+#        local('virtualenv .denv')
+# 
+#    with virtualenv(DEV_ENV_DIR):
+#        local('pip install -r requirements.txt')
 
-
-
+@task
 def sdist():
     if file_exists('dist/'):
         local('rm -rf dist/')
@@ -26,7 +24,7 @@ def sdist():
     with virtualenv(VENV_DIR):
         local('python setup.py sdist')
 
-
+@task
 def publish(version):
     with virtualenv(VENV_DIR):
         local('git push origin master')
@@ -36,36 +34,33 @@ def publish(version):
         local('python setup.py register')
         local('twine upload dist/*.tar.gz')
 
-
-def setup():
+@task
+def env():
     if file_exists('.env'):
         local('rm -rf .env')
     local('virtualenv .env')
+    local('pip install -r requirements.txt')
 
-
+@task
 def install():
     with virtualenv(VENV_DIR):
         local('pip install --upgrade setuptools')
         local('pip install dist/*.tar.gz')
         local('pip install pytest')
 
-
+@task
 def make():
     with virtualenv(VENV_DIR):
         with lcd(path.dirname(__file__)):
             local('python setup.py build_ext --inplace')
 
-
+@task
 def clean():
     with lcd(path.dirname(__file__)):
         local('python setup.py clean --all')
 
-
+@task
 def test():
     with virtualenv(VENV_DIR):
         with lcd(path.dirname(__file__)):
             local('python -m pytest -x thinc')
-
-
-def travis():
-    local('open https://travis-ci.org/spacy-io/thinc')
