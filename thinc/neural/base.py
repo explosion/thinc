2aa482ab4289955080466be07e98f759b3c380aa
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-02 17:51:44 +0100

    Work on new API

--- thinc/neural/base.py
+++ thinc/neural/base.py
@@ -1,6 +1,7 @@
 import numpy
 
 from . import util
+from .util import Unassigned
 from .train import Trainer
 from .exceptions import ShapeError
 
@@ -8,20 +9,45 @@ from .exceptions import ShapeError
 class Model(object):
     '''Model base class.'''
     name = 'model'
-    device = 'cpu'
     Trainer = Trainer
-    ops = None
-    output_shape = None
-    input_shape = None
-    
+    ops = Unassigned(Ops)
+    output_shape = Unassigned(tuple)
+    input_shape = Unassigned(tuple)
+    layers = Unassigned(list)
+    params = Unassigned(Params)
+
+    @property
+    def size(self):
+        raise NotImplementedError
+
+    @property
+    def describe_params(self):
+        pass
+
     def __init__(self, *args, **kwargs):
         self.layers = []
-        kwargs = self.update_defaults(*args, **kwargs)
+        kwargs = self._args2kwargs(
+                    ('output_shape', 'input_shape'),
+                    args, **kwargs)
+        kwargs = self._update_defaults(*args, **kwargs)
+        self.setup(*args, **kwargs)
         if self.ops is None:
             self.ops = util.get_ops(self.device)
-        self.setup(*args, **kwargs)
 
-    def update_defaults(self, *args, **kwargs):
+    def _args2kwargs(self, names, args, **kwargs):
+        # Move positional args into the keyword args, so they can be handled
+        # via the _update_defaults machinery.
+        assert len(names) == len(args), "TODO: Error message"
+        if not args:
+            return kwargs
+        if len(args) >= 1:
+            assert 'output_shape' not in kwargs, "TODO: Error message"
+            kwargs['output_shape'] = args.pop(0)
+        if len(args) >= 1:
+            assert 'input_shape' not in kwargs, "TODO: Error message"
+            kwargs['input_shape'] = args.pop(1)
+    
+    def _update_defaults(self, *args, **kwargs):
         new_kwargs = {}
         for key, value in kwargs.items():
             if hasattr(self, key):
@@ -30,25 +56,35 @@ class Model(object):
                 new_kwargs[key] = value
         return new_kwargs
     
-    def setup(self, *args, **kwargs):
-        pass
+    def setup(self, *layers, **kwargs):
+        for i, layer in enumerate(layers):
+            if isinstance(layer, Model):
+                self.layers.append(layer)
+            else:
+                self.layers.append(layer(**kwargs))
 
-    def check_shape(self, x, is_batch):
-        if is_batch:
-            if len(x.shape) != 2:
-                raise ShapeError.expected_batch(locals(), globals())
-            if x.shape[1] != self.nr_in:
-                dims = (x.shape[1], self.nr_in)
-                raise ShapeError.dim_mismatch(dims, locals(), globals())
+    def initialize_params(self, train_data):
+        shape = train_data[0].shape
+        for i, dim in enumerate(shape):
+            if self.shape[i] is None:
+                self.shape[i] = dim
+        for name, shape, init in self.describe_params:
+            self.params.add(name, shape)
+            init(self.params.get(name), train_data)
+        for layer in self.layers:
+            layer.initialize_params(train_data)
+        self.params.merge_params(layer.params for layer in self.layers)
+
+    def check_input(self, x):
+        if is_batch(x):
+            shape = x.shape[1:]
+        if shape != self.input_shape:
+            raise ShapeError.dim_mismatch(x, self)
         else:
-            if x.shape[0] != self.nr_in:
-                dims = (x.shape[0], self.nr_in)
-                raise ShapeError.dim_mismatch(dims, locals(), globals())
+            return True
 
     def __call__(self, x):
         '''Predict a single x.'''
-        if not self.is_initialized:
-            self.set_weights(initialize=True, example=x)
         is_batch = self.is_batch(x)
         self.check_shape(x, is_batch)
         if is_batch:
@@ -56,22 +92,19 @@ class Model(object):
         else:
             return self.predict_one(x)
 
-    def is_batch(self, X):
-        if hasattr(X, 'shape') and len(X.shape) >= 2:
-            return True
-        else:
-            return False
-    
     def predict_one(self, x):
         X = self.ops.expand_dims(x, axis=0)
         return self.predict_batch(X)[0]
 
     def predict_batch(self, X):
-        raise NotImplementedError
-    
-    def begin_training(self, train_data):
-        return self.Trainer(self, train_data)
+        for layer in self.layers:
+            X = layer.predict_batch(X)
+        return X
 
+    def begin_training(self, train_data):
+        self.initialize_params(train_data)
+        return self.Trainer(self, train_data)
+    
     def pipe(self, stream, batch_size=1000):
         for batch in util.minibatch(stream, batch_size):
             ys = self.predict_batch(batch)
@@ -84,86 +117,31 @@ class Model(object):
             gradient = finish_update(y)
             yield gradient
     
-    def average_params(self, optimizer):
-        pass
-
-    def begin_update(self, X, dropout=0.0):
-        raise NotImplementedError
-    
-    @property
-    def is_initialized(self):
-        return True
-
-
-class Network(Model):
-    '''A model that chains together other Models.'''
-    name = 'mlp'
-
-    @property
-    def input_shape(self):
-        return self.layers[0].input_shape
-
-    @property
-    def output_shape(self):
-        return self.layers[-1].output_shape
-
-    @property
-    def nr_weight(self):
-        return sum(layer.nr_weight for layer in self.layers)
-
-    def setup(self, *layers, **kwargs):
-        for i, layer in enumerate(layers):
-            if isinstance(layer, Model):
-                self.layers.append(layer)
-            else:
-                self.layers.append(layer(**kwargs))
-        self.set_weights(initialize=True)
-        self.set_gradient()
-
-    def set_weights(self, example=None, data=None, initialize=True):
-        if data is None:
-            self.data = self.ops.allocate_pool(self.nr_weight,
-                            name=(self.name, 'pool'))
-        else:
-            self.data = data
-        for layer in self.layers:
-            layer.set_weights(data=self.data, initialize=initialize)
-            layer.data = None
-
-    def set_gradient(self, data=None):
-        if data is None:
-            self.d_data = self.ops.allocate_pool(self.nr_weight,
-                            name=(self.name, 'd_pool'))
-        else:
-            self.d_data = data
-        for layer in self.layers:
-            layer.set_gradient(data=self.d_data)
-            layer.d_data = None
-
-    def predict_batch(self, X):
-        for layer in self.layers:
-            X = layer.predict_batch(X)
-        return X
-
-    def begin_update(self, X, dropout=0.0):
+    def begin_update(self, X, **kwargs):
         callbacks = []
         for layer in self.layers:
-            X, finish_update = layer.begin_update(X, dropout=dropout)
+            X, finish_update = layer.begin_update(X, **kwargs)
             callbacks.append(finish_update)
         return X, self._get_finish_update(callbacks)
-
-    def average_params(self, optimizer):
-        for layer in self.layers:
-            layer.average_params(optimizer)
-        if self.data is not None and ('data', self.name) in optimizer.averages:
-            self.data.data[:] = optimizer.averages[('data', self.name)]
-
+    
     def _get_finish_update(self, callbacks):
         def finish_update(gradient, optimizer, **kwargs):
             for callback in reversed(callbacks):
                 gradient = callback(gradient, optimizer=optimizer, **kwargs)
-            if optimizer is not None and self.data is not None:
-                optimizer(self.data.data, self.d_data.data,
+            if optimizer is not None and self.params is not None:
+                optimizer(self.params.data, self.params.gradient,
                     key=('data', self.name), **kwargs)
             return gradient
         return finish_update
+
+    def average_params(self, averages):
+        if not self.is_allocated:
+            raise Exception("TODO Error")
+
+        for layer in self.layers:
+            layer.average_params(averages)
+        self.params.update(averages)
+        if ('data', self.name) in averages:
+            self.params.data[:] = averages[('data', self.name)]
+
+
--- thinc/neural/vec2vec.py
+++ thinc/neural/vec2vec.py
@@ -4,153 +4,71 @@ from .exceptions import ShapeError
 
 class Affine(Model):
     name = 'affine'
-    nr_out = None
-    nr_in = None
-    data = None
+    
+    @property
+    def describe_params(self):
+        yield 'W', (self.nr_out, self.nr_in), self.ops.xavier_uniform_init
+        yield 'b', (self.nr_out,), self.ops.xavier_uniform_init
 
     @property
-    def is_initialized(self):
-        return self.W is not None
+    def shape(self):
+        return (self.nr_out, self.nr_in)
 
-    @property
-    def input_shape(self):
-        return (self.nr_in,)
-
-    @property
-    def output_shape(self):
-        return (self.nr_out,)
-
-    @property
-    def nr_weight(self):
-        return (self.nr_out * self.nr_in) + self.nr_out
-
-    def setup(self, *args, **kwargs):
-        self.W = None
-        if 'W' in kwargs:
-            self.nr_out = kwargs.get('W').shape[0]
-            self.nr_in = kwargs.get('W').shape[1]
-        if self.nr_out is not None and self.nr_in is not None:
-            self.set_weights(initialize=True)
-            self.set_gradient()
-        if 'W' in kwargs:
-            self.W[:] = kwargs.get('W')
-        if 'b' in kwargs:
-            self.b[:] = kwargs.get('b')
-
-    def set_weights(self, data=None, initialize=True, example=None):
-        if example is not None:
-            self.nr_in = example.shape[-1]
-        if data is None:
-            if self.data is None:
-                self.data = self.ops.allocate_pool(self.nr_weight,
-                                name=(self.name, 'pool'))
-            data = self.data
-        self.W = data.allocate_shape((self.nr_out, self.nr_in))
-        self.b = data.allocate_shape((self.nr_out,))
-        if initialize:
-            self.ops.xavier_uniform_init(self.W, inplace=True)
-
-    def set_gradient(self, data=None, initialize=False):
-        if data is None:
-            self.d_data = self.ops.allocate_pool(self.nr_weight,
-                            name=(self.name, 'pool'))
-        else:
-            self.d_data = data
-        self.d_W = self.d_data.allocate_shape((self.nr_out, self.nr_in))
-        self.d_b = self.d_data.allocate_shape((self.nr_out,))
+    def setup(self, nr_out=None, nr_in=None, **kwargs):
+        self.nr_out = nr_out
+        self.nr_in = nr_in
+        self.params = Params(self.ops)
+        self.ops.xavier_uniform_init(self.params.W, inplace=True)
 
     def predict_batch(self, input_BI):
-        return self.ops.affine(self.W, self.b, input_BI)
+        return self.ops.affine(self.params.W, self.params.b, input_BI)
 
     def begin_update(self, input_BI, dropout=0.0):
         self.check_shape(input_BI, True)
         output_BO = self.predict_batch(input_BI)
         output_BO, bp_dropout = self.ops.dropout(output_BO, dropout)
         return output_BO, bp_dropout(self._get_finish_update(input_BI))
-   
+
     def _get_finish_update(self, acts_BI):
         def finish_update(d_acts_BO, optimizer=None, **kwargs):
-            self.d_b += d_acts_BO.sum(axis=0)
-            self.d_W += self.ops.batch_outer(d_acts_BO, acts_BI)
-            if optimizer is not None and self.data is not None:
-                optimizer(self.W, self.d_W, key=('W', self.name), **kwargs)
-                optimizer(self.b, self.d_b, key=('b', self.name), **kwargs)
+            self.params.d_b += d_acts_BO.sum(axis=0)
+            self.params.d_W += self.ops.batch_outer(d_acts_BO, acts_BI)
+            if optimizer is not None:
+                optimizer(self.params.data, self.params.gradient,
+                    key=('W', self.name), **kwargs)
             return self.ops.batch_dot(d_acts_BO, self.W.T)
         return finish_update
 
 
-class ReLu(Affine):
-    name = 'relu'
-    def predict_batch(self, input_BI):
-        output_BO = Affine.predict_batch(self, input_BI)
-        return self.ops.xp.maximum(output_BO, 0., out=output_BO)
-
-    def begin_update(self, input_BI, dropout=0.0):
-        output_BO, finish_affine = Affine.begin_update(self, input_BI)
-        def finish_update(gradient, *args, **kwargs):
-            return finish_affine(gradient * (output_BO > 0), *args, **kwargs)
-        return output_BO, finish_update
-
-
-class Softmax(Affine):
-    name = 'softmax'
-
-    def set_weights(self, example=None, data=None, initialize=True):
-        Affine.set_weights(self, example=example, data=data, initialize=False)
-
-    def predict_batch(self, input_bi):
-        output_bo = Affine.predict_batch(self, input_bi)
-        return self.ops.softmax(output_bo, axis=-1)
-
-    def begin_update(self, input_BI, dropout=0.0):
-        return Affine.begin_update(self, input_BI, dropout=0.0)
-
-
 class Maxout(Affine):
     name = 'maxout'
-    nr_piece = 3
-
+    
     @property
-    def nr_weight(self):
-        return self.nr_piece * ((self.nr_out * self.nr_in) + self.nr_out)
+    def describe_params(self):
+        yield 'W', (self.nr_out, self.nr_in), self.ops.xavier_uniform_init
+        yield 'b', (self.nr_out,), None
 
-    def set_weights(self, data=None, initialize=True, example=None):
-        if example is not None:
-            self.nr_in = example.shape[-1]
-        if data is None:
-            if self.data is None:
-                self.data = self.ops.allocate_pool(self.nr_weight,
-                                name=(self.name, 'pool'))
-            data = self.data
-        self.W = data.allocate_shape((self.nr_out, self.nr_piece, self.nr_in))
-        self.b = data.allocate_shape((self.nr_out, self.nr_piece))
-        if initialize:
-            for i in range(self.nr_piece):
-                self.ops.xavier_uniform_init(self.W[:, i], inplace=True)
-
-    def set_gradient(self, data=None, initialize=False):
-        if data is None:
-            self.d_data = self.ops.allocate_pool(self.nr_weight,
-                            name=(self.name, 'pool'))
-        else:
-            self.d_data = data
-        self.d_W = self.d_data.allocate_shape((self.nr_out, self.nr_piece, self.nr_in))
-        self.d_b = self.d_data.allocate_shape((self.nr_out, self.nr_piece))
-
+    def setup(self, nr_out=None, nr_in=None, nr_piece=None, **kwargs):
+        self.nr_out = nr_out
+        self.nr_in = nr_in
+        self.nr_piece = nr_piece
+        self.shape = (self.nr_out, self.nr_piece, self.nr_in)
+        self.params = Params(self.ops)
 
     def predict_batch(self, input_bi):
-        acts_bop = self.ops.xp.tensordot(input_bi, self.W, axes=[[1], [-1]])
-        acts_bop += self.b
+        acts_bop = self.ops.xp.tensordot(input_bi, self.params.W,
+                        axes=[[1], [-1]])
+        acts_bop += self.params.b
         which_bo = self.ops.argmax(acts_bop, axis=-1)
-        return _take_which(self.ops, acts_bop, which_bo)
+        return self.ops.take_which(acts_bop, which_bo)
 
     def begin_update(self, input_BI, dropout=0.0):
-        W_OCI = self.W
-        b_OC = self.b
+        W_OCI = self.params.W
+        b_OC = self.params.b
         output_BOC = self.ops.xp.tensordot(input_BI, W_OCI, axes=[[1], [-1]])
         output_BOC += b_OC
         which_BO = self.ops.argmax(output_BOC, axis=-1)
-        best_BO = _take_which(self.ops, output_BOC, which_BO)
+        best_BO = self.ops.take_which(output_BOC, which_BO)
         best_BO, bp_dropout = self.ops.dropout(best_BO, dropout, inplace=True)
         finish_update = self._get_finish_update(input_BI, which_BO)
         return best_BO, bp_dropout(finish_update)
@@ -162,16 +80,10 @@ class Maxout(Affine):
             for i in range(self.nr_piece):
                 d_acts_BOP[:, :, i] += d_acts_BO * (which_BO == i)
             self.d_b += d_acts_BOP.sum(axis=0)
-            self.d_W += self.ops.xp.tensordot(d_acts_BOP, acts_BI, axes=[[0], [0]])
+            self.d_W += self.ops.xp.tensordot(d_acts_BOP, acts_BI,
+                                              axes=[[0], [0]])
             # Bop,opi->Bi
-            d_acts_BI = self.ops.xp.tensordot(d_acts_BOP, self.W, axes=[[1,2], [0, 1]])
+            d_acts_BI = self.ops.xp.tensordot(d_acts_BOP, self.W,
+                                              axes=[[1,2], [0, 1]])
             return d_acts_BI
         return finish_update
-
-
-def _take_which(ops, x, which, axis=-1):
-    output = ops.allocate(which.shape)
-    for i in range(x.shape[axis]):
-        output += x[:, :, i] * (which == i)
-    return output
- 
