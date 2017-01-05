f1d830609d460e8213252251bf3b512511032add
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-05 18:53:49 +0100

    Add placeholder for relu gradient tests

---
+++ thinc/neural/tests/integration/test_relu_gradient.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.mark.skip
+def test_relu_clips_negative(model, input_BI):
+    # TODO: There was a bug related to this
+    pass
+
+
+@pytest.mark.skip
+def test_relu_clipped_gradients_are_zero():
+    # TODO: There was a bug related to this
+    pass
