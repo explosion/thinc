0794a11c98a502c361a162c9a13ca206a85afd17
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-06 00:47:43 +0100

    Update mnist example

--- examples/mnist.py
+++ examples/mnist.py
@@ -26,24 +26,29 @@ def main(depth=2, width=512, nb_epoch=10):
     train_X, train_Y = zip(*train_data)
     dev_X, dev_Y = zip(*dev_data)
     test_X, test_Y = zip(*test_data)
+    dev_X = model.ops.asarray(dev_X)
+    dev_Y = model.ops.asarray(dev_Y)
+    test_X = model.ops.asarray(test_X)
+    test_Y = model.ops.asarray(test_Y)
 
     with model.begin_training(train_data) as (trainer, optimizer):
         for i in range(nb_epoch):
             for batch_X, batch_Y in trainer.iterate(
-                    model, train_data, dev_data, nb_epoch=1):
+                    model, train_data, dev_X, dev_Y, nb_epoch=1):
                 batch_X = model.ops.asarray(batch_X)
                 guess, finish_update = model.begin_update(batch_X,
-                                        dropout=trainer.dropout)
+                                         dropout=trainer.dropout)
                 gradient, loss = categorical_crossentropy(guess, batch_Y)
                 optimizer.set_loss(loss)
                 trainer._loss += loss / len(batch_Y)
                 finish_update(gradient, optimizer)
-    acc = score_model(model, dev_X, dev_Y)
-    print('Dev.: %.3f' % score_model(model, dev_X, dev_Y))
-    print('Test.: %.3f' % score_model(model, test_X, test_Y))
- 
-    with open('out.pickle', 'wb') as file_:
-        pickle.dump(model, file_, -1)
+            with model.use_params(optimizer.averages):
+                print('Avg dev.: %.3f' % score_model(model, dev_X, dev_Y))
+    with model.use_params(optimizer.averages):
+        print('Avg dev.: %.3f' % score_model(model, dev_X, dev_Y))
+        print('Avg test.: %.3f' % score_model(model, test_X, test_Y))
+        with open('out.pickle', 'wb') as file_:
+            pickle.dump(model, file_, -1)
 
 
 if __name__ == '__main__':
