f044f4c951234e3256ef590971f0bfb9a699a7cd
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-06 20:17:59 +0100

    Move im2col into util

--- thinc/neural/util.py
+++ thinc/neural/util.py
@@ -43,6 +43,29 @@ def minibatch(stream, batch_size=1000):
         yield batch
 
 
+def im2col(sequence, window):
+    assert window == 2
+    output = numpy.zeros((sequence.shape[0], window*2+1, sequence.shape[1]))
+    output[:, 2] = sequence
+    output[2:, 0] = sequence[:-2]
+    output[1:, 1] = sequence[:-1]
+    output[:-1, 3] = sequence[1:]
+    output[:-2, 4] = sequence[2:]
+    # Words 0 and 1 have no LL feature
+    assert sum(abs(output[0, 0])) == 0
+    if len(output) >= 2:
+        assert sum(abs(output[1, 0])) == 0
+        # Word 0 no L feature
+        assert sum(abs(output[1, 0])) == 0
+    # Words -1 and -2 have no RR feature
+    assert sum(abs(output[-1, 4])) == 0
+    if len(output) >= 2:
+        assert sum(abs(output[-2, 4])) == 0
+        # Word -1 has no R feature
+    assert sum(abs(output[-1, 3])) == 0
+    return output
+
+
 class Unassigned(object):
     def __init__(self, expected_type):
         self.expected_type = expected_type
