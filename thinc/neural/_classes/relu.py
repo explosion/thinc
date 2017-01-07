10ba20e9768603079e20bf0c8a31ec74c8319ed3
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-07 17:11:08 +0100

    Describe params in relu

--- thinc/neural/_classes/relu.py
+++ thinc/neural/_classes/relu.py
@@ -3,6 +3,17 @@ from .affine import Affine
 
 class ReLu(Affine):
     name = 'relu'
+    @property
+    def describe_params(self):
+        '''
+        Yields (name, shape, initializer) triples describing the weights directly
+        owned by the layer.
+        '''
+        #def init(W, **kwargs):
+        #    W += self.ops.he_normal_init(W.shape, W.shape[-1])
+        init = self.ops.xavier_uniform_init
+        yield 'W-%s' % self.name, (self.nr_out, self.nr_in), init
+        yield 'b-%s' % self.name, (self.nr_out,), None
     
     def predict_batch(self, X):
         output = Affine.predict_batch(self, X)
@@ -12,4 +23,5 @@ class ReLu(Affine):
         output_BO, finish_affine = Affine.begin_update(self, input_BI)
         def finish_update(gradient, *args, **kwargs):
             return finish_affine(gradient * (output_BO > 0), *args, **kwargs)
+        output_BO = self.ops.clip_low(output_BO, 0, inplace=True)
         return output_BO, finish_update
