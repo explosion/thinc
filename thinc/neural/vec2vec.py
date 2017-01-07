314c5c5672e307a3e13d283d8ad9263b9c4c25ab
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-08 00:38:44 +0100

    Import resnet block in vec2vec

--- thinc/neural/vec2vec.py
+++ thinc/neural/vec2vec.py
@@ -4,3 +4,4 @@ from ._classes.relu    import ReLu
 from ._classes.elu    import ELU
 from ._classes.maxout  import Maxout
 from ._classes.softmax import Softmax
+from ._classes.resnet import ReLuResBN
