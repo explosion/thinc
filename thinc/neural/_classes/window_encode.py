3fe98d9b4aa04f757887ceac2a4ac70d293f9347
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-07 11:18:14 +0100

    Update window encode

--- thinc/neural/_classes/window_encode.py
+++ thinc/neural/_classes/window_encode.py
@@ -11,8 +11,16 @@ class MaxoutWindowEncode(Model):
 
     @property
     def describe_params(self):
-        init = self.ops.xavier_uniform_init
-        yield 'W-%s' % self.name, self.shape, init
+        def _init(W, inplace=True):
+            fan_in = W.shape[2] * W.shape[3]
+            fan_out = W.shape[0]
+            for i in range(W.shape[1]):
+                #scale = self.ops.xp.sqrt(6.) / self.ops.xp.sqrt(fan_in + fan_out)
+                scale = self.ops.xp.sqrt(2. / (fan_in + fan_out))
+                W[:, i] = self.ops.xp.random.uniform(-scale, scale,
+                                                     W[:, i].shape)
+            return W
+        yield 'W-%s' % self.name, self.shape, _init
         yield 'b-%s' % self.name, (self.nr_out, self.nr_piece), None
 
     @property
@@ -38,12 +46,20 @@ class MaxoutWindowEncode(Model):
     def b(self):
         return self.params.get('b-%s' % self.name, require=True)
 
+    @property
+    def d_W(self):
+        return self.params.get('d_W-%s' % self.name, require=True)
+    
+    @property
+    def d_b(self):
+        return self.params.get('d_b-%s' % self.name, require=True)
+
     def __init__(self, nr_out, **kwargs):
         self.nr_out = nr_out
         Model.__init__(self, **kwargs)
 
     def initialize_params(self, train_data=None, add_gradient=True):
-        if train_data is not None:
+        if train_data is not None and self.nr_in is None:
             self.nr_in = self._get_vector_dim_from_input(train_data)
         assert self.shape is not None, "TODO: Error"
         for name, shape, init in self.describe_params:
@@ -59,16 +75,17 @@ class MaxoutWindowEncode(Model):
         out, _ = self._forward(positions, vectors, lengths)
         return self.ops.flatten(out)
 
-    def begin_update(self, ids_vectors, dropout=0.0):
-        ids, vectors = ids_vectors
+    def begin_update(self, ids_vectors_lengths, dropout=0.0):
+        ids, vectors, lengths = ids_vectors_lengths
         positions = _get_positions(ids)
-        lengths = [len(seq) for seq in ids]
         flat_out, whiches = self._forward(positions, vectors, lengths)
         flat_out, bp_dropout = self.ops.dropout(flat_out, dropout, inplace=True)
-        finish_update = self._get_finish_update(ids, vectors, flat_out, whiches)
+        finish_update = self._get_finish_update(vectors, whiches, lengths)
         return flat_out, bp_dropout(finish_update)
 
     def _forward(self, positions, vectors, lengths):
+        positions, vectors = _get_uniq_vectors(positions, vectors)
+        vectors = self.ops.asarray(vectors)
         dotted = _dot_ids(self.ops, self.W, positions, vectors, lengths)
         out = []
         whiches = []
@@ -77,7 +94,7 @@ class MaxoutWindowEncode(Model):
             which = self.ops.argmax(cands)
             out.append(self.ops.take_which(cands, which))
             whiches.append(which)
-        return out, whiches
+        return self.ops.flatten(out), whiches
 
     def _get_finish_update(self, vectors_BI, whiches_BO, lengths_B):
         B, I = vectors_BI.shape
@@ -95,7 +112,7 @@ class MaxoutWindowEncode(Model):
             # Bop,Bfi->opfi
             d_W = self.d_W
             d_W += self.ops.batch_outer(gradients_BOP, inputs_BFI)
-            if self.skip_gradient:
+            if kwargs.get('skip_gradient'):
                 return None
             else:
                 gradients_BI = self.ops.xp.einsum(
@@ -104,18 +121,41 @@ class MaxoutWindowEncode(Model):
         return finish_update
 
 
+def _get_uniq_vectors(positions, vectors):
+    # Need to set id back into sequence
+    # Or, really, need to fix API of WindowEncode tagger. Maybe insert
+    # shim operation?
+    uniq_vectors = []
+    remapped_positions = {}
+    for i, (key, key_positions) in enumerate(positions.items()):
+        seq_idx = key_positions[0]
+        uniq_vectors.append(vectors[seq_idx])
+        remapped_positions[i] = key_positions
+    return remapped_positions, uniq_vectors
+
+
+def _get_positions(ids):
+    positions = defaultdict(list)
+    for i, id_ in enumerate(ids):
+        positions[id_].append(i)
+    return positions
+
+
 def _dot_ids(ops, W, positions, vectors, lengths):
-    # Shift the lengths, so that we don't have to special-case the starts and
-    # ends. We'll shift back afterwards.
     window_size = int((W.shape[2]-1) / 2)
-    out = [ops.allocate((length+(window_size*2), W.shape[0], W.shape[1]))
-           for length in lengths]
+    total_length = sum(lengths)
+    # Shift the input, so that we don't have to special-case the starts and
+    # ends. We'll shift back afterwards.
+    out = ops.allocate((total_length+(window_size*2), W.shape[0], W.shape[1]))
     H__bopf = _compute_hidden_layer(ops, W, vectors, lengths)
-    for vec_idx, ijs in positions.items():
-        for i, j in ijs:
-            out[i][j : j+5] += H__bopf[vec_idx]
+    
+    for vec_idx, tok_idxs in positions.items():
+        for i in tok_idxs:
+            out[i : i+5] += H__bopf[vec_idx]
     # Shift the output, to correct for the 'padding' shift above.
-    out = [arr[window_size : -window_size] for arr in out]
+    out = out[window_size : -window_size]
+    # Recover sequences
+    out = ops.unflatten(out, lengths)
     # Zero the LL, L, R and RR features for starts and ends.
     for i in range(len(out)):
         out[i][0, 0] = 0
@@ -133,14 +173,6 @@ def _compute_hidden_layer(ops, W__opfi, vectors__bi, lengths):
     H__bopf = ops.xp.tensordot(vectors__bi, W__opfi, axes=[[1], [3]])
     H__bfop = H__bopf.transpose((0, 3, 1, 2))
     return H__bfop
-
-
-def _get_positions(id_seqs):
-    positions = defaultdict(list)
-    for i, id_seq in enumerate(id_seqs):
-        for j, key, in enumerate(id_seq):
-            positions[key].append((i, j))
-    return positions
 
 
 def _get_full_gradients(flat_gradients, gradients, whiches):
