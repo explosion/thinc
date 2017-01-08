7f95ddd0be093942989961939995d443e3ce2508
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-08 22:07:54 +0100

    Add Cython Adam and update_averages

--- thinc/neural/optimizers.pyx
+++ thinc/neural/optimizers.pyx
@@ -1,25 +1,52 @@
+# cython: profile=True
+# cython: cdivision=True
+# cython: infer_types=True
+cimport cython
+from libc.string cimport memcpy, memset
+from libc.math cimport exp, sqrt
+from libc.stdlib cimport calloc, malloc, free
+
+
 from collections import defaultdict
 import numpy
+
+from ..typedefs cimport weight_t
+from ..linalg cimport Mat, MatMat, MatVec, VecVec, Vec, sqrt
 
 
 def linear_decay(rate, decay, nr_upd):
     return rate * 1./(1. + decay * nr_upd)
 
 
-def update_averages(averages, key, weights, nr_upd, max_decay=0.9999):
-    decay = (1. + nr_upd) / (10. + nr_upd)
-    decay = min(decay, max_decay)
-
-    if key not in averages:
-        averages[key] = numpy.zeros(weights.shape)
-    avg = averages[key]
-    avg -= (1-decay) * (avg - weights)
-
-
-def clip_gradient(gradient, threshold):
-    grad_norm = numpy.linalg.norm(gradient)
+@cython.cdivision(True)
+cdef void _clip_gradient(weight_t* gradient, weight_t threshold, int nr_weight) nogil:
+    # Clip gradient
+    grad_norm = Vec.norm(gradient, nr_weight)
     if grad_norm >= threshold:
-        gradient *= threshold / grad_norm
+        Vec.mul_i(gradient, threshold / grad_norm, nr_weight)
+
+
+@cython.cdivision(True)
+cdef void _update_averages(weight_t* ema,
+        const weight_t* weights, int nr_weight, weight_t t) nogil:
+    cdef weight_t decay = (1.0 + t) / (10.0 + t)
+    if decay > 0.9999:
+        decay = 0.9999
+    for i in range(nr_weight):
+        ema[i] -= (1-decay) * (ema[i] - weights[i])
+
+
+def update_averages(averages, key, weight_t[:] weights,
+        nr_upd, max_decay=0.9999):
+    if key not in averages:
+        averages[key] = numpy.zeros((weights.size,), dtype='f')
+    cdef weight_t[:] avg = averages[key]
+    _update_averages(&avg[0],
+        &weights[0], avg.shape[0], nr_upd)
+
+
+def clip_gradient(weight_t[:] gradient, threshold):
+    _clip_gradient(&gradient[0], threshold, gradient.shape[0])
 
 
 class SGD(object):
@@ -82,35 +109,53 @@ class Adam(object):
             return 0
         return max(self.nr_update.values())
 
-    def __call__(self, weights, gradient, key=None):
+    def __call__(self, weight_t[:] weights, weight_t[:] gradient, key=None):
         assert key is not None
         assert len(gradient) >= 1
         assert not self.ops.xp.isnan(weights).any()
         assert not self.ops.xp.isnan(gradient).any()
         if key not in self.mom1:
-            self.mom1[key] = self.ops.allocate(weights.shape)
+            self.mom1[key] = self.ops.allocate(weights.size)
         if key not in self.mom2:
-            self.mom2[key] = self.ops.allocate(weights.shape)
+            self.mom2[key] = self.ops.allocate(weights.size)
         self.nr_update[key] += 1
         nr_upd = self.nr_update[key]
 
         clip_gradient(gradient, len(gradient) / 100.)
-        mom1 = self.mom1[key]
-        mom1 *= self.b1
-        mom1 += (gradient * (1-self.b1))
 
-        mom2 = self.mom2[key]
-        mom2 *= self.b2
-        mom2 += (1-self.b2) * gradient ** 2
+        cdef weight_t[:] mom1 = self.mom1[key]
+        cdef weight_t[:] mom2 = self.mom2[key]
+        cdef weight_t lr = self.lr(nr_upd)
+        cdef weight_t b1 = self.b1
+        cdef weight_t b2 = self.b1
+        cdef weight_t eps = self.eps
         
-        lr = self.lr(nr_upd)
-        weights -= lr * mom1 / (self.d * numpy.sqrt(mom2) + self.eps)
-        gradient.fill(0)
+        _adam(&weights[0], &gradient[0], &mom1[0], &mom2[0],
+            weights.shape[0], b1, b2, eps, lr)
+        
         if self.averages is not None:
             update_averages(self.averages, key, weights, nr_upd)
 
     def set_loss(self, loss):
         pass
+
+
+@cython.cdivision(True)
+cdef void _adam(
+    weight_t* weights, weight_t* gradient, weight_t* mom1, weight_t* mom2, 
+        int nr_weight, weight_t beta1, weight_t beta2, weight_t eps,
+        weight_t learn_rate) nogil:
+    Vec.mul_i(mom1,
+        beta1, nr_weight)
+    VecVec.add_i(mom1,
+        gradient, 1-beta1, nr_weight)
+    for i in range(nr_weight):
+        mom2[i] = (beta2 * mom2[i]) + ((1-beta2) * gradient[i] * gradient[i])
+    # Here we assume this is calculated by the caller.
+    #cdef weight_t a_t = learn_rate * sqrt(1-beta2**hp.t) / (1-beta1**hp.t)
+    for i in range(nr_weight):
+        weights[i] -= learn_rate * (mom1[i] / (sqrt(mom2[i]) + eps))
+    memset(gradient, 0, sizeof(gradient[0]) * nr_weight)
 
 
 class Eve(Adam):
