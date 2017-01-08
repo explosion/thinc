727fa02f181f834385264c065de196603712b945
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-09 00:26:56 +0100

    Fix ELU bug caused by inplace operation.

--- thinc/neural/_classes/elu.py
+++ thinc/neural/_classes/elu.py
@@ -17,15 +17,21 @@ class ELU(Affine):
         yield 'b-%s' % self.name, (self.nr_out,), None
  
     def predict_batch(self, X):
-        output = Affine.predict_batch(self, X)
+        output = self.ops.xp.ascontiguousarray(
+                    Affine.predict_batch(self, X), dtype='f')
         self.ops.elu(output, inplace=True)
         return output
 
     def begin_update(self, input_BI, dropout=0.0):
         output_BO, finish_affine = Affine.begin_update(self, input_BI)
+
+        output_copy = self.ops.xp.ascontiguousarray(output_BO, dtype='f')
+        self.ops.elu(output_copy, inplace=True)
         def finish_update(gradient, *args, **kwargs):
-            self.ops.backprop_elu(gradient, output_BO, inplace=True)
+            gradient = self.ops.xp.ascontiguousarray(gradient, dtype='f')
+            self.ops.backprop_elu(gradient, output_copy, inplace=True)
             return finish_affine(gradient, *args, **kwargs)
+        output_BO[:] = output_copy
         return output_BO, finish_update
 
 
