da4ae49f73807ea8f7b84bcf1dc1b893e1bdfd03
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-10 22:27:18 +0100

    Add tests for operator binding

---
+++ thinc/neural/tests/unit/Model/test_bind_operators.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ...._classes.model import Model
+
+
+def test_bind_plus():
+    with Model.bind_operators({'+': lambda a, b: (a.name, b.name)}):
+        m = Model(name='a') + Model(name='b')
+        assert m == ('a', 'b')
+
+def test_plus_chain():
+    with Model.bind_operators({'+': lambda a, b: a}):
+        m = Model(name='a') + Model(name='b') + Model(name='c') + Model(name='d')
+        assert m.name == 'a'
