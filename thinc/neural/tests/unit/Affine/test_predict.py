5d6a51fdb2a2be851d917c4e404b3ca7e08b158e
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-12 23:23:49 +0100

    Fix test_predict unit tests for Affine

--- thinc/neural/tests/unit/Affine/test_predict.py
+++ thinc/neural/tests/unit/Affine/test_predict.py
@@ -1,7 +1,7 @@
 import numpy
 import pytest
 from numpy.testing import assert_allclose
-from hypothesis import given
+from hypothesis import given, settings
 from hypothesis.strategies import integers
 
 from ....ops import NumpyOps
@@ -10,14 +10,13 @@ from ....exceptions import ShapeError
 
 from ...strategies import arrays_OI_O_BI
 
-@pytest.mark.skip
+
+@settings(max_examples=100)
 @given(arrays_OI_O_BI(max_batch=8, max_out=8, max_in=8))
-def test_predict_batch_quickly(W_b_input):
-    ops = NumpyOps()
+def test_predict_small(W_b_input):
     W, b, input_ = W_b_input
     nr_out, nr_in = W.shape
-    model = Affine(nr_out, nr_in, ops=ops)
-    model.initialize_params()
+    model = Affine(nr_out, nr_in)
     model.W[:] = W
     model.b[:] = b
 
@@ -26,18 +25,15 @@ def test_predict_batch_quickly(W_b_input):
     
     expected_output = einsummed + b
     
-    predicted_output = model.predict_batch(input_)
+    predicted_output = model.predict(input_)
     assert_allclose(predicted_output, expected_output, rtol=1e-03, atol=0.001)
-
 
 @pytest.mark.skip
 @given(arrays_OI_O_BI(max_batch=100, max_out=100, max_in=100))
-def test_predict_batch_extensively(W_b_input):
-    ops = NumpyOps()
+def test_predict_extensive(W_b_input):
     W, b, input_ = W_b_input
     nr_out, nr_in = W.shape
-    model = Affine(nr_out, nr_in, ops=ops)
-    model.initialize_params()
+    model = Affine(nr_out, nr_in)
     model.W[:] = W
     model.b[:] = b
 
@@ -46,5 +42,5 @@ def test_predict_batch_extensively(W_b_input):
     
     expected_output = einsummed + b
     
-    predicted_output = model.predict_batch(input_)
+    predicted_output = model.predict(input_)
     assert_allclose(predicted_output, expected_output, rtol=1e-04, atol=0.0001)
