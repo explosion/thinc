aff1cc1860f9b90836facf6e4a3aa209b1d85394
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-13 21:08:44 +0100

    Update softmax

--- thinc/neural/_classes/softmax.py
+++ thinc/neural/_classes/softmax.py
@@ -1,25 +1,21 @@
-from .model import Model
+from .affine import Affine
 from ... import describe
 from ...describe import Dimension, Synapses, Biases
 
 
-@describe.output(("nO",))
-@describe.input(("nI",))
 @describe.attributes(
-    nI=Dimension("Size of input"),
-    nP=Dimension("Number of pieces"),
-    nO=Dimension("Size of output"),
-    W=Synapses("The weights matrix", ("nO", "nI")),
-    b=Biases("Bias parameter", ("nO", "nI"))
+    W=Synapses("Weights matrix",
+        lambda obj: (obj.nO, obj.nI),
+        lambda W, ops: None)
 )
-class Softmax(Model):
+class Softmax(Affine):
     def predict(self, input__BI):
         output__BO = self.ops.affine(self.W, self.b, input__BI)
         self.ops.softmax(output__BO, inplace=True)
         return output__BO
 
     def begin_update(self, input__BI):
-        output__BO = self.predict(input_BI)
+        output__BO = self.predict(input__BI)
         def finish_update(grad__BO):
             self.d_W += self.ops.batch_outer(grad__BO, input__BI)
             self.d_b += grad__BO.sum(axis=0)
