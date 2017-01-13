9ef0073ee703b28fd470797f413503f1754dde65
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-13 11:15:20 +0100

    Clean up maxout layer

--- thinc/neural/_classes/maxout.py
+++ thinc/neural/_classes/maxout.py
@@ -1,47 +1,38 @@
 from .model import Model
+from ... import describe
+from ...describe import Dimension, Synapses, Biases
 
 
-@declare_dimensions(
-    I=("Size of input"),
-    P=("Number of pieces"),
-    O=("Size of output"),
-)
-@declare_input(shape=Floats("I"))
-@declare_output(shape=Floats("O"))
-@declare_weights(
-    W=Schema(
-        "The weights matrix",
-        shape=("O", "P", "I"),
-        initialize=xavier_init,
-        static=False
-    ),
-    b=Schema(
-        "Bias parameter",
-        shape=("O", "P"),
-        inititialize=initializers.zeros,
-        static=False
-    )
+@describe.output(("nO",))
+@describe.input(("nI",))
+@describe.attributes(
+    nI=Dimension("Size of input"),
+    nP=Dimension("Number of pieces"),
+    nO=Dimension("Size of output"),
+    W=Synapses("The weights matrix", ("nO", "nP", "nI"),
+        lambda W, ops: ops.xavier_uniform_init),
+    b=Biases("Bias parameter", ("nO", "nP"))
 )
 class Maxout(Model):
     def predict(self, X__BI):
         X__BOP = self.ops.xp.tensordot(X__BI, self.w.W, axes=[[1], [-1]])
-        X__BOP += self.w.b
+        X__BOP += self.b
         which__BO = self.ops.argmax(X__BOP, axis=-1)
         return self.ops.take_which(X__BOP, which__BO)
 
     def begin_update(self, X__BI):
-        output__BOC = self.ops.xp.tensordot(X__BI, self.w.W, axes=[[1], [-1]])
+        output__BOC = self.ops.xp.tensordot(X__BI, self.W, axes=[[1], [-1]])
         output__BOC += b_OC
         which__BO = self.ops.argmax(output__BOC, axis=-1)
         best__BO = self.ops.take_which(output_BOC, which_BO)
         
         def finish_update(dX__BO):
-            dX__BOP = self.ops.allocate((dX__BOP.shape[0], self.n.O, self.n.P))
-            for i in range(self.n.P):
+            dX__BOP = self.ops.allocate((dX__BOP.shape[0], self.nO, self.nP))
+            for i in range(self.nP):
                 dX__BOP[:, :, i] += dX__BOP * (which__BO == i)
-            self.d.d_b += dX__BOP.sum(axis=0)
-            self.d.d_W += self.ops.xp.tensordot(dX__BOP, X__BI, axes=[[0], [0]])
+            self.d_b += dX__BOP.sum(axis=0)
+            self.d_W += self.ops.xp.tensordot(dX__BOP, X__BI, axes=[[0], [0]])
             # Bop,opi->Bi
-            dX__BI = self.ops.xp.tensordot(dX__BOP, self.w.W, axes=[[1,2], [0, 1]])
+            dX__BI = self.ops.xp.tensordot(dX__BOP, self.W, axes=[[1,2], [0, 1]])
             return dX__BI
         return best_BO, finish_update
