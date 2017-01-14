629f54b469eb6c97a4567bf6572137b4f3132eb6
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-14 17:19:32 +0100

    Use optimized relu layer

--- thinc/neural/_classes/relu.py
+++ thinc/neural/_classes/relu.py
@@ -6,12 +6,12 @@ from ...describe import Dimension, Synapses, Biases
 class ReLu(Affine):
     def predict(self, input__BI):
         output__BO = Affine.predict(self, input__BI)
-        output__BO *= output__BO > 0
+        output__BO = self.ops.xp.ascontiguousarray(output__BO, dtype='float32')
+        self.ops.relu(output__BO, inplace=True)
         return output__BO
 
     def begin_update(self, input__BI, drop=0.0):
         output__BO, finish_affine = Affine.begin_update(self, input__BI, drop=0.)
-
         output_copy = self.ops.xp.ascontiguousarray(output__BO, dtype='float32')
         self.ops.relu(output_copy, inplace=True)
         def finish_update(gradient, sgd=None):
@@ -19,4 +19,5 @@ class ReLu(Affine):
             self.ops.backprop_relu(gradient, output_copy, inplace=True)
             return finish_affine(gradient, sgd)
         output__BO[:] = output_copy
-        return output__BO, finish_update
+        output__BO, bp_dropout = self.ops.dropout(output__BO, drop, inplace=True)
+        return output__BO, bp_dropout(finish_update)
