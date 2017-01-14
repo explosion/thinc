227062ed2b6af318843dfefd658ab2b69d83a408
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-14 14:43:36 +0100

    Update ELU class

--- thinc/neural/_classes/elu.py
+++ thinc/neural/_classes/elu.py
@@ -4,19 +4,19 @@ from ...describe import Dimension, Synapses, Biases
 
 
 class ELU(Affine): # pragma: no cover
-    def predict(self, input__BI):
-        output__BO = Affine.predict(self, input__BI)
-        self.ops.elu(output__BO, inplace=True)
-        return output__BO
+    def predict(self, input__bi):
+        output__bo = Affine.predict(self, input__bi)
+        self.ops.elu(output__bo, inplace=True)
+        return output__bo
 
-    def begin_update(self, input__BI):
-        output__BO, finish_affine = Affine.begin_update(self, input__BI)
+    def begin_update(self, input__bi, drop=0.):
+        output__bo, finish_affine = Affine.begin_update(self, input__bi, drop=drop)
 
-        output_copy = self.ops.xp.ascontiguousarray(output__BO, dtype='f')
+        output_copy = self.ops.xp.ascontiguousarray(output__bo, dtype='f')
         self.ops.elu(output_copy, inplace=True)
-        def finish_update(gradient):
+        def finish_update(gradient, sgd=None):
             gradient = self.ops.xp.ascontiguousarray(gradient, dtype='f')
             self.ops.backprop_elu(gradient, output_copy, inplace=True)
-            return finish_affine(gradient)
-        output__BO[:] = output_copy
-        return output__BO, finish_update
+            return finish_affine(gradient, sgd)
+        output__bo[:] = output_copy
+        return output__bo, finish_update
