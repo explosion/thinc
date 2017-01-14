bab60ac0203720e5cbd465835fa1e124a68ae7cc
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-14 14:43:57 +0100

    Update feed forward class

--- thinc/neural/_classes/feed_forward.py
+++ thinc/neural/_classes/feed_forward.py
@@ -1,17 +1,21 @@
 from .model import Model
 from ... import describe
+from .. import exceptions
+
 
 def _run_child_hooks(model, X, y):
     for layer in model._layers:
         for hook in layer.on_data_hooks:
             hook(layer, X, y)
+        X = layer(X[:1000])
+
 
 @describe.on_data(_run_child_hooks)
 class FeedForward(Model):
     '''A feed-forward network, that chains multiple Model instances together.'''
     def __init__(self, layers, **kwargs):
+        self._layers = layers
         Model.__init__(self, **kwargs)
-        self._layers.extend(layers)
 
     @property
     def input_shape(self):
@@ -24,12 +28,10 @@ class FeedForward(Model):
     def begin_update(self, X, drop=0.):
         callbacks = []
         for layer in self._layers:
-            X = self.ops.xp.ascontiguousarray(X, dtype='float32')
             X, inc_layer_grad = layer.begin_update(X, drop=drop)
             callbacks.append(inc_layer_grad)
         def continue_update(gradient, sgd=None):
             for callback in reversed(callbacks):
-                gradient = self.ops.xp.ascontiguousarray(gradient, dtype='float32')
                 gradient = callback(gradient, sgd)
             return gradient
         return X, continue_update
