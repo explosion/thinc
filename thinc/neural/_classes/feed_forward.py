5d6206f42323c9fd5e4185f36e75a2466adf79e8
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-14 02:01:03 +0100

    Improve how child hooks are run in FeedForward

--- thinc/neural/_classes/feed_forward.py
+++ thinc/neural/_classes/feed_forward.py
@@ -1,31 +1,29 @@
 from .model import Model
+from ... import describe
 
+def _run_child_hooks(model, X, y):
+    for layer in model._layers:
+        for hook in layer.on_data_hooks:
+            hook(layer, X, y)
 
+@describe.on_data(_run_child_hooks)
 class FeedForward(Model):
     '''A feed-forward network, that chains multiple Model instances together.'''
     def __init__(self, layers, **kwargs):
         Model.__init__(self, **kwargs)
-        self.layers.extend(layers)
-        if self.layers:
-            nO = self.layers[0].output_shape[1]
-            for layer in self.layers[1:]:
-                if nO is not None and layer.nI is None:
-                    layer.nI = nO
-                nO = layer.nO 
+        self._layers.extend(layers)
 
     @property
     def input_shape(self):
-        return self.layers[0].input_shape
+        return self._layers[0].input_shape
 
     @property
     def output_shape(self):
-        return self.layers[-1].output_shape
+        return self._layers[-1].output_shape
 
     def begin_update(self, X, drop=0.):
         callbacks = []
         for layer in self.layers:
-            assert layer.W is not None
-            assert layer.b is not None
             X = self.ops.xp.ascontiguousarray(X, dtype='float32')
             X, inc_layer_grad = layer.begin_update(X, drop=drop)
             callbacks.append(inc_layer_grad)
