2429109bb981ac8d2a1f422a1dd2c883e9977dd4
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-14 14:22:03 +0100

    Fix batchnorm class

--- thinc/neural/_classes/batchnorm.py
+++ thinc/neural/_classes/batchnorm.py
@@ -1,35 +1,71 @@
 from .model import Model
+from ... import describe
 
 
-class ScaleShift(Model): # pragma: no cover
-    def begin_update(self, input__BI):
-        def finish_update(gradient__BI):
-            self.d.b += gradient__BI.sum(axis=0)
-            for i in range(gradient__BI.shape[0]):
-                self.d.G += gradient__BI[i] * input__BI[i]
-            return gradient__BI * self.d.G
-        return input__BI * self.d.G + self.w.b, finish_update
+def _init_to_one(W, ops):
+    W[:] = 1.
+
+def _set_dimensions_if_needed(model, X, y=None):
+    if model.nI is None:
+        model.nI = X.shape[1]
 
 
-class BatchNormalization(Model): # pragma: no cover
+@describe.on_data(_set_dimensions_if_needed)
+@describe.attributes(
+    nB=describe.Dimension("Batch size"),
+    nI=describe.Dimension("Input size"),
+    G=describe.Weights("Scaling vector", lambda obj: (obj.nI,), _init_to_one),
+    b=describe.Biases("Bias vector", lambda obj: (obj.nI,)),
+    d_G=describe.Gradient("G"),
+    d_b=describe.Gradient("b")
+)
+class BatchNorm(Model):
+    name = 'batchnorm'
+
+    @property
+    def input_shape(self):
+        return (self.nB, self.nI)
+
+    @property
+    def output_shape(self):
+        return (self.nB, self.nI)
+
+    def __init__(self, nI=None, **kwargs):
+        Model.__init__(self, **kwargs)
+        self.nI = nI
+
     def predict(self, X):
         N, mu, var = _get_moments(self.ops, X)
-        return _forward(self.ops, X, mu, var)
+        Xh = _forward(self.ops, X, mu, var)
+        y = Xh * self.G + self.b
+        return y
  
-    def begin_update(self, X):
+    def begin_update(self, X, drop=0.):
         N, mu, var = _get_moments(self.ops, X)
         Xhat = _forward(self.ops, X, mu, var)
+        y, backprop_rescale = self._begin_update_scale_shift(Xhat)
 
-        def finish_update(dy):
+        def finish_update(dy, sgd=None):
+            dy = backprop_rescale(dy, sgd)
             dist, sum_dy, sum_dy_dist = _get_d_moments(self.ops, dy, X, mu)
             d_xhat = N * dy - sum_dy - dist * var**(-1.) * sum_dy_dist
             d_xhat *= var ** (-1. / 2)
             d_xhat /= N
             return d_xhat
-        return Xhat, finish_update
+        return y, finish_update
+
+    def _begin_update_scale_shift(self, input__BI):
+        def finish_update(gradient__BI, sgd=None):
+            self.d_b += gradient__BI.sum(axis=0)
+            for i in range(gradient__BI.shape[0]):
+                self.d_G += gradient__BI[i] * input__BI[i]
+            if sgd is not None:
+                sgd(self._mem.weights, self._mem.gradient, key=id(self._mem))
+            return gradient__BI * self.G
+        return input__BI * self.G + self.b, finish_update
 
 
-def _get_moments(ops, X): # pragma: no cover
+def _get_moments(ops, X):
     if hasattr(X, 'shape') and len(X.shape) == 2:
         mu = X.mean(axis=0)
         var = X.var(axis=0) + 1e-8
@@ -39,7 +75,7 @@ def _get_moments(ops, X): # pragma: no cover
         return stacked.shape[0], stacked.mean(axis=0), stacked.var(axis=0)
 
 
-def _get_d_moments(ops, dy, X, mu): # pragma: no cover
+def _get_d_moments(ops, dy, X, mu):
     if hasattr(dy, 'shape'):
         dist = X-mu
         return dist, ops.xp.sum(dy, axis=0), ops.xp.sum(dy * dist, axis=0)
@@ -50,38 +86,8 @@ def _get_d_moments(ops, dy, X, mu): # pragma: no cover
         return dist, sum_dy, sum_dy_dot_dist
 
 
-def _forward(ops, X, mu, var): # pragma: no cover
+def _forward(ops, X, mu, var):
     if hasattr(X, 'shape'):
         return (X-mu) * var ** (-1./2.)
     else:
         return [_forward(x, mu, var) for x in X]
-
-
-#    @property
-#    def G(self):
-#        return self.params.get('G-%s' % self.name)
-#
-#    @property
-#    def b(self):
-#        return self.params.get('b-%s' % self.name)
-#
-#    @property
-#    def d_G(self):
-#        return self.params.get('d_G-%s' % self.name, require=True)
-#
-#    @property
-#    def d_b(self):
-#        return self.params.get('d_b-%s' % self.name, require=True)
-#
-#    @property
-#    def describe_params(self):
-#        '''
-#        Yields (name, shape, initializer) triples describing the weights directly
-#        owned by the layer.
-#        '''
-#        def init(G, **kwargs):
-#            G += 1
-#        yield 'G-%s' % self.name, (self.nr_out,), init
-#        yield 'b-%s' % self.name, (self.nr_out,), None
-#
-#
