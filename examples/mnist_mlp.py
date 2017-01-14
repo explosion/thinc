99991f6bdbf3560fae08d5206ffaddf520303269
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-14 17:20:55 +0100

    Update mnist example

--- examples/mnist_mlp.py
+++ examples/mnist_mlp.py
@@ -3,7 +3,7 @@ import plac
 import dill as pickle
 
 from thinc.neural.vec2vec import Model, ReLu, Softmax
-from thinc.neural._classes.batchnorm import BatchNorm
+from thinc.neural._classes.batchnorm import BatchNorm as BN
 from thinc.api import clone, chain
 from thinc.loss import categorical_crossentropy
 
@@ -12,7 +12,9 @@ from thinc.extra import datasets
 
 def main(depth=4, width=512, nb_epoch=5):
     with Model.define_operators({'**': clone, '>>': chain}):
-        model = (ReLu(width)>>BatchNorm())**depth >> Softmax()
+        model = BN(ReLu(width, 784)) \
+                >> BN(ReLu(width)) \
+                >> Softmax()
    
     train_data, dev_data, _ = datasets.mnist()
     train_X, train_y = model.ops.unzip(train_data)
@@ -21,10 +23,12 @@ def main(depth=4, width=512, nb_epoch=5):
     with model.begin_training(train_X, train_y) as (trainer, optimizer):
         trainer.each_epoch(lambda: print(model.evaluate(dev_X, dev_y)))
         trainer.nb_epoch = nb_epoch
+        trainer.dropout = 0.2
+        trainer.dropout_decay = 0.0
         for X, y in trainer.iterate(train_X, train_y):
             yh, backprop = model.begin_update(X, drop=trainer.dropout)
             d_loss, loss = categorical_crossentropy(yh, y)
-            #optimizer.set_loss(loss)
+            optimizer.set_loss(loss)
             backprop(d_loss, optimizer)
     with model.use_params(optimizer.averages):
         print('Avg dev.: %.3f' % model.evaluate(dev_X, dev_y))
