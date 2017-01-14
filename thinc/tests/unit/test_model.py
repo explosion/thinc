abbb8e8d0259782705f561e1d661e55d2c33169d
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-14 02:13:55 +0100

    Fix model tests

--- thinc/tests/unit/test_model.py
+++ thinc/tests/unit/test_model.py
@@ -3,10 +3,9 @@ from __future__ import unicode_literals
 import pytest
 from flexmock import flexmock
 from hypothesis import given, strategies
-import abc
 
-from .....neural._classes import model as base
-from .....neural.ops import NumpyOps
+from ...neural._classes import model as base
+from ...neural.ops import NumpyOps
 
 
 @pytest.fixture
@@ -79,21 +78,21 @@ def test_init_calls_hooks():
 
 
 def test_bind_plus():
-    with Model.define_operators({'+': lambda a, b: (a.name, b.name)}):
-        m = Model(name='a') + Model(name='b')
+    with base.Model.define_operators({'+': lambda a, b: (a.name, b.name)}):
+        m = base.Model(name='a') + base.Model(name='b')
         assert m == ('a', 'b')
 
 def test_plus_chain():
-    with Model.define_operators({'+': lambda a, b: a}):
-        m = Model(name='a') + Model(name='b') + Model(name='c') + Model(name='d')
+    with base.Model.define_operators({'+': lambda a, b: a}):
+        m = base.Model(name='a') + base.Model(name='b') + base.Model(name='c') + base.Model(name='d')
         assert m.name == 'a'
 
 
 @pytest.mark.parametrize('op', '+ - * @ / // % ** << >> & ^ |'.split())
 def test_all_operators(op):
-    m1 = Model(name='a')
-    m2 = Model(name='b')
-    with Model.define_operators({op: lambda a, b: a.name + b.name}):
+    m1 = base.Model(name='a')
+    m2 = base.Model(name='b')
+    with base.Model.define_operators({op: lambda a, b: a.name + b.name}):
         if op == '+':
             value = m1 + m2
         else:
@@ -168,4 +167,4 @@ def test_all_operators(op):
         else:
             with pytest.raises(TypeError):
                 value = m1 | m2
-    assert Model._operators == {}
+    assert base.Model._operators == {}
