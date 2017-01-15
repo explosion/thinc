d532eb399fdb26d9e221d06be238596ed6a7af5d
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-15 13:40:10 +0100

    Start work on better exceptions

--- thinc/exceptions.py
+++ thinc/exceptions.py
@@ -1,7 +1,30 @@
-class ShapeError(ValueError): # pragma: no cover
+import sys
+
+SHAPE_ERR_TEMPLATE = '''
+
+In the context of {context}:\n
+
+Shape1 != Shape2
+
+Where:
+
+Shape1={shape1}
+Shape2={shape2}
+'''
+
+class ShapeError(ValueError):
+    def __init__(self, shape1, shape2, context):
+        msg = SHAPE_ERR_TEMPLATE.strip().format(
+            shape1=shape1, shape2=shape2, context=context)
+        ValueError.__init__(self, msg)
+        self.tb = sys.exc_info()[2]
+
     @classmethod
-    def expected_batch(cls):
-        return cls("Expected batch")
+    def dimensions_mismatch(cls, shape1, shape2, context):
+        if shape1 == shape2:
+            return None
+        else:
+            return cls(shape1, shape2, context)
     
     @classmethod
     def dim_mismatch(cls, expected, observed):
