86056326cab3350bdd8a6b935d94f479a4f25dec
Ines Montani <ines@ines.io>
2017-01-15 23:30:18 +0100

    Update example sandbox

--- examples/check_args.py
+++ examples/check_args.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 from collections import defaultdict
-from thinc.check import ArgCheckAdder
-from thinc.exceptions import ExpectedIntError
+from thinc import check
+# from thinc.check import ArgCheckAdder
+# from thinc.exceptions import ExpectedIntError
 
 
 def check_arg(arg_id, *constraints):
@@ -16,16 +17,19 @@ def check_int_value(min=None, max=None):
     return constraint
 
 
-@check_arg(0, check_int_value(min=0))
-@check_arg(1, check_int_value(min=0))
-def add_positive_integers(int1, int2):
+#@check_arg(0, check_int_value(min=0))
+#@check_arg(1, check_int_value(min=0))
+@check.args_equal_length((0, 1))
+def add_stuff(int1, int2):
     return int1 + int2
 
 
 def main():
-    print(add_positive_integers(None, 5))
-    print(add_positive_integers(0, 5))
-    print(add_positive_integers(-1, 5))
+    print(add_stuff(None, None))
+    #print(add_stuff((1, 2), (0, 1, 2)))
+    print(add_stuff("abc", "bye"))
+    #print(add_positive_integers(0, 5))
+    #print(add_positive_integers(-1, 5))
 
 
 if __name__ == '__main__':
