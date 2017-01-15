602c01caa23df0c6dad5963412a340087012f692
Ines Montani <ines@ines.io>
2017-01-16 00:12:37 +0100

    Update test and import errors

--- thinc/tests/integration/test_shape_check.py
+++ thinc/tests/integration/test_shape_check.py
@@ -2,12 +2,13 @@ import pytest
 import numpy
 
 from ...neural._classes.model import Model
-
+from ...exceptions import UndefinedOperatorError, DifferentLengthError
+from ...exceptions import ExpectedTypeError, ShapeMismatchError
 
 
 def test_mismatched_shapes_raise_ShapeError():
     X = numpy.ones((3, 4))
     model = Model(10, 5)
-    with pytest.raises(ValueError):
-        y = model.begin_training(X)
-    
+    with pytest.raises(ShapeMismatchError):
+        y = model.begin_training(X, X)
+
