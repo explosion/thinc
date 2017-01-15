2e31e89581e0946bd6dd45aea4a45bb6f50b4d5a
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-16 00:35:12 +0100

    Update shape checking test

--- thinc/tests/integration/test_shape_check.py
+++ thinc/tests/integration/test_shape_check.py
@@ -1,14 +1,13 @@
 import pytest
 import numpy
 
-from ...neural._classes.model import Model
+from ...neural._classes.affine import Affine
 from ...exceptions import UndefinedOperatorError, DifferentLengthError
 from ...exceptions import ExpectedTypeError, ShapeMismatchError
 
 
 def test_mismatched_shapes_raise_ShapeError():
     X = numpy.ones((3, 4))
-    model = Model(10, 5)
-    with pytest.raises(ShapeMismatchError):
-        y = model.begin_training(X, X)
+    model = Affine(10, 5)
+    y = model.begin_update(X)
 
