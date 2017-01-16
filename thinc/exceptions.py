768ffa2d38dca2350e3013f09601eeef81bad4e8
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-16 18:18:26 +0100

    Tweak error message for ShapeMismatch

--- thinc/exceptions.py
+++ thinc/exceptions.py
@@ -68,7 +68,7 @@ class ShapeMismatchError(ValueError):
     def __init__(self, shape, dim, shape_names):
         self.tb = traceback.extract_stack()
         ValueError.__init__(self, get_error(
-            "Shape mismatch: {s} does not have the right dimension {d}.".format(s=shape, d=dim),
+            "Shape mismatch: input {s} not compatible with {d}.".format(s=shape, d=dim),
             tb=self.tb
         ))
 
