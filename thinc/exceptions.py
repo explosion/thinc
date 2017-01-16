265f83f6e03ae91466da9243fb54cb68bfabedfd
Ines Montani <ines@ines.io>
2017-01-16 01:26:24 +0100

    Add highlighting to user errors

--- thinc/exceptions.py
+++ thinc/exceptions.py
@@ -7,25 +7,33 @@ import traceback
 def get_error(title, *args, **kwargs):
     template = '\n\n\t\033[91m\033[1m{title}\033[0m{info}{tb}\n'
     info = '\n' + '\n'.join(['\t' + arg for arg in args]) if args else ''
-    tb = _get_traceback(kwargs['tb']) if 'tb' in kwargs else ''
+    highlight = kwargs['highlight'] if 'highlight' in kwargs else False
+    tb = _get_traceback(kwargs['tb'], highlight) if 'tb' in kwargs else ''
     return template.format(title=title, info=info, tb=tb).encode('utf8')
 
 
-def _get_traceback(tb):
+def _get_traceback(tb, highlight):
     template = '\n\n\t\033[94m\033[1m{title}:\033[0m\n\t{tb}'
     tb_range = tb[-5:-2]
-    tb_list = [_format_traceback(p, l, fn, t, i, len(tb_range)) for i, (p, l, fn, t) in enumerate(tb_range)]
+    tb_list = [_format_traceback(p, l, fn, t, i, len(tb_range), highlight) for i, (p, l, fn, t) in enumerate(tb_range)]
     tb_str = '\n'.join(tb_list).strip()
     return template.format(title='Traceback', tb=tb_str)
 
 
-def _format_traceback(path, line, fn, text, i, count):
+def _format_traceback(path, line, fn, text, i, count, highlight):
     template = '\t{i} \033[1m{fn}\033[0m [{l}] in \033[4m{p}\033[0m{t}'
-    template_text = '\n\t{sp} \033[91m>>>\033[0m {t}'
     indent = ('└─' if i == count-1 else '├─') + '──'*i
     filename = path.rsplit('/thinc/', 1)[1] if '/thinc/' in path else path
-    text = template_text.format(sp='   '*i, t=text) if i == count-1 else ''
+    text = _format_user_error(text, i, highlight) if i == count-1 else ''
     return template.format(l=str(line), fn=fn, p=filename, i=indent, t=text)
+
+
+def _format_user_error(text, i, highlight):
+    template = '\n\t{sp} \033[91m>>>\033[0m {t}'
+    if highlight:
+        template_h = '\033[93m{t}\033[0m'.format(t=str(highlight))
+        text = text.replace(str(highlight), template_h)
+    return template.format(sp='   '*i, t=text)
 
 
 class UndefinedOperatorError(TypeError):
@@ -35,7 +43,8 @@ class UndefinedOperatorError(TypeError):
             "Undefined operator: {op}".format(op=op),
             "Called by ({arg1}, {arg2})".format(arg1=arg1, arg2=arg2),
             "Available: {ops}".format(ops= ', '.join(operators.keys())),
-            tb=self.tb
+            tb=self.tb,
+            highlight=op
         ))
 
 
@@ -63,7 +72,8 @@ class ExpectedTypeError(TypeError):
         self.tb = traceback.extract_stack()
         TypeError.__init__(self, get_error(
             "Expected type {e}, but got: {v} ({t})".format(e='/'.join(expected), v=bad_type, t=type(bad_type)),
-            tb=self.tb
+            tb=self.tb,
+            highlight=bad_type
         ))
 
 
