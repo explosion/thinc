4facd05fbff9d3dbe37113379a76603e5f8a625e
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-17 14:05:58 +0100

    Remove old test file

--- thinc/tests/unit/WindowEncode/test_init.py
+++ thinc/tests/unit/WindowEncode/test_init.py
@@ -1,22 +1,32 @@
 import pytest
 
 from ....neural._classes.window_encode import MaxoutWindowEncode
+from ....neural._classes.embed import Embed
 from ....neural.ops import NumpyOps
 
+@pytest.fixture
+def ndim():
+    return 10
 
-@pytest.mark.xfail
+@pytest.fixture
+def embed():
+    return Embed(ndim)
+
+@pytest.fixture
+def model():
+    return MaxoutWindowEncode(embed, 10)
+
 def test_init_succeeds():
-    model = MaxoutWindowEncode(10)
+    model = MaxoutWindowEncode(embed, 10)
 
 
-@pytest.mark.xfail
-def test_init_defaults_and_overrides():
-    model = MaxoutWindowEncode(10)
-    assert model.nr_piece == MaxoutWindowEncode.nr_piece
-    assert model.nr_feat == MaxoutWindowEncode.nr_feat
-    assert model.nr_out == 10
-    assert model.nr_in == None
-    model = MaxoutWindowEncode(10, nr_piece=MaxoutWindowEncode.nr_piece-1)
-    assert model.nr_piece == MaxoutWindowEncode.nr_piece-1
-    model = MaxoutWindowEncode(10, nr_feat=MaxoutWindowEncode.nr_feat-1)
-    assert model.nr_feat == MaxoutWindowEncode.nr_feat-1
+def test_init_defaults_and_overrides(model):
+    model = MaxoutWindowEncode(Embed(10, 10), 10)
+    assert model.nP == MaxoutWindowEncode.nP
+    assert model.nW == MaxoutWindowEncode.nW
+    assert model.nO == 10
+    assert model.nI == None
+    model = MaxoutWindowEncode(Embed(), 10, nP=MaxoutWindowEncode.nP-1)
+    assert model.nP == MaxoutWindowEncode.nP-1
+    model = MaxoutWindowEncode(10, nW=MaxoutWindowEncode.nF-1)
+    assert model.nF == MaxoutWindowEncode.nF-1
