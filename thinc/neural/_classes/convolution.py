0824bfd48692d3ca7711171c0dea6868411db4ce
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-21 13:18:51 +0100

    Fix window extraction convolution

--- thinc/neural/_classes/convolution.py
+++ thinc/neural/_classes/convolution.py
@@ -22,41 +22,33 @@ class ExtractWindow(Model):
         nr_feat = self.nW * 2 + 1
         shape = (X.shape[0], nr_feat) + X.shape[1:]
         output = self.ops.allocate(shape)
-        # Let L(w[i]) LL(w[i]), R(w[i]), RR(w[i])
-        # denote the words one and two to the left and right of word i.
-        # So L(w[i]) == w[i-1] for i >= 1 and {empty} otherwise.
-        # output[i, n-1] will be L(w[i]) so w[i-1] or empty
-        # output[i, n-2] will be LL(w[i]) so w[i-2] or empty
-        # etc
-        for i in range(self.nW):
-            output[i+1:, i] = X[:-(i+1)]
+        for i in range(1, self.nW+1):
+            output[:-i, self.nW-i] = X[i:]
         # output[i, n] will be w[i]
         output[:, self.nW] = X
-        # Now R(w[i]), RR(w[i]) etc
         for i in range(1, self.nW+1):
-            output[:-i, self.nW + i] = X[:-i]
+            output[i:, self.nW + i] = X[:-i]
         return output.reshape(shape[0], self.ops.xp.prod(shape[1:]))
 
-    def begin_update(self, X, drop=0.0):
-        output = self.predict(X)
-        output, bp_dropout = self.ops.dropout(output, drop)
+    def begin_update(self, X__bi, drop=0.0):
+        X__bo = self.predict(X__bi)
+        X__bo, bp_dropout = self.ops.dropout(X__bo, drop)
+        finish_update = self._get_finish_update()
+        return X__bo, bp_dropout(finish_update)
+    
+    def _get_finish_update(self):
+        return lambda d, sgd=None: backprop_concatenate(self.ops, d, self.nW)
 
-        def finish_update(gradient, sgd=None, **kwargs):
-            nr_feat = self.nW * 2 + 1
-            shape = (gradient.shape[0], nr_feat, int(gradient.shape[-1] / nr_feat))
-            gradient = gradient.reshape(shape)
-            output = self.ops.allocate((gradient.shape[0], gradient.shape[-1]))
-            # Word w[i+1] is the R feature of w[i]. So
-            # grad(w[i+1]) += grad(R[i, n+1])
-            # output[:-1] += gradient[1:, self.nW+1] 
-            for i in range(1, self.nW+1): # As rightward features
-                output[:-i] += gradient[i:, self.nW+i]
-            # Word w[i-1] is the L feature of w[i]. So
-            # grad(w[i-1]) += grad(L[i, n-1])
-            # output[1:] += gradient[:-1, self.nW-1] 
-            for i in range(1, self.nW+1): # As leftward features 
-                output[i:] += gradient[:-i, self.nW-i]
-            # Central column of gradient is the word's own feature
-            output += gradient[:, self.nW, :]
-            return output
-        return output, bp_dropout(finish_update)
+
+def backprop_concatenate(ops, dY__bo, nW):
+    nr_feat = nW * 2 + 1
+    bfi = (dY__bo.shape[0], nr_feat, int(dY__bo.shape[-1] / nr_feat))
+    dY__bfi = dY__bo.reshape(bfi)
+    dX__bi = ops.allocate((dY__bo.shape[0], bfi[-1]))
+    for f in range(1, nW+1):
+        dX__bi[f:] += dY__bfi[:-f, nW-f] # Words at start not used as rightward feats
+    dX__bi += dY__bfi[:, nW]
+    for f in range(1, nW+1):
+        dX__bi[:-f] += dY__bfi[f:, nW+f] # Words at end not used as leftward feats
+    return dX__bi
+
