05de65933792140703bcdd14f5c7f7239251e1b1
Ines Montani <ines@ines.io>
2017-01-24 15:02:17 +0100

    Add test for loss function

---
+++ thinc/tests/unit/test_loss.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+import pytest
+from mock import MagicMock
+from numpy import ndarray
+
+from ...loss import categorical_crossentropy
+
+
+@pytest.mark.parametrize('shape,labels', [([100, 100, 100], [-1, -1, -1])])
+def test_loss(shape, labels):
+    scores = MagicMock(spec=ndarray, shape=shape)
+    loss = categorical_crossentropy(scores, labels)
+    assert len(loss) == 2
