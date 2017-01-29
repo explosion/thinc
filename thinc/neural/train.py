c4a47aa58f6e036eb59ff9a280a4e8a9d2c87789
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-29 16:46:01 -0600

    Improve slicing for GPU in train

--- thinc/neural/train.py
+++ thinc/neural/train.py
@@ -11,7 +11,8 @@ class Trainer(object):
     def __init__(self, model, train_data, L2=0.0):
         self.ops = model.ops
         self.model = model
-        self.optimizer = Eve(Adam(model.ops, 0.001))
+        #self.optimizer = Eve(Adam(model.ops, 0.001))
+        self.optimizer = Eve(SGD(model.ops, 0.0001, momentum=0.9))
         self.batch_size = 128
         self.nb_epoch = 1
         self.i = 0
@@ -48,6 +49,7 @@ class Trainer(object):
 
 
 def _take_slice(data, slice_):
-    x = [data[i] for i in slice_]
-    return x
+    return data[slice_]
+    #x = [data[int(i)] for i in slice_]
+    #return x
 
