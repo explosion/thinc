df6fdf1a7a1f356ba06ef5641deb24861e2db77d
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-30 06:49:45 -0600

    Clean up inplace ops in relu

--- thinc/neural/_classes/relu.py
+++ thinc/neural/_classes/relu.py
@@ -9,20 +9,16 @@ class ReLu(Affine):
     @check.arg(1, has_shape(('nB', 'nI')))
     def predict(self, input__BI):
         output__BO = Affine.predict(self, input__BI)
-        output__BO = output__BO.copy()
-        output__BO = self.ops.relu(output__BO, inplace=True)
+        output__BO = self.ops.relu(output__BO, inplace=False)
         return output__BO
 
     @check.arg(1, has_shape(('nB', 'nI')))
     def begin_update(self, input__BI, drop=0.0):
         output__BO, finish_affine = Affine.begin_update(self, input__BI, drop=0.)
-        output_copy = output__BO.copy()
-        output_copy = self.ops.relu(output_copy, inplace=True)
+        output__BO = self.ops.relu(output__BO)
         @check.arg(0, has_shape(('nB', 'nO')))
         def finish_update(gradient, sgd=None):
-            gradient = gradient.copy()
-            gradient = self.ops.backprop_relu(gradient, output_copy, inplace=True)
+            gradient = self.ops.backprop_relu(gradient, output__BO)
             return finish_affine(gradient, sgd)
-        output__BO[:] = output_copy
-        output__BO, bp_dropout = self.ops.dropout(output__BO, drop, inplace=True)
-        return output__BO, bp_dropout(finish_update)
+        dropped, bp_dropout = self.ops.dropout(output__BO, drop, inplace=False)
+        return dropped, bp_dropout(finish_update)
