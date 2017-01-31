39cea46fe25a004d9b9a7e025a63be198d87c0ca
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-31 11:24:24 -0600

    Move remap_ids function to util

--- thinc/neural/util.py
+++ thinc/neural/util.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, unicode_literals
 import numpy
+from preshed.maps import PreshMap
 from .ops import NumpyOps, CupyOps
 
 
@@ -20,6 +21,24 @@ def mark_sentence_boundaries(sequences, drop=0.): # pragma: no cover
         sequence.append('-EOL-')
         sequence.append('-EOL-')
     return sequences, None
+
+
+def remap_ids(ops):
+    id_map = PreshMap()
+    def begin_update(ids, drop=0.):
+        return ops.remap_ids(id_map, ids, len(id_map)), None
+    return begin_update
+
+#    def _unique_ids(self, ids):
+#        id_map = {}
+#        for i, id_ in enumerate(ids.flatten()):
+#            if id_ not in id_map:
+#                id_map[id_] = [i]
+#            else:
+#                id_map[id_].append(i)
+#        # Currently this is handled on CPU anyway, so allocate on CPU.
+#        uniques = numpy.asarray(sorted(id_map.keys()), dtype='uint64')
+#        return uniques, id_map
 
 
 def flatten_sequences(sequences, drop=0.): # pragma: no cover
