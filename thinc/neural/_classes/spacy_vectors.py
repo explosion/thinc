8460d017551905119076989d753867835f3488ea
Matthew Honnibal <honnibal+gh@gmail.com>
2017-02-12 20:17:01 -0600

    Load vectors. not NLP object

--- thinc/neural/_classes/spacy_vectors.py
+++ thinc/neural/_classes/spacy_vectors.py
@@ -6,7 +6,7 @@ from .._lsuv import LSUVinit
 from ..ops import NumpyOps
 from ...api import layerize
 from .model import Model
-from ...extra.load_nlp import get_spacy
+from ...extra.load_nlp import get_vectors
 
 try:
     import cupy
@@ -20,11 +20,11 @@ def get_word_ids(docs, drop=0.):
     seqs = []
     ops = Model.ops
     for doc in docs:
-        arr = ops.xp.zeros((len(doc)+1,), dtype='uint64')
+        arr = numpy.zeros((len(doc)+1,), dtype='uint64')
         for token in doc:
-            arr[token.i] = token.orth
+            arr[token.i] = token.lex_id
         arr[len(doc)] = 0
-        seqs.append(arr)
+        seqs.append(ops.asarray(arr))
     return seqs, None
 
 
@@ -40,49 +40,26 @@ def get_word_ids(docs, drop=0.):
         d_W=Gradient("W"),
 )
 class SpacyVectors(Model):
-    ops = NumpyOps()
     name = 'spacy-vectors'
     def __init__(self, lang, nO):
         Model.__init__(self)
-        self._id_map = {0: 0}
         self.nO = nO
         # This doesn't seem the cleverest solution,
         # but it ensures multiple models load the
         # same copy of spaCy if they're deserialised.
-        nlp = get_spacy(lang)
+        vectors = get_vectors(self.ops, lang)
         self.lang = lang
-        self.nM = nlp.vocab.vectors_length
-
-    @property
-    def nV(self):
-        nlp = get_spacy(self.lang)
-        return len(nlp.vocab)
+        self.nM = vectors.shape[1]
+        self.nV = vectors.shape[0]
 
     def begin_update(self, ids, drop=0.):
-        if not isinstance(ids, numpy.ndarray):
-            ids = ids.get()
-            gpu_in = True
-        else:
-            gpu_in = False
-        nlp = get_spacy(self.lang)
-        uniqs, inverse = numpy.unique(ids, return_inverse=True)
-        vectors = self.ops.allocate((uniqs.shape[0], self.nM))
-        for i, orth in enumerate(uniqs):
-            lex = nlp.vocab[orth]
-            if lex.vector_norm:
-                vectors[i] = lex.vector / lex.vector_norm
+        vector_table = get_vectors(self.ops, self.lang)
+        ids *= ids < vector_table.shape[0]
+        vectors = vector_table[ids]
         def finish_update(gradients, sgd=None):
-            if gpu_in:
-                gradients = gradients.get()
-            self.d_W += self.ops.batch_outer(gradients, vectors[inverse, ])
+            self.d_W += self.ops.batch_outer(gradients, vectors)
             if sgd is not None:
-                ops = sgd.ops
-                sgd.ops = self.ops
                 sgd(self._mem.weights, self._mem.gradient, key=self.id)
-                sgd.ops = ops
             return None
         dotted = self.ops.batch_dot(vectors, self.W)
-        if gpu_in:
-            return cupy.asarray(dotted[inverse, ]), finish_update
-        else:
-            return dotted[inverse, ], finish_update
+        return dotted, finish_update
