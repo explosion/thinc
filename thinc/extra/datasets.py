9701461ed63ec77047ea72e08159dadcfd3b7374
Matthew Honnibal <honnibal@gmail.com>
2017-02-13 03:52:32 -0600

    Use 2 labels for SNLI data by default

--- thinc/extra/datasets.py
+++ thinc/extra/datasets.py
@@ -148,19 +148,21 @@ def quora_questions(loc=None):
     return train, dev
 
 
-LABELS = {'entailment': 0, 'contradiction': 1, 'neutral': 2}
-def snli(loc=None):
+THREE_LABELS = {'entailment': 2, 'contradiction': 1, 'neutral': 0}
+TWO_LABELS = {'entailment': 2, 'contradiction': 0, 'neutral': 0}
+def snli(loc=None, ternary=False):
+    label_scheme = THREE_LABELS if ternary else TWO_LABELS
     if loc is None:
         loc = get_file('snli_1.0', SNLI_URL, unzip=True)
     if isinstance(loc, basestring):
         loc = Path(loc)
- 
-    train = read_snli(Path(loc) / 'snli_1.0_train.jsonl')
-    dev = read_snli(Path(loc) / 'snli_1.0_dev.jsonl')
+
+    train = read_snli(Path(loc) / 'snli_1.0_train.jsonl', label_scheme)
+    dev = read_snli(Path(loc) / 'snli_1.0_dev.jsonl', label_scheme)
     return train, dev
 
 
-def read_snli(loc):
+def read_snli(loc, label_scheme):
     rows = []
     with loc.open() as file_:
         for line in file_:
@@ -168,7 +170,7 @@ def read_snli(loc):
             label = eg['gold_label']
             if label == '-':
                 continue
-            rows.append(((eg['sentence1'], eg['sentence2']), LABELS[label]))
+            rows.append(((eg['sentence1'], eg['sentence2']), label_scheme[label]))
     return rows
 
 
