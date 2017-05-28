07b8596a8cfc2a48f4e4e0b4c147c61d9182d440
Matthew Honnibal <honnibal+gh@gmail.com>
2017-05-28 12:35:41 -0500

    Fix residual indentation

--- thinc/neural/_classes/resnet.py
+++ thinc/neural/_classes/resnet.py
@@ -6,19 +6,19 @@ import cytoolz as toolz
 
 
 def Residual(layer):
-	def forward(X, drop=0.):
-		y, bp_y = layer.begin_update(X, drop=drop)
-		output = X+y
-		def backward(d_output, sgd=None):
-			return d_output + bp_y(d_output, sgd)
-		return output, backward
-	model = layerize(forward)
-	model._layers.append(layer)
-	def on_data(self, X, y=None):
-		for layer in self._layers:
-			for hook in layer.on_data_hooks:
-				hook(layer, X, y)
+    def residual_fwd(X, drop=0.):
+        y, bp_y = layer.begin_update(X, drop=drop)
+        output = X+y
+        def residual_bwd(d_output, sgd=None):
+            return d_output + bp_y(d_output, sgd)
+        return output, backward
+    model = layerize(residual_fwd)
+    model._layers.append(layer)
+    def on_data(self, X, y=None):
+        for layer in self._layers:
+            for hook in layer.on_data_hooks:
+                hook(layer, X, y)
             if hasattr(layer, 'W'):
                 layer.W.fill(0)
-	model.on_data_hooks.append(on_data)
-	return model
+    model.on_data_hooks.append(on_data)
+    return model
