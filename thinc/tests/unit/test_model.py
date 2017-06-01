b772e06cc8b96128bc48a07f005e48b2277765ad
Matthew Honnibal <honnibal+gh@gmail.com>
2017-06-01 06:03:14 -0500

    Add commented-out tests for model loading

--- thinc/tests/unit/test_model.py
+++ thinc/tests/unit/test_model.py
@@ -85,6 +85,43 @@ def test_use_device():
     assert id(base.Model.ops) == dev_id
 
 
+
+# TODO: These were implemented into spaCy.
+# Port them over to work here, now that the
+# functions are owned by Model
+#def test_simple_model_roundtrip_bytes():
+#    model = Maxout(5, 10, pieces=2)
+#    model.b += 1
+#    data = model_to_bytes(model)
+#    model.b -= 1
+#    model_from_bytes(model, data)
+#    assert model.b[0, 0] == 1
+#
+#
+#def test_multi_model_roundtrip_bytes():
+#    model = chain(Maxout(5, 10, pieces=2), Maxout(2, 3))
+#    model._layers[0].b += 1
+#    model._layers[1].b += 2
+#    data = model_to_bytes(model)
+#    model._layers[0].b -= 1
+#    model._layers[1].b -= 2
+#    model_from_bytes(model, data)
+#    assert model._layers[0].b[0, 0] == 1
+#    assert model._layers[1].b[0, 0] == 2
+#
+#
+#def test_multi_model_load_missing_dims():
+#    model = chain(Maxout(5, 10, pieces=2), Maxout(2, 3))
+#    model._layers[0].b += 1
+#    model._layers[1].b += 2
+#    data = model_to_bytes(model)
+#
+#    model2 = chain(Maxout(5), Maxout())
+#    model_from_bytes(model2, data)
+#    assert model2._layers[0].b[0, 0] == 1
+#    assert model2._layers[1].b[0, 0] == 2
+
+
 def test_bind_plus():
     with base.Model.define_operators({'+': lambda a, b: (a.name, b.name)}):
         m = base.Model(name='a') + base.Model(name='b')
