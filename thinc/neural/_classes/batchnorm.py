c0a0bc0d90f204e24a94224463e09a0dd012554b
Matthew Honnibal <honnibal+gh@gmail.com>
2017-06-07 05:24:49 -0500

    Improve batch norm with 'renormalization' from Ioffe (2017)

--- thinc/neural/_classes/batchnorm.py
+++ thinc/neural/_classes/batchnorm.py
@@ -16,6 +16,10 @@ def _run_child_hooks(model, X, y=None):
         lambda obj: (obj.nO,), _init_to_one),
     b=describe.Biases("Bias vector",
         lambda obj: (obj.nO,)),
+    m=describe.Moment("EMA of mean",
+        lambda obj: (1, obj.nO)),
+    v=describe.Moment("EMA of variance",
+        lambda obj: (1, obj.nO)),
     d_G=describe.Gradient("G"),
     d_b=describe.Gradient("b")
 )
@@ -29,19 +33,44 @@ class BatchNorm(Model):
             self.nO = kwargs['nO']
         elif getattr(child, 'nO', None):
             self.nO = child.nO
+        self.nr_upd = 0
         Model.__init__(self, **kwargs)
 
     def predict(self, X):
         X = self.child.predict(X)
-        N, mu, var = _get_moments(self.ops, X)
-        Xh = _forward(self.ops, X, mu, var)
+        if self.nr_upd < 10:
+            N, mu, var = _get_moments(self.ops, X)
+            Xh = _forward(self.ops, X, mu, var)
+        else:
+            Xh = _forward(self.ops, X, self.m, self.v+1e-08)
         y = Xh * self.G + self.b
         return y
 
     def begin_update(self, X, drop=0.):
         X, backprop_child = self.child.begin_update(X, drop=0.)
         N, mu, var = _get_moments(self.ops, X)
+
+        self.nr_upd += 1
+        #alpha = (1. + self.nr_upd) / (10. + self.nr_upd)
+        #alpha = min(0.9, alpha)
+        alpha = 0.01
+        
+        # I'm not sure this is the best thing to do -- 
+        # Here we make a running estimate of the mean and variance,
+        # Should we consider a sample be the instance, or the batch?
+        diff = X - self.m
+        incr = (1-alpha) * diff
+        self.m += incr.mean(axis=0)
+        self.v += (diff * incr).mean(axis=0)
+        self.v *= alpha
+
         Xhat = _forward(self.ops, X, mu, var)
+        
+        # Batch "renormalization"
+        if self.nr_upd >= 50000:
+            Xhat *= var / (self.v+1e-08)
+            Xhat += (mu - self.m) / (self.v+1e-08)
+
         y, backprop_rescale = self._begin_update_scale_shift(Xhat)
 
         def finish_update(dy, sgd=None):
@@ -51,8 +80,8 @@ class BatchNorm(Model):
             d_xhat *= var ** (-1. / 2)
             d_xhat /= N
             return backprop_child(d_xhat, sgd)
-        #drop *= getattr(self.child, 'drop_factor', 1.0)
-        #y, bp_dropout = self.ops.dropout(y, drop)
+        drop *= getattr(self.child, 'drop_factor', 1.0)
+        y, bp_dropout = self.ops.dropout(y, drop)
         return y, finish_update
 
     def _begin_update_scale_shift(self, input__BI):
@@ -68,7 +97,7 @@ class BatchNorm(Model):
 
 def _get_moments(ops, X):
     mu = X.mean(axis=0)
-    var = X.var(axis=0) + 1e-8
+    var = X.var(axis=0) + 1e-08
     return X.shape[0], mu, var
 
 
