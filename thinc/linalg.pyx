185d380b53bdfb636057b825f8cf84bc4afe872f
Matthew Honnibal <honnibal+gh@gmail.com>
2017-09-29 19:54:56 +0200

    Add Python hook for variance calculation

--- thinc/linalg.pyx
+++ thinc/linalg.pyx
@@ -1,2 +1,7 @@
 # This executes the code in the module, initialising blis
-#import blis.blis
+import blis.blis
+cimport numpy as np
+
+def variance(float[::1] X):
+    return Vec.variance(&X[0], X.shape[0])
+
