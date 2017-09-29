Fix bug in layernorm gradient

thinc/neural/_classes/layernorm.py
@@ -74,7 +74,7 @@ class LayerNorm(Model):
 def _get_moments(ops, X):
     mu = X.mean(axis=1, keepdims=True)
     var = X.var(axis=1, keepdims=True) + 1e-08
-    return ops.asarray([X.shape[0]], dtype='f'), mu, var
+    return ops.asarray([X.shape[1]], dtype='f'), mu, var
 
 
 def _get_d_moments(ops, dy, X, mu):