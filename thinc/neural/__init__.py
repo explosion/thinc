767131c3addcc182b8b0c73a977df3540b282500
Matthew Honnibal <honnibal+gh@gmail.com>
2017-10-03 18:37:00 +0200

    Remove problematic imports in __init__ -- causing cycles and weirdness.

--- thinc/neural/__init__.py
+++ thinc/neural/__init__.py
@@ -6,11 +6,7 @@ from ._classes.softmax import Softmax
 from ._classes.elu import ELU
 from ._classes.maxout import Maxout
 
-from ._classes.embed import Embed
-from ._classes.static_vectors import StaticVectors
-#from ._classes.embed import HashEmbed
-
-#from .pooling import Pooling, mean_pool, max_pool
+from .pooling import Pooling, mean_pool, max_pool
 from ._classes.convolution import ExtractWindow
-#from ._classes.batchnorm import BatchNorm
-#from ._classes.difference import Siamese
+from ._classes.batchnorm import BatchNorm
+from ._classes.difference import Siamese
