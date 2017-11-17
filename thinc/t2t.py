5b1a1a9b9623b3bc9a7b0e08cafb7309850744e8
Matthew Honnibal <honnibal+gh@gmail.com>
2017-11-17 20:06:01 +0100

    Don't import 'fast' maxout CNN

--- thinc/t2t.py
+++ thinc/t2t.py
@@ -1,4 +1,3 @@
 from .neural._classes.convolution import ExtractWindow
 from .neural._classes.attention import ParametricAttention
 from .neural._classes.rnn import RNN, LSTM, BiLSTM
-from .neural._fast_maxout_cnn import MaxoutWindowEncoder
