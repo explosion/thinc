31885895520ea5342ebac859a824bae4df9146dc
Matthew Honnibal <honnibal+gh@gmail.com>
2017-11-25 01:16:51 +0000

    Update text classifier example. Gets to 92.2

--- examples/imdb_cnn.py
+++ examples/imdb_cnn.py
@@ -1,26 +1,27 @@
 from __future__ import print_function
 import tqdm
+import pickle
 
+from thinc.i2v import StaticVectors, HashEmbed
 from thinc.v2v import Model, SELU, ReLu, Maxout, Softmax, Affine
 from thinc.t2t import ExtractWindow
+from thinc.t2t import ParametricAttention
+from thinc.t2v import Pooling, sum_pool, max_pool, mean_pool
 from thinc.misc import BatchNorm as BN
+from thinc.misc import LayerNorm as LN
 from thinc.misc import Residual
 
-from thinc.t2t import ParametricAttention
-
-from thinc.neural.pooling import Pooling, sum_pool, max_pool, mean_pool
 from thinc.extra import datasets
 from thinc.neural.util import to_categorical
-from thinc.neural._classes.hash_embed import HashEmbed
 from thinc.api import layerize, chain, concatenate, clone
 from thinc.api import foreach, foreach_sentence, uniqued
 from thinc.api import layerize, with_flatten, flatten_add_lengths, with_getitem
 from thinc.api import FeatureExtracter
 import spacy
-from spacy.attrs import ORTH, LOWER, SHAPE, PREFIX, SUFFIX
+from spacy.attrs import ORTH, LOWER, SHAPE, PREFIX, SUFFIX, ID
 
-from thinc.extra.hpbff import BestFirstFinder, train_epoch
 from thinc.neural.ops import CupyOps
+from spacy.util import compounding
 
 
 @layerize
@@ -33,21 +34,22 @@ def build_model(nr_class, width, depth, conv_depth, **kwargs):
     with Model.define_operators({'|': concatenate, '>>': chain, '**': clone}):
         embed = (
             (HashEmbed(width, 5000, column=1)
+            | StaticVectors('spacy_pretrained_vectors', width, column=5)
             | HashEmbed(width//2, 750, column=2)
             | HashEmbed(width//2, 750, column=3)
             | HashEmbed(width//2, 750, column=4))
-            >> Maxout(width)
+            >> LN(Maxout(width))
         )
 
         sent2vec = (
             flatten_add_lengths
             >> with_getitem(0,
                 embed
-                >> Residual(ExtractWindow(nW=1) >> SELU(width)) ** conv_depth
+                >> Residual(ExtractWindow(nW=1) >> LN(Maxout(width))) ** conv_depth
             )
             >> ParametricAttention(width)
             >> Pooling(sum_pool)
-            >> Residual(SELU(width)) ** depth
+            >> Residual(LN(Maxout(width))) ** depth
         )
 
         model = (
@@ -55,27 +57,14 @@ def build_model(nr_class, width, depth, conv_depth, **kwargs):
             >> flatten_add_lengths
             >> ParametricAttention(width, hard=False)
             >> Pooling(sum_pool)
-            >> Residual(SELU(width)) ** depth
+            >> Residual(LN(Maxout(width))) ** depth
             >> Softmax(nr_class)
         )
     model.lsuv = False
     return model
 
 
-def simple_train(nlp, model_data, train_X, train_y, dev_X, dev_y):
-    model, sgd, hp = model_data
-    for i in range(10):
-        _, ((model, sgd, hp), train_acc, dev_acc) = train_epoch(model, sgd, hp,
-                                                        train_X, train_y,
-                                                        dev_X, dev_y,
-                                                        device_id=0)
-        print(train_acc, dev_acc)
-    with model.use_params(optimizer.averages):
-        dev_acc = model.evaluate(dev_X, dev_y)
-    return (model, optimizer, hp), train_acc, dev_acc
-
-
-def main(use_gpu=True):
+def main(use_gpu=True, nb_epoch=20):
     if use_gpu:
         Model.ops = CupyOps()
         Model.Ops = CupyOps
@@ -83,19 +72,18 @@ def main(use_gpu=True):
     print("Load data")
     train_X, train_y = zip(*train)
     test_X, test_y = zip(*test)
+    #train_X = train_X[:2000]
+    #test_X = test_X[:2000]
     train_y = Model.ops.asarray(to_categorical(train_y, nb_classes=2))
     test_y = Model.ops.asarray(to_categorical(test_y, nb_classes=2))
     
-    #train_X = train_X[:2000]
-    #test_X = test_X[:2000]
-
     nlp = spacy.load('en_vectors_web_lg')
     nlp.add_pipe(nlp.create_pipe('sentencizer'), first=True)
     nlp.vocab.lex_attr_getters[PREFIX] = lambda string: string[:3]
     for word in nlp.vocab:
         word.prefix_ = word.orth_[:3]
 
-    preprocessor = FeatureExtracter([ORTH, LOWER, PREFIX, SUFFIX, SHAPE])
+    preprocessor = FeatureExtracter([ORTH, LOWER, PREFIX, SUFFIX, SHAPE, ID])
     train_X = [preprocessor(list(doc.sents)) for doc in tqdm.tqdm(nlp.pipe(train_X))]
     test_X = [preprocessor(list(doc.sents)) for doc in tqdm.tqdm(nlp.pipe(test_X))]
 
@@ -107,59 +95,28 @@ def main(use_gpu=True):
     n_sent = sum([len(list(sents)) for sents in train_X])
     print("%d sentences" % n_sent)
 
-    hpsearch = BestFirstFinder(
-                 nonlin=[SELU],
-                 width=[64],
-                 depth=[2],
-                 conv_depth=[2],
-                 batch_size=[512],
-                 learn_rate=[0.001],
-                 L2=[1e-6],
-                 beta1=[0.9],
-                 beta2=[0.999],
-                 dropout=[0.2],
-                 nr_update=[len(train_X)//128])
-
-    for hp in hpsearch.configs:
-        for _ in range(3):
-            model = build_model(2, train_X=train_X, train_y=train_y, **hp)
-            with model.begin_training(train_X[:100], train_y[:100]) as (_, sgd):
-                pass
-            _, (model_data, train_acc, dev_acc) = train_epoch(model, sgd, hp,
-                                                    train_X, train_y,
-                                                    dev_X, dev_y,
-                                                    device_id=-1 if not use_gpu else 0)
-            print('0', dev_acc*100, train_acc*100, hp)
-            hpsearch.enqueue(model_data, train_acc, dev_acc)
-            hpsearch.temperature = 0.0
-    print("Train")
-    total = 0
-    temperature = 0.0
-    while True:
-        for model, sgd, hp in hpsearch:
-            _, (new_model, train_acc, dev_acc)  = train_epoch(model, sgd, hp,
-                                                    train_X, train_y, dev_X, dev_y,
-                                                    device_id=-1 if not use_gpu else 0,
-                                                    temperature=hpsearch.temperature)
-        hp = new_model[-1]
-        print('%d,%d,%d:\t%.2f\t%.2f\t%.2f\t%d\t%.2f\t%.3f\t%d\t%d\t%.3f\t%.3f\t%.3f' % (
-            total,
-            hp['epochs'],
-            hp['parent'],
-            hpsearch.best_acc * 100,
-            dev_acc * 100,
-            train_acc * 100,
-            int(hp['batch_size']),
-            hp['dropout'],
-            hp['learn_rate'],
-            hp['width'],
-            hp['depth'],
-            hpsearch.temperature,
-            hpsearch.queue[0][0],
-            hpsearch.queue[-1][0]
-        ))
-        total += 1
-        hpsearch.enqueue(new_model, train_acc, dev_acc)
+    model = build_model(2, width=128, conv_depth=2, depth=2,
+                        train_X=train_X, train_y=train_y)
+    with model.begin_training(train_X[:100], train_y[:100]) as (trainer, optimizer):
+        epoch_loss = [0.]
+        def report_progress():
+            with model.use_params(optimizer.averages):
+                print(epoch_loss[-1], model.evaluate(dev_X, dev_y), trainer.dropout)
+            epoch_loss.append(0.)
+        trainer.each_epoch.append(report_progress)
+        batch_sizes = compounding(4, 1024, 1.001)
+        trainer.dropout = 0.3
+        trainer.batch_size = next(batch_sizes)
+        trainer.dropout_decay = 0.0
+        optimizer.learn_rate = 0.001
+        for X, y in trainer.iterate(train_X, train_y):
+            yh, backprop = model.begin_update(X, drop=trainer.dropout)
+            loss = ((yh-y)**2.).sum() / y.shape[0]
+            backprop((yh-y)/yh.shape[0], optimizer)
+            epoch_loss[-1] += loss
+            trainer.batch_size = next(batch_sizes)
+        with model.use_params(optimizer.averages):
+            print('Avg dev.: %.3f' % model.evaluate(dev_X, dev_y))
 
 
 if __name__ == '__main__':
