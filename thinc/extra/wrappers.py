31f7f9c33730fb68b7e7addb6ee6ccf2c3e94970
Matthew Honnibal <honnibal+gh@gmail.com>
2018-03-14 23:49:24 +0100

    Work on PyTorch wrapper

--- thinc/extra/wrappers.py
+++ thinc/extra/wrappers.py
@@ -1,14 +1,16 @@
+import contextlib
 from ..compat import BytesIO
 from ..neural._classes.model import Model
 
 try:
     import torch.autograd
+    import torch.optim
     import torch
 except ImportError:
     pass
 
 
-class PytorchWrapper(Model):
+class PyTorchWrapper(Model):
     '''Wrap a PyTorch model, so that it has the same API as Thinc models.
     To optimize the model, you'll need to create a PyTorch optimizer and call
     optimizer.step() after each batch --- see examples/wrap_pytorch.py
@@ -16,6 +18,7 @@ class PytorchWrapper(Model):
     def __init__(self, model):
         Model.__init__(self)
         self._model = model
+        self._optimizer = None
 
     def begin_update(self, x_data, drop=0.):
         '''Return the output of the wrapped PyTorch model for the given input,
@@ -30,9 +33,22 @@ class PytorchWrapper(Model):
             torch.autograd.backward((y_var,), grad_variables=(dy_var,))
             dX = self.ops.asarray(x_var.grad.data)
             if sgd is not None:
-                optimizer.step()
+                if self._optimizer is None:
+                    self._optimizer = self._create_optimizer(sgd)
+                self._optimizer.step()
+                self._optimizer.zero_grad()
             return dX
-        return self.ops.asarray(y_var.data), backward
+        return self.ops.asarray(y_var.data), backward_pytorch
+
+    def _create_optimizer(self, sgd):
+        params = self._model.parameters()
+        if sgd.b1 != 0 and sgd.b2 != 0:
+            optimizer = torch.optim.Adam(params, lr=sgd.alpha, betas=(sgd.b1, sgd.b2))
+        elif sgd.b2 == 0:
+            optimizer = torch.optim.SGD(params, lr=sgd.alpha, momentum=sgd.b1)
+        else:
+            raise NotImplementedError
+        return optimizer
 
     def to_disk(self, path):
         # TODO: Untested
@@ -54,23 +70,26 @@ class PytorchWrapper(Model):
         self._model.load_state_dict(torch.load(filelike))
 
     def to_gpu(self, device_num):
-        # TODO: Implement
-        raise NotImplementedError
+        self._model.cuda(device_num)
 
     def to_cpu(self):
-        # TODO: Implement
-        raise NotImplementedError
+        self._model.cpu()
 
-    def resize_output(self):
-        # TODO: Required for spaCy add label
+    def resize_output(self, new_dim):
+        #self.weight = nn.Parameter(F.pad(self.weight, ...)) # add classes
+        #self.weight = nn.Parameter(F.pad(model.weight, ...)) # add classes
         raise NotImplementedError
 
     def resize_input(self):
-        # TODO: Not required yet, but should be useful
         raise NotImplementedError
 
     @contextlib.contextmanager
     def use_params(self, params): # pragma: no cover
-        # TODO: Implement
-        raise NotImplementedError
+        if self.id in params:
+            backup = self.to_bytes()
+            self.from_bytes(params[self.id])
+        else:
+            backup = None
+        yield
+        self.from_bytes(backup)
 
