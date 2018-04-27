f701ee5a8c1ee707fedcb9e20c86161f537b9013
Matthew Honnibal <honnibal+gh@gmail.com>
2018-04-27 18:12:39 +0000

    Make residual connections work for list-valued inputs

--- thinc/neural/_classes/resnet.py
+++ thinc/neural/_classes/resnet.py
@@ -10,13 +10,24 @@ class Residual(Model):
         self.on_data_hooks.append(on_data)
 
     def __call__(self, X):
-        return X + self._layers[0](X)
+        Y = self._layers[0](X)
+        if isinstance(X, list) or isinstance(X, tuple):
+            return [X[i]+Y[i] for i in range(len(X))]
+        else:
+            return X + Y
 
     def begin_update(self, X, drop=0.):
         y, bp_y = self._layers[0].begin_update(X, drop=drop)
-        output = X+y
+        if isinstance(X, list) or isinstance(X, tuple):
+            output = [X[i]+y[i] for i in range(len(X))]
+        else:
+            output = X+y
         def residual_bwd(d_output, sgd=None):
-            return d_output + bp_y(d_output, sgd)
+            dX = bp_y(d_output, sgd)
+            if isinstance(d_output, list) or isinstance(d_output, tuple):
+                return [d_output[i]+dX[i] for i in range(len(d_output))]
+            else:
+                return d_output + dX
         return output, residual_bwd
 
 def on_data(self, X, y=None):
