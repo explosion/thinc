b15a79e311c45cd25181b79d9657eedcf5ac3785
Matthew Honnibal <honnibal+gh@gmail.com>
2018-05-20 17:25:29 +0200

    Set version to v6.11.1

--- thinc/about.py
+++ thinc/about.py
@@ -4,11 +4,11 @@
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
 __name__ = 'thinc'
-__version__ = '6.11.1.dev20'
+__version__ = '6.11.1'
 __summary__ = "Practical Machine Learning for NLP"
 __uri__ = 'https://github.com/explosion/thinc'
 __author__ = 'Matthew Honnibal'
 __email__ = 'matt@explosion.ai'
 __license__ = 'MIT'
 __title__ = "thinc"
-__release__ = False
+__release__ = True
