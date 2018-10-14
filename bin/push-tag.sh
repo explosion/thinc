f2f66fa093f2d17282e2d661c5e3a9097d64dd4c
Matthew Honnibal <honnibal+gh@gmail.com>
2018-10-14 16:24:53 +0200

    Push to correct branch

--- bin/push-tag.sh
+++ bin/push-tag.sh
@@ -8,7 +8,7 @@ git diff-index --quiet HEAD
 git checkout v6.10.x
 git pull origin v6.10.x
 git push origin v6.10.x
-version=$(grep "__version__ = " spacy/about.py)
+version=$(grep "__version__ = " thinc/about.py)
 version=${version/__version__ = }
 version=${version/\'/}
 version=${version/\'/}
