6a31f9509aa024bdfb48f3f472dbc5f6b6ebc488
Matthew Honnibal <honnibal+gh@gmail.com>
2018-10-14 16:24:27 +0200

    Push to correct branch

--- bin/push-tag.sh
+++ bin/push-tag.sh
@@ -5,7 +5,7 @@ set -e
 # Insist repository is clean
 git diff-index --quiet HEAD
 
-git checkout master
+git checkout v6.10.x
 git pull origin v6.10.x
 git push origin v6.10.x
 version=$(grep "__version__ = " spacy/about.py)
