8dd00e16960a30928902def2ad7910bd9280fbcd
Matthew Honnibal <honnibal+gh@gmail.com>
2018-11-09 13:26:22 +0100

    Fix rates

--- thinc/rates.py
+++ thinc/rates.py
@@ -3,7 +3,7 @@
 from __future__ import division
 
 
-def decaying(base_rate, decay, start=0):
+def decaying(base_rate, decay, t=0):
     '''Yield an infinite series of linearly decaying values, 
     following the schedule:
     
@@ -15,13 +15,12 @@ def decaying(base_rate, decay, start=0):
         >>> next(learn_rates)
         >>> next(learn_rates)
     '''
-    t = start
     while True:
-        yield rate * (1./(1. + decay * t))
+        yield base_rate * (1./(1. + decay * t))
         t += 1
 
 
-def compounding(start, stop, compound, start=0.):
+def compounding(start, stop, compound, t=0.):
     """Yield an infinite series of compounding values. Each time the
     generator is called, a value is produced by multiplying the previous
     value by the compound rate.
@@ -34,16 +33,15 @@ def compounding(start, stop, compound, start=0.):
     """
     curr = float(start)
     while True:
-        yield clip(curr, stop)
+        yield _clip(curr, start, stop)
         curr *= compound
 
 
-def _clip(value, stop):
+def _clip(value, start, stop):
     return max(value, stop) if (start > stop) else min(value, stop)
 
 
-def annealing(rate, decay, decay_steps, start=0.):
-    t = start
+def annealing(rate, decay, decay_steps, t=0.):
     while True:
         if decay == 0.0:
             yield rate
@@ -53,17 +51,16 @@ def annealing(rate, decay, decay_steps, start=0.):
 
 
 def slanted_triangular(max_rate, num_steps, cut_frac=0.1, ratio=32, decay=1,
-        start=0.):
+        t=0.):
     '''Yield an infinite series of values according to Howard and Ruder's 
     "slanted triangular learning rate" schedule.
     '''
     cut = int(num_steps * cut_frac)
-    t = start
     while True:
         t += 1
-        if step < cut:
+        if t < cut:
             p = t / cut
         else:
-            p = 1 - ((t-cut) / (cut * (1/cut_frac - 1))
+            p = 1 - ((t-cut) / (cut * (1/cut_frac - 1)))
         learn_rate = max_rate * (1 + p * (ratio - 1)) * (1/ ratio)
         yield learn_rate
