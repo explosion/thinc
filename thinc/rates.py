0a875b0c40f4fba0f4a425f23368378735e89b4c
Matthew Honnibal <honnibal+gh@gmail.com>
2018-11-10 18:39:53 +0100

    Add module with rates and schedules, for learning rates etc (#81)
    * Add thinc.rates module
    
    * Note thinc.rates module in setup.py
    
    * Fix rates
    
    * Remove rates from setup
    
    * Add some tests for rates
    
    * Update comment

---
+++ thinc/rates.py
@@ -0,0 +1,68 @@
+'''Generators that provide different rates, schedules, decays or series.'''
+
+from __future__ import division
+
+
+def decaying(base_rate, decay, t=0):
+    '''Yield an infinite series of linearly decaying values, 
+    following the schedule:
+    
+        base_rate * 1/(1+decay*t)
+
+    Example:
+    
+        >>> learn_rates = linear_decay(0.001, 1e-4)
+        >>> next(learn_rates)
+        0.001
+        >>> next(learn_rates)
+        0.00999
+    '''
+    while True:
+        yield base_rate * (1./(1. + decay * t))
+        t += 1
+
+
+def compounding(start, stop, compound, t=0.):
+    """Yield an infinite series of compounding values. Each time the
+    generator is called, a value is produced by multiplying the previous
+    value by the compound rate.
+
+    EXAMPLE:
+      >>> sizes = compounding(1., 10., 1.5)
+      >>> assert next(sizes) == 1.
+      >>> assert next(sizes) == 1 * 1.5
+      >>> assert next(sizes) == 1.5 * 1.5
+    """
+    curr = float(start)
+    while True:
+        yield _clip(curr, start, stop)
+        curr *= compound
+
+
+def _clip(value, start, stop):
+    return max(value, stop) if (start > stop) else min(value, stop)
+
+
+def annealing(rate, decay, decay_steps, t=0.):
+    while True:
+        if decay == 0.0:
+            yield rate
+        else:
+            yield rate * decay ** (t / decay_steps)
+            t += 1
+
+
+def slanted_triangular(max_rate, num_steps, cut_frac=0.1, ratio=32, decay=1,
+        t=0.):
+    '''Yield an infinite series of values according to Howard and Ruder's 
+    "slanted triangular learning rate" schedule.
+    '''
+    cut = int(num_steps * cut_frac)
+    while True:
+        t += 1
+        if t < cut:
+            p = t / cut
+        else:
+            p = 1 - ((t-cut) / (cut * (1/cut_frac - 1)))
+        learn_rate = max_rate * (1 + p * (ratio - 1)) * (1/ ratio)
+        yield learn_rate
---
+++ thinc/tests/unit/test_rates.py
@@ -0,0 +1,41 @@
+from ...rates import *
+
+def test_decaying_rate():
+    rates = decaying(0.001, 1e-4)
+    rate = next(rates)
+    assert rate == 0.001
+    next_rate = next(rates)
+    assert next_rate < rate
+    assert next_rate > 0
+    assert next_rate > next(rates)
+
+def test_compounding_rate():
+    rates = compounding(1, 16, 1.01)
+    rate0 = next(rates)
+    assert rate0 == 1.0
+    rate1 = next(rates)
+    rate2 = next(rates)
+    rate3 = next(rates)
+    assert rate3 > rate2 > rate1 > rate0
+    assert (rate3-rate2) > (rate2-rate1) > (rate1-rate0)
+
+def test_annealing_rate():
+    rates = annealing(0.001, 1e-4, 1000)
+    rate0 = next(rates)
+    rate1 = next(rates)
+    rate2 = next(rates)
+    rate3 = next(rates)
+    assert rate0 == 0.001
+    assert rate3 < rate2 < rate1 < rate0
+    assert (rate2-rate3) < (rate1-rate2)
+
+def test_slanted_triangular_rate():
+    rates = slanted_triangular(1., 20., ratio=10)
+    rate0 = next(rates)
+    assert rate0 < 1.
+    rate1 = next(rates)
+    assert rate1 > rate0
+    rate2 = next(rates)
+    assert rate2 < rate1
+    rate3 = next(rates)
+    assert rate0 < rate3 < rate2
