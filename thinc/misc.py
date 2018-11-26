f37e20360a5203b6752d053346c455a1c154db43
Matthew Honnibal <honnibal+gh@gmail.com>
2018-11-26 16:06:42 +0100

    Add alias for FeatureExtracter to thinc.misc

--- thinc/misc.py
+++ thinc/misc.py
@@ -1,3 +1,7 @@
 from .neural._classes.batchnorm import BatchNorm
 from .neural._classes.layernorm import LayerNorm
 from .neural._classes.resnet import Residual
+from .neural._classes.feature_extracter import FeatureExtracter
+from .neural._classes.function_layer import FunctionLayer
+from .neural._classes.feed_forward import FeedForward
+
