7e5730a9535ca7a0e386d49239801b613c08c7d1
Matthew Honnibal <honnibal+gh@gmail.com>
2018-12-03 02:44:30 +0100

    Update minibatch function

--- thinc/neural/util.py
+++ thinc/neural/util.py
@@ -161,20 +161,3 @@ def partition(examples, split_size):  # pragma: no cover
     n_docs = len(examples)
     split = int(n_docs * split_size)
     return examples[:split], examples[split:]
-
-
-def minibatch(stream, batch_size=1000):  # pragma: no cover
-    if hasattr(stream, "__len__") and hasattr(stream, "__getitem__"):
-        i = 0
-        while i < len(stream):
-            yield stream[i : i + batch_size]
-            i += batch_size
-    else:
-        batch = []
-        for X in stream:
-            batch.append(X)
-            if len(batch) >= batch_size:
-                yield batch
-                batch = []
-        if len(batch) != 0:
-            yield batch
