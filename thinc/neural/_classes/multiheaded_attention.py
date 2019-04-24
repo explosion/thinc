f2fc17c740d609e7fd9118093f7fad7824d3415e
Giannis Daras <daras.giannhs@gmail.com>
2019-04-24 23:58:39 +0300

    Refactor attention

--- thinc/neural/_classes/multiheaded_attention.py
+++ thinc/neural/_classes/multiheaded_attention.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 import copy
 import torch.nn.functional as F
 import torch
+from thinc.extra.wrappers import PyTorchWrapper
 import math
 
 
@@ -50,39 +51,35 @@ class MultiHeadedAttention(Model):
         # q0: nB, nL, nM
         # k0: nB, nL, nM
         # v0: nB, nL, nM
-        # q1: nB, nL, nH, nD
-        # k1: nB, nL, nH, nD
-        # v1: nB, nL, nH, nD
-        # x1: nB, nL, nH, nD
-        # x2: nB, nL, nM
-        # x3: nB, nL, nM
+        # q1: nB, nH, nL, nD
+        # k1: nB, nH, nL, nD
+        # v1: nB, nH, nL, nD
         nB, nL, nD, nH = x0.shape[0], x0.shape[1], self.nD, self.nH
         q0, get_dx0 = self.get_queries.begin_update(x0)
-        q1 = q0.reshape(nB, -1, self.nH, self.nD)
+        q1 = q0.reshape(nB, -1, self.nH, self.nD).transpose(0, 2, 1, 3)
         k0, get_dy0_1 = self.get_keys.begin_update(y0)
-        k1 = k0.reshape(nB, -1, self.nH, self.nD)
+        k1 = k0.reshape(nB, -1, self.nH, self.nD).transpose(0, 2, 1, 3)
         v0, get_dy0_2 = self.get_values.begin_update(y0)
-        v1 = v0.reshape(nB, -1, self.nH, self.nD)
-
+        v1 = v0.reshape(nB, -1, self.nH, self.nD).transpose(0, 2, 1, 3)
         x1, get_dq1_dk1_dv1 = self.attn(q1, k1, v1, mask=mask, sentX=sentX,
                                         sentY=sentY, self_attn=self_attention)
-
-        x2 = x1.reshape(x1.shape[0], x1.shape[1], x1.shape[2]*x1.shape[3])
+        x2 = x1.transpose(0, 2, 1, 3).reshape((nB, nL, nH*nD))
         x3, get_dx2 = self.get_output.begin_update(x2)
 
         def finish_update(dx3, sgd=None):
             dx2 = get_dx2(dx3, sgd=sgd)
-            dx1 = dx2.reshape(nB, nL, nH, nD)
+            dx1 = dx2.reshape((nB, nL, nH, nD)).transpose(0, 2, 1, 3)
             dq1, dk1, dv1 = get_dq1_dk1_dv1(dx1)
-            dv0 = dv1.reshape(nB, nL, nH, nD)
-            dk0 = dk1.reshape(nB, nL, nH, nD)
-            dq0 = dq1.reshape(nB, nL, nH, nD)
+            nM = nH * nD
+            dq0 = dq1.transpose(0, 2, 1, 3).reshape((nB, nL, nM))
+            dk0 = dk1.transpose(0, 2, 1, 3).reshape((nB, nL, nM))
+            dv0 = dv1.transpose(0, 2, 1, 3).reshape((nB, nL, nM))
             dy0 = get_dy0_2(dv0, sgd=sgd) + get_dy0_1(dk0, sgd=sgd)
             dx0 = get_dx0(dq0, sgd=sgd)
             if self_attention:
                 return dx0 + dy0
             else:
-                return (dy0, dx0)
+                return (dx0, dy0)
         return x3, finish_update
 
     def attn(self, Q, K, V, mask=None, sentX=None, sentY=None, self_attn=True):
@@ -92,14 +89,10 @@ class MultiHeadedAttention(Model):
         compute an attention matrix, which is used to rescale
         V.
         '''
-        # query shape: nB, nL, nH, nD
-
         S0, bp_scaled_dp = self._scaled_dot_prod(Q, K)
         S1, bp_mask = self._mask(S0, mask)
-        S2, bp_softmax = self._softmax(S1)
-        if sentX is not None and sentY is not None:
-            visualize_attention(sentX, sentY, S2[0], layer=self.layer,
-            self_attn=self_attn)
+        # S2, bp_softmax = self._softmax(S1)
+        S2, bp_softmax = self._softmax.begin_update(S1)
         S3, bp_apply_attn = self._apply_attn(S2, V)
 
         def backprop_attn(dS3):
@@ -108,38 +101,34 @@ class MultiHeadedAttention(Model):
             dS1 = bp_softmax(dS2)
             dS0 = bp_mask(dS1)
             dQ, dK = bp_scaled_dp(dS0)
-            #print("attn", "dQ", dQ.mean(), dQ.var(), "dK", dK.mean(), dK.var(), "dV", dV.mean(), dV.var())
             return dQ, dK, dV
         return S3, backprop_attn
 
     def _scaled_dot_prod(self, Q0, K0):
-        # nB: #Sentences, nL: #Length, nH: #Heads, nD: #Dimensions
-        nB, nL, nH, nD = Q0.shape
-        # Shape of Q0: (nB, nL, nH, nD)
-        # Shape of K0: (nB, nL, nH, nD)
-        # --> (nB*nH, nL, nD)
-
-        Q1 = Q0.transpose(0, 2, 1, 3).reshape(nB*nH, nL, nD)
-
-        # --> (nB*nH, nD, nL)
-        K1 = K0.transpose(0, 2, 3, 1).reshape(nB*nH, nD, nL)
-
+        # Q0: nB, nH, nL, nD
+        # K0: nB, nH, nL, nD
+        nB, nH, nL, nD = Q0.shape
+        # Q1: nB*nH, nL, nD
+        Q1 = Q0.reshape((nB*nH, nL, nD))
+        # K1: (nB*nH, nD, nL)
+        K1 = K0.transpose(0, 1, 3, 2).reshape((nB*nH, nD, nL))
+        # K2: (nB*nH, nD, nL)
         K2 = K1 / self.ops.xp.sqrt(self.nM)
-        # (nB*nH, nL, nD) @ (nB*nH, nD, nL) --> (nB*nH, nL, nL)
+        # S0: (nB*nH, nL, nL)
+        S0 = self.ops.xp.matmul(Q1, K2)
 
-        S = self.ops.xp.matmul(Q1, K2)
+        # S1 shape: (nB, nH, nL, nL)
+        S1 = S0.reshape((nB, nH, nL, nL))
 
-        def backprop_attn1(dS):
-            # (nB*nH, nL, nL) @ (nB*nH, nD, nL).T --> (nB*nH, nL, nD)
-            dS = dS.reshape(nB*nH, nL, nL)
-            dQ1 = self.ops.xp.matmul(dS, K2.transpose(0, 2, 1))
-            # (nB*nH, nL, nD).T @ (nB*nH, nL, nL) --> (nB*nH, nD, nL)
-            dK2 = self.ops.xp.matmul(Q1.transpose(0, 2, 1), dS)
-            dK1 = dK2 / self.ops.xp.sqrt(self.nM)
-            dK0 = dK1.reshape((nB, nH, nD, nL)).transpose(0, 2, 3, 1)
-            dQ0 = dQ1.reshape((nB, nH, nL, nD)).transpose(0, 2, 1, 3)
+        def backprop_attn1(dS1):
+            dS0 = dS1.reshape((nB*nH, nL, nL))
+            dQ1 = self.ops.xp.matmul(dS0, K2.transpose(0, 2, 1))
+            dK2 = self.ops.xp.matmul(Q1.transpose(0, 2, 1), dS0)
+            dK1 = dK2 * self.ops.xp.sqrt(self.nM)
+            dK0 = dK1.reshape((nB, nH, nD, nL)).transpose(0, 1, 3, 2)
+            dQ0 = dQ1.reshape((nB, nH, nL, nD))
             return dQ0, dK0
-        return S.reshape((nB, nH, nL, nL)), backprop_attn1
+        return S1, backprop_attn1
 
     def _mask(self, S0, mask):
         S1 = S0.transpose(1, 0, 2, 3)
@@ -154,42 +143,37 @@ class MultiHeadedAttention(Model):
 
         return S3, backprop_attn2
 
-    def _softmax(self, S0):
-        ''' A simple softmax to the scores '''
-        # S0: nB, nH, nL, nL
-        # S1: nB, nH, nL, nL
-        S1 = self.ops.softmax(S0)
-
-        def backprop_attn3(dS1):
-            dS0 = dS1 * S1
-            sum_dS0 = dS0.sum(axis=2, keepdims=True)
-            dS0 -= sum_dS0 * S1
-            #print("bp_softmax", "S1", S1.mean(), S1.var(), "dS1", dS1.mean(), dS1.var(), "dS0", dS0.mean(), dS0.var())
-            return dS0
-        return S1, backprop_attn3
+    # def _softmax(self, S0):
+    #     ''' A simple softmax to the scores '''
+    #     # S0: nB, nH, nL, nL
+    #     # S1: nB, nH, nL, nL
+    #     S1 = self.ops.softmax(S0)
+    #
+    #     def backprop_attn3(dS1):
+    #         dS0 = dS1 * S1
+    #         sum_dS0 = dS0.sum(axis=2, keepdims=True)
+    #         dS0 -= sum_dS0 * S1
+    #         return dS0
+    #     return S1, backprop_attn3
 
     def _apply_attn(self, S0, V0):
         ''' Multiplication with values '''
-        nB, nH, nL, nL = S0.shape
-        nD = V0.shape[-1]
-        V1 = V0.reshape((nB*nH, nL, nD))
-
-        S1 = S0.reshape((nB*nH, nL, nL))
         # S0: (nB, nH, nL, nL)
+        # VO: (nB, nH, nL, nD)
         # S1: (nB*nH, nL, nL)
         # V1:  (nB*nH, nL, nD)
         # S2: (nB*nH, nL, nD)
-        # S3: (nB, nL, nH, nD)
-
-        # (nB*nH, nL, nL) @ (nB*nH, nL, nD) --> (nB*nH, nL, nD)
-
+        # S3: (nB, nH, nL, nD)
+        nB, nH, nL, nL = S0.shape
+        nD = V0.shape[-1]
+        V1 = V0.reshape((nB*nH, nL, nD))
+        S1 = S0.reshape((nB*nH, nL, nL))
         S2 = self.ops.xp.matmul(S1, V1)
 
-        S3 = S2.reshape((nB, nH, nL, nD)).transpose(0, 2, 1, 3)
+        S3 = S2.reshape((nB, nH, nL, nD))
 
         def backprop_attn4(dS3):
-            # (nB, nL, nH, nD) --> (nB*nH, nL, nD)
-            dS2 = dS3.transpose(0, 2, 1, 3).reshape((nB*nH, nL, nD))
+            dS2 = dS3.reshape((nB*nH, nL, nD))
             # (nB*nH, nL, nD) @ (nB*nH, nL, nD).T --> (nB*nH, nL, nL)
             dS1 = self.ops.xp.matmul(dS2, V1.transpose(0, 2, 1))
             # (nB*nH, nL, nL).T @ (nB*nH, nL, nD) --> (nB*nH, nL, nD)
@@ -212,7 +196,7 @@ def attention(query, key, value, mask=None, dropout=None):
              / math.sqrt(d_k)
     if mask is not None:
         scores = scores.masked_fill(mask == 0, -1e9)
-    p_attn = F.softmax(scores, dim = -1)
+    p_attn = F.softmax(scores, dim=-1)
     if dropout is not None:
         p_attn = dropout(p_attn)
     return torch.matmul(p_attn, value), p_attn
