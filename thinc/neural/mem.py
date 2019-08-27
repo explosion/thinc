7df20c02b4f1acfaa6403fae6e9550442e15287e
Matthew Honnibal <honnibal+gh@gmail.com>
2019-08-27 11:44:10 +0200

    Separate gradients array from weights array

--- thinc/neural/mem.py
+++ thinc/neural/mem.py
@@ -12,18 +12,19 @@ class Memory(object):
         if size < 0:
             raise ValueError("TODO error re negative size %d" % size)
         self.ops = ops
-        self._mem = self.ops.allocate((2, size))
+        self._weights_array = self.ops.allocate((size,))
+        self._grads_array = self.ops.allocate((size,))
         self._offsets = {}
         self._sizes = {}
         self._i = 0
 
     @property
     def weights(self):
-        return self._mem[0, : self._i]
+        return self._weights_array[:self._i]
 
     @property
     def gradient(self):
-        return self._mem[1, : self._i]
+        return self._grads_array[: self._i]
 
     def __contains__(self, name):
         return name in self._offsets
@@ -31,13 +32,17 @@ class Memory(object):
     def __getitem__(self, name):
         offset, col, shape = self._offsets[name]
         size = self._sizes[name]
-        return self._mem[col, offset : offset + size].reshape(shape)
+        if col == 0:
+            array = self._weights_array
+        else:
+            array = self._grads_array
+        return array[offset : offset+size].reshape(shape)
 
     def get(self, name, default=None):
         return self[name] if name in self._offsets else default
 
     def set(self, value):
-        self._mem[0, : self._i] = value
+        self._weights_array[:self._i] = value
 
     @check.arg(2, is_shape)
     def add(self, name, shape):
@@ -45,8 +50,8 @@ class Memory(object):
         self._offsets[name] = (self._i, 0, shape)
         size = prod(shape)
         self._sizes[name] = size
-        blob = self._get_blob(size)
-        return blob[0].reshape(shape)
+        params, grads = self._get_blob(size)
+        return params.reshape(shape)
 
     def add_gradient(self, grad_name, param_name):
         assert grad_name not in self._offsets, "TODO error"
@@ -54,20 +59,24 @@ class Memory(object):
         size = self._sizes[param_name]
         self._offsets[grad_name] = (offset, 1, shape)
         self._sizes[grad_name] = size
-        return self._mem[1, offset : offset + size].reshape(shape)
+        return self._grads_array[offset : offset + size].reshape(shape)
 
     def _get_blob(self, nr_req):
-        nr_avail = self._mem.shape[1] - (self._i + 1)
+        nr_avail = self._weights_array.shape[0] - (self._i + 1)
         if nr_avail < nr_req:
-            self._realloc(max(self._mem.shape[1], nr_req) * 2)
-        blob = self._mem[:, self._i : self._i + nr_req]
+            self._realloc(max(self._weights_array.shape[0], nr_req) * 2)
+        w = self._weights_array[self._i : self._i + nr_req]
+        g = self._grads_array[self._i : self._i + nr_req]
         self._i += nr_req
-        return blob
+        return w, g
 
     def _realloc(self, new_size):
-        new_mem = self.ops.allocate((self._mem.shape[0], new_size))
-        new_mem[:, : self._i + 1] = self._mem[:, : self._i + 1]
-        self._mem = new_mem
+        new_weights = self.ops.allocate((new_size,))
+        new_grads = self.ops.allocate((new_size,))
+        new_weights[: self._i + 1] = self._weights_array[: self._i + 1]
+        new_grads[: self._i + 1] = self._grads_array[: self._i + 1]
+        self._weights_array = new_weights
+        self._grads_array = new_grads
 
 
 #
