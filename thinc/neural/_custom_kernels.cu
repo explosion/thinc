066bc2f04ae9b86e6e5c2ccbe583c5dbb6bc5e7c
Matthw Honnibal <honnibal+gh@gmail.com>
2019-10-18 22:42:32 +0200

    Move cuda source into .cu file

---
+++ thinc/neural/_custom_kernels.cu
@@ -0,0 +1,93 @@
+
+extern "C" __global__
+void sum_pool(float* output,
+    const float* X, const int* lengths, int B, int T, int O)
+{
+    // Compute sums of a batch of concatenated sequences
+    int b = blockIdx.x * blockDim.x + threadIdx.x; // Batch-item we're working on
+    if (b >= B) return;
+
+    // Go to the regions we're working on
+    for (int i=0; i < b; ++i) {
+        output += O;
+	X += lengths[i] * O;
+    }
+    int length = lengths[b];
+    // Each invocation of the kernel sums one batch.
+    for (int _=0; _ < length; ++_) // Iterate over rows
+    {
+        for (int i=0; i < O; ++i) 
+        {
+          output[i] += X[i];
+        }
+        X += O;
+    }
+}
+
+
+extern "C" __global__
+void maxout(float* best, int* which,
+        const float* cands, int B, int O, int P)
+{
+    int b = blockIdx.x * blockDim.x + threadIdx.x; 
+    if (b >= B) return;
+
+    // Go to the regions we're working on
+    for (int i=0; i < b; ++i) {
+        best += O;
+        which += O;
+        cands += O * P;
+    }
+
+    for (int i=0; i < O; ++i)
+    {
+        which[i] = 0
+        best[i] = cands[0];
+        for (int p=1; p < P; ++p)
+	{
+            if (cands[i+p] > best[0])
+	    {
+                which[i] = p;
+                best[i] = cands[i+p];
+	    }
+	}
+    }
+}
+
+
+extern "C" __global__
+void max_pool(float* maxes, int* which,
+    const float* X, const int* lengths, int B, int T, int O)
+{
+    int b = blockIdx.x * blockDim.x + threadIdx.x; // Batch-item we're working on
+    if (b >= B) return;
+
+    // Go to the regions we're working on
+    for (int i=0; i < b; ++i) {
+        maxes += O;
+        which += O;
+	X += lengths[i] * O;
+    }
+ 
+    // Each invocation of the kernel maxes one batch.
+    // Start by assuming maxes are at i=0
+    for (int j=0; j < O; ++j) {
+        maxes[j] = X[j];
+	which[j] = 0;
+    }
+    X += O;
+    
+    int length = lengths[b];
+    for (int i=1; i < length; ++i) // Iterate over rows
+    {
+        for (int j=0; j < O; ++j)
+	{
+            if (X[j] > maxes[j])
+            {
+                maxes[j] = X[j];
+                which[j] = i;
+	    }
+	}
+	X += O;
+    }
+}
--- thinc/neural/_custom_kernels.py
+++ thinc/neural/_custom_kernels.py
@@ -1,4 +1,6 @@
 from numpy.testing import assert_allclose
+import re
+from pathlib import Path
 
 try:
     import cupy
@@ -6,110 +8,26 @@ except ImportError:
     cupy = None
 
 
-SUM_POOL_STR = """
-extern "C" __global__
-void sum_pool(float* output,
-    const float* X, const int* lengths, int B, int T, int O)
-{
-    // Compute sums of a batch of concatenated sequences
-    int b = blockIdx.x * blockDim.x + threadIdx.x; // Batch-item we're working on
-    if (b >= B) return;
-
-    // Go to the regions we're working on
-    for (int i=0; i < b; ++i) {
-        output += O;
-	X += lengths[i] * O;
-    }
-    int length = lengths[b];
-    // Each invocation of the kernel sums one batch.
-    for (int _=0; _ < length; ++_) // Iterate over rows
-    {
-        for (int i=0; i < O; ++i) 
-        {
-          output[i] += X[i];
-        }
-        X += O;
-    }
-}
-"""
-
-
-MAXOUT_STR = """
-extern "C" __global__
-void maxout(float* best, int* which,
-        const float* cands, int B, int O, int P)
-{
-    int b = blockIdx.x * blockDim.x + threadIdx.x; 
-    if (b >= B) return;
-
-    // Go to the regions we're working on
-    for (int i=0; i < b; ++i) {
-        best += O;
-        which += O;
-        cands += O * P;
-    }
-
-    for (int i=0; i < O; ++i)
-    {
-        which[i] = 0
-        best[i] = cands[0];
-        for (int p=1; p < P; ++p)
-	{
-            if (cands[i+p] > best[0])
-	    {
-                which[i] = p;
-                best[i] = cands[i+p];
-	    }
-	}
-    }
-}
-"""
-
-
-MAX_POOL_STR = """
-extern "C" __global__
-void max_pool(float* maxes, int* which,
-    const float* X, const int* lengths, int B, int T, int O)
-{
-    int b = blockIdx.x * blockDim.x + threadIdx.x; // Batch-item we're working on
-    if (b >= B) return;
-
-    // Go to the regions we're working on
-    for (int i=0; i < b; ++i) {
-        maxes += O;
-        which += O;
-	X += lengths[i] * O;
-    }
- 
-    // Each invocation of the kernel maxes one batch.
-    // Start by assuming maxes are at i=0
-    for (int j=0; j < O; ++j) {
-        maxes[j] = X[j];
-	which[j] = 0;
-    }
-    X += O;
+kernel_re = re.compile(r"extern \"C\" __global__.+(?=extern|$)", re.DOTALL)
+name_re = re.compile(r"(?<=void )\w+(?=\()")
+def parse_kernels(src):
+    kernels = {}
+    for kernel in kernel_re.findall(src):
+        name = name_re.search(kernel).group()
+        kernels[name] = kernel
+    return kernels
     
-    int length = lengths[b];
-    for (int i=1; i < length; ++i) // Iterate over rows
-    {
-        for (int j=0; j < O; ++j)
-	{
-            if (X[j] > maxes[j])
-            {
-                maxes[j] = X[j];
-                which[j] = i;
-	    }
-	}
-	X += O;
-    }
-}
-"""
+SRC = (Path(__file__).parent / "_custom_kernels.cu").open().read()
+kernels = parse_kernels(SRC)
+for name, src in kernels.items():
+    print(name)
+    print(src)
 
 
-if cupy is not None:
-    sum_pool_kernel = cupy.RawKernel(SUM_POOL_STR, "sum_pool")
-    max_pool_kernel = cupy.RawKernel(MAX_POOL_STR, "max_pool")
-
+#if cupy is not None:
+#    sum_pool_kernel = cupy.RawKernel(SUM_POOL_STR, "sum_pool")
+#    max_pool_kernel = cupy.RawKernel(MAX_POOL_STR, "max_pool")
+#
 
 def sum_pool(X, lengths, out=None, threads_per_block=128):
     if out is None:
@@ -172,5 +90,5 @@ def test_max_pool():
         start += length
 
 
-test_sum_pool()
-test_max_pool()
+#test_sum_pool()
+#test_max_pool()
