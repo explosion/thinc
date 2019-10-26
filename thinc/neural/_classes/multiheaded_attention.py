5a368bfb325fcc4465e02232149284e0325d44af
Matthew Honnibal <honnibal+gh@gmail.com>
2019-10-27 01:45:25 +0200

    WIP on rewrite self-attention

--- thinc/neural/_classes/multiheaded_attention.py
+++ thinc/neural/_classes/multiheaded_attention.py
@@ -4,73 +4,196 @@ from .model import Model
 from .affine import Affine
 from ...api import with_reshape, layerize, wrap
 from ..util import get_array_module
-import numpy as np
+from ..ops import NumpyOps
+import numpy
 import copy
 import math
 
+class PaddedAttentionInputs(object):
+    def __init__(self, QKV, lengths):
+        self.QKV = QKV
+        self.nB, self.nL, three, self.nH, self.nD = QKV.shape
+        self.nN = sum(lengths)
+        assert three == 3
+        self.lengths = lengths
+        self.ops = NumpyOps()
 
-def prepare_self_attention(affine, window=None, nM=300, nH=6):
+    def queries_dot_keys(self, scale=1.):
+        queries = self.QKV[:, :, 0]
+        keys = self.QKV[:, :, 1]
+        assert queries.shape == (self.nB, self.nL, self.nH, self.nD)
+        assert keys.shape == (self.nB, self.nL, self.nH, self.nD)
+        queries = queries.transpose((0, 2, 1, 3))
+        keys = keys.transpose((0, 2, 3, 1))
+        dots = self.ops.xp.matmul(queries, keys)
+        dots /= scale
+        return dots
+
+
+class AttentionInputs(object):
+    """Inputs for an attention model."""
+    def __init__(self, QKV, lengths): 
+        self.QKV = QKV
+        self.nH = QKV.shape[2]
+        self.nD = QKV.shape[3]
+        self.nM = self.nH*self.nD
+        self.nN = QKV.shape[0]
+        self.nB = len(lengths)
+        self.nP = sum(length*length for length in lengths)
+        self.cmp_size = max(lengths)
+        self.lengths = lengths
+        self.ops = NumpyOps()
+
+    @property
+    def slices(self):
+        start = 0
+        attn_start = 0
+        for length in self.lengths:
+            attn_length = length*length
+            yield start, start+length, attn_start, attn_start+attn_length
+            start += length
+            attn_start += attn_length
+
+    def _get_feature(self, index, dims, contiguous):
+        data = self.QKV[:, index]
+        return self.transpose(data,
+            current=("nN", "nH", "nD"), target=dims, contiguous=contiguous)
+
+    def get_queries(self, dims, contiguous=False):
+        return self._get_feature(0, dims, contiguous)
+
+    def get_keys(self, dims, contiguous=False):
+        return self._get_feature(1, dims, contiguous)
+    
+    def get_values(self, dims, contiguous=False):
+        return self._get_feature(2, dims, contiguous)
+
+    def transpose(self, array, target, current, contiguous=False):
+        assert set(current) == set(target)
+        if current == target:
+            return array
+        array = array.transpose([current.index(d) for d in target])
+        if contiguous:
+            array = self.ops.xp.ascontiguousarray(array)
+        return array
+
+    def apply_attention(self, attn, attn_dims=("nH", "nN")):
+        attn = self.transpose(attn,
+            target=("nH", "nP"), current=attn_dims, contiguous=True)
+        values = self.get_values(("nH", "nN", "nD"), contiguous=True)
+        # Attn is (nH, nQ, nV)
+        # V    is (nH, nV, nD)
+        # Do: (nH, nQ, nV) @ (nH, nV, nD) = (nH, nQ, nD)
+        output = self.ops.allocate((self.nH, self.nN, self.nD))
+        for h in range(self.nH):
+            for s, e, aS, aE in self.slices:
+                seq_attn = attn[h, aS:aE].reshape((e-s, -1))
+                self.ops.gemm(seq_attn, values[h, s:e], out=output[h, s:e])
+        output = self.transpose(output,
+            target=("nN", "nH", "nD"), current=("nH", "nN", "nD"), contiguous=True)
+
+        def backprop_apply_attention(d_output):
+            output = self.transpose(d_output,
+                target=("nH", "nN", "nD"), current=("nN", "nH", "nD"), contiguous=True)
+            d_values = self.ops.allocate((self.nH, self.nN, self.nD))
+            d_attn = self.ops.allocate((self.nH, self.nP))
+            for h in range(self.nH):
+                for s, e, aS, aE in self.slices:
+                    n = e-s
+                    seq_attn = attn[h, aS:aE].reshape((e-s, -1))
+                    # (nQ, nV).T @ (nQ, nD) = (nV, nD)
+                    self.ops.gemm(seq_attn, d_output[h, s:e],
+                        trans1=True, out=d_values[h, s:e])
+                    # (nQ, nD) @ (nV, nD).T = (nQ, nV)
+                    self.ops.gemm(d_output[h, s:e], d_values[h, s:e],
+                        trans2=True, out=d_attn[h, aS:aE].reshape((e-s, -1)))
+            d_values = self.transpose(d_values,
+                target=("nN", "nH", "nD"), current=("nH", "nN", "nD"))
+            d_attn = self.transpose(d_attn,
+                target=("nN", "nH", "nN"), current=("nH", "nN", "nN"))
+            return d_values, d_attn
+        return output, backprop_apply_attention
+
+    def get_attn(self, scale=1.):
+        return self._get_attn_cpu(self.ops.xp.array([scale], dtype="f"))
+
+    def _get_attn_cpu(self, scale):
+        # Transpose keys to (heads, lengths, dims)
+        Q = self.get_queries(("nH", "nN", "nD"), contiguous=True)
+        K = self.get_keys(("nH", "nN", "nD"), contiguous=True)
+        attn = numpy.zeros((self.nH, self.nP), dtype="f")
+        for h in range(self.nH):
+            for s, e, aS, aE in self.slices:
+                dots = self.ops.gemm(Q[h, s:e], K[h, s:e], trans2=True)
+                dots /= scale
+                smax = self.ops.softmax(dots, axis=-1)
+                attn[h, aS:aE] = self.ops.softmax(dots, axis=-1).ravel()
+        
+        def backprop_get_attn_cpu(d_attn):
+            dQ = self.ops.allocate((self.nH, self.nN, self.nD))
+            dK = self.ops.allocate((self.nH, self.nN, self.nD))
+            for h in range(self.nH):
+                for s, e, aS, aE in self.slices:
+                    # d_dots has shape (qlength, klength)
+                    d_dots = self.ops.backprop_softmax(
+                        attn[h, s:e], d_attn[h, aS:aE], axis=-1)
+                    # Compute (qlength, klength) @ (klength, nD) = (qlength, nD)
+                    self.ops.gemm(d_dots, K[h, s:e],
+                        out=dQ[h, s:e])
+                    # Compute (qlength, klength).T @ (qlength, nD) = (klength, nD)
+                    self.ops.gemm(d_dots, Q[h, s:e], trans1=True, out=dK[h, s:e])
+            dQ = self.transpose(dQ, target=("nN", "nH"), current=("nH", "nN"))
+            dK = self.transpose(dK, target=("nN", "nH"), current=("nH", "nN"))
+            return dQ, dK
+        return attn, backprop_get_attn_cpu
+
+    def _dot_queries_to_keys_gpu(queries, keys, values, out=None):
+        A_ptr = self.K.data.ptr
+        B_ptr = self.Q.data.ptr
+        C_ptr = out.data.ptr
+        strideA = self.cmp_size * self.K.shape[1]
+        strideB = self.Q.shape[1]
+        strideC = self.cmp_size
+
+        sgemmStridedBatched(
+            handle,
+            False, False,
+            Q.shape[0], 1, Q.shape[1],
+            alpha,
+            A_ptr, K.shape[1], da, strideA,
+            B_ptr, Q.shape[1], strideB,
+            C_ptr, V.shape[1], strideC,
+            batchCount
+        )
+
+
+def prepare_self_attention(affine, nM=300, nH=6):
     nD = nM // nH
-    if window is not None:
-        get_mask = window_mask(window)
-    else:
-        get_mask = None
+    
     def qkv_sa_forward(Xs, drop=0.0):
-        X = affine.ops.flatten(Xs)
-        lengths = [len(x) for x in Xs]
+        (X, lengths), was_ragged = _get_ragged_array(affine.ops, Xs)
+        assert X.shape == (sum(lengths), Xs[0].shape[-1]), (X.shape, Xs[0].shape)
         QKV, get_dX = affine.begin_update(X, drop=drop)
-        Qs, Ks, Vs = _split_seqs(QKV, lengths, nH, nD)
+        output = AttentionInputs(QKV.reshape((X.shape[0], 3, nH, -1)), lengths)
 
-        def qkv_sa_backward(dQs_dKs_dVs, sgd=None):
-            dQs, dKs, dVs = dQs_dKs_dVs
-            dQKV = _join_seqs(dQs, dKs, dVs, nH, nD)
+        def qkv_sa_backward(d_output, sgd=None):
+            dQKV = d_output.QKV.reshape((QKV.shape[0], -1))
             dX = get_dX(dQKV, sgd=sgd)
-            return affine.ops.unflatten(dX, lengths)
-        if get_mask is not None:
-            xp = get_array_module(X)
-            masks = [get_mask(xp, length, length) for length in lengths]
-        else:
-            masks = [None for _ in lengths]
-        return (Qs, Ks, Vs, masks), qkv_sa_backward
+            if was_ragged:
+                return dX, lengths
+            else:
+                return affine.ops.unflatten(dX, lengths)
+
+        return output, qkv_sa_backward
+
     return wrap(qkv_sa_forward, affine)
 
 
-def window_mask(n):
-    def get_mask(xp, nX, nY):
-        mask = xp.zeros((nX, nY), dtype='f')
-        for i in range(nX):
-            mask[i, i-n:i+n] = 1
-        return mask
-    return get_mask
-
-
-def _split_seqs(QKV, lengths, nH, nD):
-    assert sum(lengths) == QKV.shape[0], (sum(lengths), QKV.shape[0])
-    Qs = []
-    Ks = []
-    Vs = []
-    i = 0
-    xp = get_array_module(QKV)
-    for length in lengths:
-        qkv = QKV[i:i+length]
-        qkv = qkv.reshape((length, 3, nH*nD))
-        queries = xp.ascontiguousarray(qkv[:, 0])
-        keys = xp.ascontiguousarray(qkv[:, 1])
-        values = xp.ascontiguousarray(qkv[:, 2])
-        Qs.append(queries.reshape((-1, nH, nD)))
-        Ks.append(keys.reshape((-1, nH, nD)))
-        Vs.append(values.reshape((-1, nH, nD)))
-        i += length
-    return Qs, Ks, Vs
-
-
-def _join_seqs(Qs, Ks, Vs, nH, nD):
-    xp = get_array_module(Qs[0])
-    Q = xp.vstack(Qs).reshape((-1, nH*nD))
-    K = xp.vstack(Ks).reshape((-1, nH*nD))
-    V = xp.vstack(Vs).reshape((-1, nH*nD))
-    assert Q.shape[0] == K.shape[0] == V.shape[0]
-    return xp.hstack((Q, K, V))
+def _get_ragged_array(ops, Xs):
+    if hasattr(Xs, "__len__") and len(Xs) == 2 and Xs[1].dtype == "i":
+        return Xs, True
+    else:
+        return (ops.flatten(Xs), [len(x) for x in Xs]), False
 
 
 class MultiHeadedAttention(Model):
@@ -80,132 +203,18 @@ class MultiHeadedAttention(Model):
     def __init__(self):
         Model.__init__(self)
 
-    def begin_update(self, Qs_Ks_Vs_masks, drop=0.0):
-        Qs, Ks, Vs, masks = Qs_Ks_Vs_masks
-        if masks is None:
-            masks = [None for _ in Qs]
-        assert len(Qs) == len(Ks) == len(Vs)
-        return self._attend_seqs(Qs, Ks, Vs, masks)
-        
-    def _attend_seqs(self, Qs, Ks, Vs, masks):
-        outputs = []
-        backprops = []
-        assert len(Qs) == len(Ks) == len(Vs) == len(masks), (len(Qs), len(Ks), len(Vs), len(masks))
-        for Q, K, V, mask in zip(Qs, Ks, Vs, masks):
-            output, backprop = self._attend(Q, K, V, mask)
-            outputs.append(output)
-            backprops.append(backprop)
-            assert output.shape[0] == Q.shape[0]
+    def begin_update(self, qkv, drop=0.0):
+        attn, get_dQ_dK = qkv.queries_dot_keys(scale=self.ops.xp.sqrt(qkv.nM))
+        output0, get_dV_d_attn = qkv.apply_attn(attn)
+        output1 = output0.reshape((qkv.nN, qkv.nH*qkv.nD))
+        lengths = qkv.lengths
 
-        def backprop_attend_seqs(d_outputs, sgd=None):
-            dQs = []
-            dKs = []
-            dVs = []
-            for d_output, backprop in zip(d_outputs, backprops):
-                dQ, dK, dV = backprop(d_output, sgd=sgd)
-                dQs.append(dQ)
-                dKs.append(dK)
-                dVs.append(dV)
-            return dQs, dKs, dVs
-        assert len(outputs) == len(Qs), len(Qs)
-        return outputs, backprop_attend_seqs
-
-    def _attend(self, Q, K, V, mask):
-        """
-        Compute attention on a (query, key, value) triplet.
-        The similarity of the (Q, K) pairs are used to
-        compute an attention matrix, which is used to rescale
-        V.
-        """
-        attn, get_dQ_dK = self._get_attn_weights(Q, K, mask)
-        output, get_d_attn_dV = self._apply_attn(attn, V)
-
-        def backprop_attend(d_output, sgd=None):
-            d_attn, dV = get_d_attn_dV(d_output)
+        def backprop_multiheaded_attention(d_output1, sgd=None):
+            d_output0 = d_output1.reshape((nN, nH, nD))
+            dV, d_attn = get_dV_d_attn(d_output0)
             dQ, dK = get_dQ_dK(d_attn)
-            return (dQ, dK, dV)
+            dQKV = self.ops.xp.hstack((dQ, dK, dV))
+            dQKV = dQKV.reshape((dQKV.shape[0], 3, nH, nD))
+            return AttentionInputs(dQKV, lengths)
 
-        return output, backprop_attend
-
-    def _get_attn_weights(self, Q0, K0, mask):
-        nQ, nK, nH, nD = (Q0.shape[0], K0.shape[0], Q0.shape[1], Q0.shape[2])
-        assert Q0.shape == (nQ, nH, nD)
-        assert K0.shape == (nK, nH, nD)
-        sqrtM = self.ops.xp.sqrt(nH*nD).astype("f")
-        Q1 = _trans(Q0, 1, 0, 2)
-        assert Q1.shape == (nH, nQ, nD)
-        K1 = _trans(K0, 1, 2, 0)
-        assert K1.shape == (nH, nD, nK)
-        K1 /= sqrtM
-        attn0 = self.ops.matmul(Q1, K1)
-        assert attn0.shape == (nH, nQ, nK)
-        attn1, backprop_mask = self._apply_mask(attn0, mask)
-        attn2 = self.ops.softmax(attn1, axis=-1)
-        assert attn2.shape == (nH, nQ, nK)
-
-        def backprop_attn1(d_attn2, sgd=None):
-            assert d_attn2.shape == (nH, nQ, nK)
-            d_attn1 = self.ops.backprop_softmax(attn2, d_attn2, axis=-1)
-            d_attn0 = backprop_mask(d_attn1)
-            assert d_attn0.shape == (nH, nQ, nK)
-            dQ1 = self.ops.matmul(d_attn0, _trans(K1, 0, 2, 1))
-            assert dQ1.shape == (nH, nQ, nD)
-            dK1 = self.ops.matmul(_trans(Q1, 0, 2, 1), d_attn0)
-            assert dK1.shape == (nH, nD, nK)
-            dK0 = _trans(dK1, 2, 0, 1)
-            dK0 /= sqrtM
-            assert dK0.shape == (nK, nH, nD)
-            dQ0 = _trans(dQ1, 1, 0, 2)
-            assert dQ0.shape == (nQ, nH, nD)
-            return dQ0, dK0
-
-        return attn2, backprop_attn1
-
-    def _apply_mask(self, attn, mask):
-        def backprop_apply_mask(d_attn, sgd=None):
-            if mask is None:
-                return d_attn
-            else:
-                return d_attn * mask
-
-        if mask is None:
-            return attn, backprop_apply_mask
-        else:
-            return attn - (1-mask)*1e9, backprop_apply_mask
-
-    def _apply_attn(self, attn, V0):
-        """ Multiplication with values """
-        nH, nQ, nV = attn.shape
-        nD = V0.shape[-1]
-        assert V0.shape == (nV, nH, nD)
-        V1 = _trans(V0, 1, 0, 2)
-        assert V1.shape == (nH, nV, nD)
-        # (nH, nQ, nV) @ (nH, nV, nD) = (nH, nQ, nD)
-        S0 = self.ops.matmul(attn, V1)
-        assert S0.shape == (nH, nQ, nD)
-        S1 = _trans(S0, 1, 0, 2)
-        assert S1.shape == (nQ, nH, nD)
-        S2 = S1.reshape((nQ, nH*nD))
-
-        def backprop_apply_attn(dS2):
-            assert dS2.shape == (nQ, nH*nD)
-            dS1 = dS2.reshape((nQ, nH, nD))
-            dS0 = dS1.transpose((1, 0, 2))
-            assert dS0.shape == (nH, nQ, nD)
-            dS0 = self.ops.xp.ascontiguousarray(dS0)
-            # (nH, nQ, nD) @ (nH, nD, nV) --> (nH, nQ, nV)
-            d_attn = self.ops.matmul(dS0, _trans(V1, 0, 2, 1))
-            assert d_attn.shape == (nH, nQ, nV)
-            # (nH, nV, nQ) @ (nH, nQ, nD) --> (nH, nV, nD)
-            dV1 = self.ops.matmul(_trans(attn, 0, 2, 1), dS0)
-            assert dV1.shape == (nH, nV, nD)
-            dV0 = dV1.transpose((1, 0, 2))
-            assert dV0.shape == (nV, nH, nD)
-            return d_attn, dV0
-
-        return S2, backprop_apply_attn
-
-def _trans(X, *order):
-    """Transpose and make contiguous"""
-    xp = get_array_module(X)
-    return xp.ascontiguousarray(X.transpose(order))
+        return (output1, lengths), backprop_multiheaded_attention
