c861c499ffbe2044be1506d562b3d346a28dba18
Matthew Honnibal <honnibal+gh@gmail.com>
2019-11-01 23:49:26 +0100

    Add SelfAttention function to thinc.t2t

--- thinc/t2t.py
+++ thinc/t2t.py
@@ -1,5 +1,6 @@
 # coding: utf8
 from __future__ import unicode_literals
+import numpy
 
 from .neural._classes.convolution import ExtractWindow  # noqa: F401
 from .neural._classes.attention import ParametricAttention  # noqa: F401
@@ -8,22 +9,26 @@ from .neural._classes.multiheaded_attention import MultiHeadedAttention
 from .neural._classes.multiheaded_attention import prepare_self_attention
 
 
-def SelfAttention(width, depth, pieces=4):
-    from .api import chain, clone, with_getitem
+def SelfAttention(width, depth, pieces=1):
+    """Create a transformer-style self-attention layer."""
+    from .api import chain, clone, with_getitem, layerize, noop, concatenate
     from .misc import LayerNorm, Residual
-    from .v2v import Mish, Affine
+    from .v2v import Mish, Affine, Maxout
 
     if (width % pieces) != 0:
         raise ValueError("Width must be divisible by pieces (aka heads)")
     return clone(
-        Residual(
-            chain(
-                prepare_self_attention(
-                    Affine(width*3, width),
-                    nM=width, nH=pieces),
-                MultiHeadedAttention(),
-                with_getitem(0, LayerNorm(Mish(width, width))),
-            )
+        chain(
+            with_getitem(0, LayerNorm(nO=width)),
+            Residual(
+                chain(
+                    prepare_self_attention(
+                        Affine(width*3, width),
+                        nM=width, nH=pieces),
+                    MultiHeadedAttention(),
+                    with_getitem(0, Mish(width, width)),
+                )
+            ),
         ),
         depth
     )
