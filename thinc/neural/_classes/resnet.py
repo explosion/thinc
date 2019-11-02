06a11752d76e91d1a2859e17a7b6d652d80031c9
Matthw Honnibal <honnibal+gh@gmail.com>
2019-11-02 01:31:12 +0100

    Add nO property to Residual

--- thinc/neural/_classes/resnet.py
+++ thinc/neural/_classes/resnet.py
@@ -10,6 +10,10 @@ class Residual(Model):
         self._layers.append(layer)
         self.on_data_hooks.append(on_data)
 
+    @property
+    def nO(self):
+        return self._layers[-1].nO
+
     def predict(self, X):
         Y = self._layers[0](X)
         if isinstance(X, list) or isinstance(X, tuple):
