e247bd5db777b2d0755df66ca7ef6e206875ab66
Matthew Honnibal <honnibal+gh@gmail.com>
2019-12-09 19:03:55 +0100

    Register FeatureExtractor and LayerNorm layers

--- thinc/misc.py
+++ thinc/misc.py
@@ -7,3 +7,14 @@ from .neural._classes.resnet import Residual  # noqa: F401
 from .neural._classes.feature_extracter import FeatureExtracter  # noqa: F401
 from .neural._classes.function_layer import FunctionLayer  # noqa: F401
 from .neural._classes.feed_forward import FeedForward  # noqa: F401
+from ._registry import registry
+
+
+@registry.layers.register("FeatureExtractor.v1")
+def make_FeatureExtractor(attrs):
+    return FeatureExtracter(attrs)
+
+
+@registry.layers.register("LayerNorm.v1")
+def make_LayerNorm(outputs=None, child=None):
+    return LayerNorm(nO=outputs, child=child)
