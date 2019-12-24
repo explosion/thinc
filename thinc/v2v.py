47371f6f62374974add3ae81ca271d6a2ea69944
Matthew Honnibal <honnibal+gh@gmail.com>
2019-12-24 14:39:47 +0100

    Add typing

--- thinc/v2v.py
+++ thinc/v2v.py
@@ -1,5 +1,6 @@
 # coding: utf8
 from __future__ import unicode_literals
+from typing import Optional
 
 from .neural._classes.model import Model  # noqa: F401
 from .neural._classes.affine import Affine  # noqa: F401
@@ -12,20 +13,20 @@ from ._registry import registry
 
 
 @registry.layers.register("ReLu.v1")
-def make_ReLu(outputs, inputs=None):
+def make_ReLu(outputs: int, inputs: Optional[int]=None):
     return ReLu(nO=outputs, nI=inputs)
 
 
 @registry.layers.register("Mish.v1")
-def make_Mish(outputs, inputs=None):
+def make_Mish(outputs: int, inputs: Optional[int]=None):
     return Mish(nO=outputs, nI=inputs)
 
 
 @registry.layers.register("Softmax.v1")
-def make_Softmax(outputs, inputs=None):
+def make_Softmax(outputs: Optional[int]=None, inputs: Optional[int]=None):
     return Softmax(nO=outputs, nI=inputs)
 
 
 @registry.layers.register("Maxout.v1")
-def make_Maxout(outputs, pieces, inputs=None):
+def make_Maxout(outputs: int, inputs: Optional[int]=None, *, pieces: int):
     return Maxout(nO=outputs, nI=inputs, pieces=pieces)
