aa5ec3de7a27760c92588541a60303ce77a350b0
Ines Montani <ines@ines.io>
2019-12-25 17:16:06 +0100

    Clean setup and add setup.cfg (#135)
    * Clean setup and add setup.cfg
    
    * Update MANIFEST.in
    
    * Move coverage setup to setup.cfg [ci skip]

--- thinc/__init__.py
+++ thinc/__init__.py
@@ -1,5 +1,5 @@
 # Necessary for some side-effects in Cython. Not sure I understand.
 import numpy  # noqa: F401
 
-from .about import __name__, __version__  # noqa: F401
+from .about import __version__  # noqa: F401
 from ._registry import registry  # noqa: F401
