12590a5333745cd7e21780af4d655c6399efd385
Matthew Honnibal <honnibal+gh@gmail.com>
2019-12-25 23:03:15 +0100

    Refactor API module

--- thinc/neural/_classes/function_layer.py
+++ thinc/neural/_classes/function_layer.py
@@ -71,3 +71,49 @@ class wrap(Model):
     
     def set_grad(self, name, value):
         self._layers[-1].set_grad(name, value)
+
+
+class ConcatenationLayer(Model):
+    name = "concatenate"
+    
+    def begin_update(self, X, drop=0.):
+        Ys, callbacks = zip(*[lyr.begin_update(X, drop=drop) for lyr in layers])
+        lengths = [Y.shape[1] for Y in Ys]
+        output = self.ops.xp.hstack(Ys)
+
+        def finish_update_concatenate(d_output, sgd=None):
+            layer_grad = None
+            start = 0
+            for bwd, length in zip(callbacks, lengths):
+                if bwd is not None:
+                    d = bwd(d_output[:, start:start+length], sgd=sgd)
+                    if d is not None and hasattr(X, "shape"):
+                        if layer_grad is None:
+                            layer_grad = d
+                        else:
+                            layer_grad += d
+                start += length
+            return layer_grad
+
+        return output, finish_update_concatenate
+
+
+class AdditionLayer(Model):
+    def begin_update(self, X, drop=0.0):
+        outs, callbacks = zip(*[lyr.begin_update(X, drop=drop) for lyr in layers])
+        out = outs[0]
+        for o in outs:
+            out += o
+
+        def backward(d_out, sgd=None):
+            grads = [bp(d_out, sgd=sgd) for bp in callbacks if bp is not None]
+            grads = [g for g in grads if g is not None]
+            if grads:
+                total = grads[0]
+                for g in grads:
+                    total += g
+                return total
+            else:
+                return None
+
+        return out, backward
