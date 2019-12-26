6f4ef6ae1717a3cdd826750adb7f714012f75300
Matthew Honnibal <honnibal+gh@gmail.com>
2019-12-26 18:35:13 +0100

    Remove more unused functions

--- thinc/neural/util.py
+++ thinc/neural/util.py
@@ -101,31 +101,6 @@ def minibatch(items, size=8):
             yield list(batch)
 
 
-def mark_sentence_boundaries(sequences, drop=0.0):  # pragma: no cover
-    """Pad sentence sequences with EOL markers."""
-    for sequence in sequences:
-        sequence.insert(0, "-EOL-")
-        sequence.insert(0, "-EOL-")
-        sequence.append("-EOL-")
-        sequence.append("-EOL-")
-    return sequences, None
-
-
-def remap_ids(ops):
-    id_map = {0: 0}
-
-    def begin_update(ids, drop=0.0):
-        n_vector = len(id_map)
-        for i, id_ in enumerate(ids):
-            if id_ not in id_map:
-                id_map[id_] = n_vector
-                n_vector += 1
-            ids[i] = id_map[id_]
-        return ids, None
-
-    return begin_update
-
-
 def copy_array(dst, src, casting="same_kind", where=None):
     if isinstance(dst, numpy.ndarray) and isinstance(src, numpy.ndarray):
         dst[:] = src
@@ -143,6 +118,14 @@ def ensure_path(path):
         return path
 
 
+def partition(examples, split_size):  # pragma: no cover
+    examples = list(examples)
+    numpy.random.shuffle(examples)
+    n_docs = len(examples)
+    split = int(n_docs * split_size)
+    return examples[:split], examples[split:]
+
+
 def to_categorical(y, nb_classes=None):
     # From keras
     xp = get_array_module(y)
@@ -155,19 +138,6 @@ def to_categorical(y, nb_classes=None):
     categorical = numpy.zeros((n, nb_classes), dtype="float32")
     categorical[numpy.arange(n), y] = 1
     return xp.asarray(categorical)
-
-
-def flatten_sequences(sequences, drop=0.0):  # pragma: no cover
-    xp = get_array_module(sequences[0])
-    return xp.concatenate(sequences), None
-
-
-def partition(examples, split_size):  # pragma: no cover
-    examples = list(examples)
-    numpy.random.shuffle(examples)
-    n_docs = len(examples)
-    split = int(n_docs * split_size)
-    return examples[:split], examples[split:]
 
 
 def is_ragged(seqs):
