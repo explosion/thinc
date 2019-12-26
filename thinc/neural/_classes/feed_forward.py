1b08ca158c715711f27449811b2a5e373837716e
Matthew Honnibal <honnibal+gh@gmail.com>
2019-12-26 18:24:43 +0100

    Update FeedForward

--- thinc/neural/_classes/feed_forward.py
+++ thinc/neural/_classes/feed_forward.py
@@ -1,11 +1,14 @@
 from .model import Model
 
 
-def _run_child_hooks(model, X, y):
-    for layer in model._layers:
+def _run_child_hooks(model, X=None, Y=None):
+    for layer in model._layers[:-1]:
         for hook in layer.on_data_hooks:
-            hook(layer, X, y)
-        X = layer(X)
+            hook(layer, X=X)
+        if X is not None:
+            X = layer.predict(X)
+    for hook in model._layers[-1].on_data_hooks:
+        hook(model._layers[-1], X=X, Y=Y)
 
 
 class FeedForward(Model):
@@ -14,21 +17,20 @@ class FeedForward(Model):
     name = "feed-forward"
 
     def __init__(self, layers, **kwargs):
-        self._layers = []
+        Model.__init__(self, **kwargs)
+        self.on_data_hooks = [_run_child_hooks]
         for layer in layers:
             if isinstance(layer, FeedForward):
                 self._layers.extend(layer._layers)
             else:
                 self._layers.append(layer)
-        Model.__init__(self, **kwargs)
-        self.on_data_hooks.append(_run_child_hooks)
 
     def infer_dimensions(self, X=None, Y=None):
         if Y is not None:
             self._layers[-1].infer_dimensions(X=None, Y=Y)
         for layer in self._layers:
             layer.infer_dimensions(X=X)
-            X = layer(X)
+            X = layer.predict(X)
 
     def has_dim(self, name):
         return self._layers[-1].has_dim(name)
@@ -65,7 +67,7 @@ class FeedForward(Model):
 
     def predict(self, X):
         for layer in self._layers:
-            X = layer(X)
+            X = layer.predict(X)
         return X
 
     def begin_update(self, X, drop=0.0):
