ec7afabe88f9b915b60cd31e81121a575b98d133
Matthew Honnibal <honnibal+gh@gmail.com>
2019-12-27 01:17:44 +0100

    Remove drop argument from combinators

--- thinc/wire.py
+++ thinc/wire.py
@@ -22,7 +22,7 @@ def layerize(begin_update=None, predict=None, *args, **kwargs):
 def noop(*layers):
     """Transform a sequences of layers into a null operation."""
 
-    def noop_forward(X, drop=0.0):
+    def noop_forward(X):
         return X, lambda D, *a, **k: D
 
     return layerize(noop_forward, layers=list(layers))
@@ -112,7 +112,7 @@ def add(*layers):
 
 
 @layerize
-def flatten_add_lengths(seqs, drop=0.0):
+def flatten_add_lengths(seqs):
     """Transform sequences to ragged arrays if necessary. If sequences are
     already ragged, do nothing. A ragged array is a tuple (data, lengths),
     where data is the concatenated data.
@@ -130,7 +130,7 @@ def flatten_add_lengths(seqs, drop=0.0):
 
 
 @layerize
-def unflatten(X_lengths, drop=0.0):
+def unflatten(X_lengths):
     """Transform sequences from a ragged format into lists."""
     ops = Model.ops
     X, lengths = X_lengths
@@ -146,14 +146,14 @@ def unflatten(X_lengths, drop=0.0):
 def with_reshape(layer):
     """Reshape data on the way into and out from a layer."""
 
-    def with_reshape_forward(X, drop=0.0):
+    def with_reshape_forward(X):
         initial_shape = X.shape
         final_shape = list(initial_shape[:-1]) + [layer.nO]
         nB = X.shape[0]
         nT = X.shape[1]
         X2d = X.reshape(-1, X.shape[2])
         X2d = X2d.astype(layer.ops.xp.float32)
-        Y2d, Y2d_backprop = layer.begin_update(X2d, drop=drop)
+        Y2d, Y2d_backprop = layer.begin_update(X2d)
         Y = Y2d.reshape(final_shape)
 
         def with_reshape_backward(dY):
@@ -170,17 +170,17 @@ def with_getitem(idx, layer):
     from a tuple. 
     """
 
-    def with_getitem_forward(items, drop=0.0):
-        X, finish = layer.begin_update(items[idx], drop=drop)
+    def with_getitem_forward(items):
+        X, finish = layer.begin_update(items[idx])
         return items[:idx] + (X,) + items[idx + 1 :], finish
 
     return wrap(with_getitem_forward, layer)
 
 
 def with_square_sequences(model):
-    def padded_forward(seqs_in, drop=0.0):
+    def padded_forward(seqs_in):
         padded_in, _, unpad = model.ops.square_sequences(seqs_in)
-        (padded_out, _), backprop_model = model.begin_update(padded_in, drop=drop)
+        (padded_out, _), backprop_model = model.begin_update(padded_in)
         seqs_out = unpad(padded_out)
 
         def backprop_padding(d_seqs_out):
@@ -194,9 +194,9 @@ def with_square_sequences(model):
 
 
 def with_flatten(layer, pad=0, ndim=4):
-    def with_flatten_forward(seqs_in, drop=0.0):
+    def with_flatten_forward(seqs_in):
         lengths = layer.ops.asarray([len(seq) for seq in seqs_in])
-        X, bp_layer = layer.begin_update(layer.ops.flatten(seqs_in, pad=pad), drop=drop)
+        X, bp_layer = layer.begin_update(layer.ops.flatten(seqs_in, pad=pad))
         if bp_layer is None:
             return layer.ops.unflatten(X, lengths, pad=pad), None
 
@@ -242,7 +242,7 @@ def uniqued(layer, column=0):
     using e.g. character LSTM.
     """
 
-    def uniqued_fwd(X, drop=0.0):
+    def uniqued_fwd(X):
         keys = X[:, column]
         keys = layer.ops.xp.ascontiguousarray(keys)
         if not isinstance(keys, numpy.ndarray):
@@ -251,7 +251,7 @@ def uniqued(layer, column=0):
             keys, return_index=True, return_inverse=True, return_counts=True
         )
         X_uniq = layer.ops.xp.ascontiguousarray(X[ind])
-        Y_uniq, bp_Y_uniq = layer.begin_update(X_uniq, drop=drop)
+        Y_uniq, bp_Y_uniq = layer.begin_update(X_uniq)
         Y = Y_uniq[inv].reshape((X.shape[0],) + Y_uniq.shape[1:])
 
         def uniqued_bwd(dY):
@@ -270,30 +270,18 @@ def uniqued(layer, column=0):
     return model
 
 
-def foreach(layer, drop_factor=1.0):
+def foreach(layer):
     """Map a layer across list items"""
 
-    def foreach_fwd(docs, drop=0.0):
+    def foreach_fwd(docs):
         sents = []
         lengths = []
         for doc in docs:
             doc_sents = [sent for sent in doc if len(sent)]
-            assert len(doc_sents)
-            if drop:
-                subset = [
-                    s for s in doc_sents if numpy.random.random() >= drop * drop_factor
-                ]
-            else:
-                subset = list(doc_sents)
-            if subset:
-                sents.extend(subset)
-                lengths.append(len(subset))
-            else:
-                numpy.random.shuffle(doc_sents)
-                sents.append(doc_sents[0])
-                lengths.append(1)
+            sents.extend(doc_sents)
+            lengths.append(len(doc_sents))
         assert len(sents)
-        flat, bp_flat = layer.begin_update(sents, drop=0.0)
+        flat, bp_flat = layer.begin_update(sents)
         output = layer.ops.unflatten(flat, lengths)
 
         def foreach_bwd(d_output):
@@ -318,27 +306,19 @@ def foreach(layer, drop_factor=1.0):
     return model
 
 
-def foreach_sentence(layer, get_sents=None, drop_factor=1.0):
+def foreach_sentence(layer, get_sents=None):
     """Map a layer across sentences"""
     if get_sents is None:
         get_sents = lambda doc: doc.sents
 
-    def sentence_fwd(docs, drop=0.0):
+    def sentence_fwd(docs):
         sents = []
         lengths = []
         for doc in docs:
             doc_sents = [sent for sent in get_sents(doc) if len(sent)]
-            subset = [
-                s for s in doc_sents if numpy.random.random() >= drop * drop_factor
-            ]
-            if subset:
-                sents.extend(subset)
-                lengths.append(len(subset))
-            else:
-                numpy.random.shuffle(doc_sents)
-                sents.append(doc_sents[0])
-                lengths.append(1)
-        flat, bp_flat = layer.begin_update(sents, drop=0.0)
+            sents.extend(doc_sents)
+            lengths.append(len(doc_sents))
+        flat, bp_flat = layer.begin_update(sents)
         output = layer.ops.unflatten(flat, lengths)
 
         def sentence_bwd(d_output):
@@ -358,12 +338,12 @@ def with_pad_and_mask(layer):
     """Wrap a layer so that list inputs are transformed into padded batches.
     The inputs are provided as (data, mask) tuples.
     """
-    def create_model_input_forward(Xs, drop=0.0):
+    def create_model_input_forward(Xs):
         nX = layer.ops.asarray([x.shape[0] for x in Xs], dtype="i")
         nL = nX.max()
         X, unpad_X = layer.ops.pad_sequences(Xs, pad_to=nL)
         X_mask = _get_mask(layer.ops, X, nX)
-        Y, bp_Y = layer.begin_update((X.astype("float32"), X_mask), drop=drop)
+        Y, bp_Y = layer.begin_update((X.astype("float32"), X_mask))
 
         def create_model_input_backward(dYs):
             dY, _ = layer.ops.pad_sequences(dYs, pad_to=nL)
