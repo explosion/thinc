3bccccc1205cbce1a09b0253d2f26f2b120925d3
Matthew Honnibal <honnibal+gh@gmail.com>
2019-12-31 14:41:42 +0100

    Add mnist test

--- thinc/tests/integration/test_mnist.py
+++ thinc/tests/integration/test_mnist.py
@@ -5,18 +5,19 @@ from thinc.layers.softmax import Softmax
 from thinc.layers.chain import chain
 from thinc.layers.clone import clone
 from thinc.backends import NumpyOps
-from thinc.util import to_categorical
+from thinc.util import to_categorical, get_shuffled_batches
+from thinc.optimizers import Adam
 import ml_datasets
 
 
 @pytest.fixture(scope="module")
-def mnist():
+def mnist(limit=1000):
     train_data, dev_data, _ = ml_datasets.mnist()
     train_X, train_y = NumpyOps().unzip(train_data)
     dev_X, dev_y = NumpyOps().unzip(dev_data)
     dev_y = to_categorical(dev_y, nb_classes=10)
-    train_y = to_categorical(dev_y, nb_classes=10)
-    return (train_X[:1000], train_y[:1000]), (dev_X, dev_y)
+    train_y = to_categorical(train_y, nb_classes=10)
+    return (train_X[:limit], train_y[:limit]), (dev_X[:limit], dev_y[:limit])
 
 
 @pytest.fixture(scope="module")
@@ -26,7 +27,7 @@ def train_X(mnist):
 
 
 @pytest.fixture(scope="module")
-def train_y(mnist):
+def train_Y(mnist):
     (train_X, train_y), (dev_X, dev_y) = mnist
     return train_y
 
@@ -38,15 +39,31 @@ def dev_X(mnist):
 
 
 @pytest.fixture(scope="module")
-def dev_y(mnist):
+def dev_Y(mnist):
     (train_X, train_y), (dev_X, dev_y) = mnist
     return dev_y
 
 
 def create_relu_softmax(depth, width):
-    with Model.define_operators({"*": clone, ">>": chain}):
-        model = ReLu(width, 784) >> Softmax(10, width)
+    with Model.define_operators({"**": clone, ">>": chain}):
+        model = (
+            ReLu(width) ** depth
+            >> Softmax(10, width)
+        )
     return model
+
+
+def evaluate_model(model, dev_X, dev_Y, batch_size):
+    score = 0.
+    total = 0.
+    for i in range(0, dev_X.shape[0], batch_size):
+        X = dev_X[i:i+batch_size]
+        Y = dev_Y[i:i+batch_size]
+        Yh = model.predict(X)
+        #print(Y[0], Yh[0])
+        score += (Y.argmax(axis=1) == Yh.argmax(axis=1)).sum()
+        total += Yh.shape[0]
+    return score / total
 
 
 @pytest.fixture(params=[create_relu_softmax])
@@ -54,22 +71,22 @@ def create_model(request):
     return request.param
 
 
-@pytest.mark.xfail
-@pytest.mark.parametrize(("depth", "width", "nb_epoch"), [(2, 8, 5)])
+@pytest.mark.parametrize(("depth", "width", "nb_epoch"), [(2, 32, 5)])
 def test_small_end_to_end(
-    depth, width, nb_epoch, create_model, train_X, train_y, dev_X, dev_y
+    depth, width, nb_epoch, create_model, train_X, train_Y, dev_X, dev_Y
 ):
-    model = create_model(depth, width)
-    assert isinstance(model, chain)
+    batch_size = 128
+    model = create_model(depth, width).initialize(X=train_X[:10], Y=train_Y[:10])
+    optimizer = Adam(0.001)
     losses = []
-    with model.begin_training(train_X, train_y) as (trainer, optimizer):
-        trainer.each_epoch.append(lambda: print(model.evaluate(dev_X, dev_y)))
-        trainer.nb_epoch = nb_epoch
-        for X, y in trainer.iterate(train_X, train_y):
-            X = model.ops.asarray(X)
-            y = model.ops.asarray(y)
-            yh, backprop = model.begin_update(X, drop=trainer.dropout)
-            backprop(yh - y, optimizer)
-            loss = ((yh - y) ** 2).sum()
-            losses.append(loss)
+    scores = []
+    for i in range(nb_epoch):
+        for X, Y in get_shuffled_batches(train_X, train_Y, batch_size):
+            Yh, backprop = model.begin_update(X)
+            backprop(Yh - Y)
+            model.finish_update(optimizer)
+            losses.append(((Yh - Y) ** 2).sum())
+        score = evaluate_model(model, dev_X, dev_Y, batch_size)
+        scores.append(score)
     assert losses[-1] < losses[0]
+    assert scores[-1] > scores[0]
