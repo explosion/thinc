5e66f2963b82169fa34e9da05699221033f37bf7
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-02 00:14:19 +0100

    Move test

--- thinc/tests/unit/test_sparse_linear.py
+++ thinc/tests/unit/test_sparse_linear.py
@@ -34,6 +34,18 @@ def test_basic(instances, sgd):
     assert loss2 < loss1
 
 
+def test_init():
+    model = SparseLinear(3)
+    keys = numpy.ones((5,), dtype="uint64")
+    values = numpy.ones((5,), dtype="f")
+    lengths = numpy.zeros((2,), dtype="int32")
+    lengths[0] = 3
+    lengths[1] = 2
+    scores, backprop = model.begin_update((keys, values, lengths))
+    assert scores.shape == (2, 3)
+    d_feats = backprop(scores)
+    assert len(d_feats) == 3
+
 # @pytest.fixture
 # def model(instances):
 #    templates = []
