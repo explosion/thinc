32876e0d404a11897eb6e48b0d66f050fabcc0ef
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-02 00:22:02 +0100

    Fix rountrip bytes

--- thinc/tests/integration/test_roundtrip_bytes.py
+++ thinc/tests/integration/test_roundtrip_bytes.py
@@ -3,7 +3,6 @@ from thinc.layers.maxout import Maxout
 from thinc.layers.chain import chain
 
 
-@pytest.mark.xfail
 def test_simple_model_roundtrip_bytes():
     model = Maxout(5, 10, nP=2)
     b = model.get_param("b")
@@ -12,10 +11,9 @@ def test_simple_model_roundtrip_bytes():
     b = model.get_param("b")
     b -= 1
     model = model.from_bytes(data)
-    assert model.get_param("b")[0] == 1
+    assert model.get_param("b")[0, 0] == 1
 
 
-@pytest.mark.xfail
 def test_multi_model_roundtrip_bytes():
     model = chain(Maxout(5, 10, nP=2), Maxout(2, 3))
     b = model.layers[0].get_param("b")
@@ -28,11 +26,10 @@ def test_multi_model_roundtrip_bytes():
     b = model.layers[1].get_param("b")
     b -= 2
     model = model.from_bytes(data)
-    assert model.layers[0].get_param("b")[0] == 1
-    assert model.layers[1].get_param("b")[0] == 2
+    assert model.layers[0].get_param("b")[0, 0] == 1
+    assert model.layers[1].get_param("b")[0, 0] == 2
 
 
-@pytest.mark.xfail
 def test_multi_model_load_missing_dims():
     model = chain(Maxout(5, 10, nP=2), Maxout(2, 3))
     b = model.layers[0].get_param("b")
@@ -43,5 +40,5 @@ def test_multi_model_load_missing_dims():
 
     model2 = chain(Maxout(5), Maxout())
     model2 = model2.from_bytes(data)
-    assert model2.layers[0].get_param("b")[0] == 1
-    assert model2.layers[1].get_param("b")[0] == 2
+    assert model2.layers[0].get_param("b")[0, 0] == 1
+    assert model2.layers[1].get_param("b")[0, 0] == 2
