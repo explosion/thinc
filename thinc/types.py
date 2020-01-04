3360059ecad2767359b66f380a6d9344b6a4a7bf
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-04 17:11:42 +0100

    Fix comment

--- thinc/types.py
+++ thinc/types.py
@@ -41,8 +41,8 @@ class Ragged:
 @dataclass
 class Padded:
     """A batch of padded sequences, sorted by decreasing length. The data array
-    is of shape (batch, step, ...). The auxiliary array size_at_t indicates the
-    length of the batch at each timestep, so you can do data[:size_at_t[t]] to
+    is of shape (step, batch, ...). The auxiliary array size_at_t indicates the
+    length of the batch at each timestep, so you can do data[:, :size_at_t[t]] to
     shrink the batch. 
     """
     data: Array
