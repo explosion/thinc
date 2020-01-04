5dccee984b2c25f6508947d3731b8934296ff4c5
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-04 12:05:38 +0100

    Work on LSTM layer

--- thinc/layers/lstm.py
+++ thinc/layers/lstm.py
@@ -1,91 +1,77 @@
 from ..model import Model
-from ..recurrent import recurrent
-from ..bidirectional import bidirectional
-from ..clone import clone
+from ..util import get_width
+from .recurrent import recurrent
+from .bidirectional import bidirectional
+from .clone import clone
+from .affine import Affine
 
 
-def BiLSTM(nO=None, nI=None, depth=1, dropout=0.0):
+def BiLSTM(nO=None, nI=None, *, depth=1, dropout=0.0):
     return clone(bidirectional(recurrent(LSTM_step(nO=nO, nI=nI))), depth)
 
 
-def LSTM(nO=None, nI=None, depth=1, dropout=0.0):
+def LSTM(nO=None, nI=None, *, depth=1, dropout=0.0):
     return clone(recurrent(LSTM_step(nO=nO, nI=nI)), depth)
 
 
 def LSTM_step(nO=None, nI=None):
     """Create a step model for an LSTM."""
-    return Model(
-        "lstm_step",
-        forward,
-        init=init,
-        dims={"nO": nO, "nI": nI},
-        params={"W": None, "b": None, "initial_hiddens": None, "initial_cells": None},
+    model = Model(
+        "lstm_step", forward, init=init, layers=[Affine()], dims={"nO": nO, "nI": nI}
     )
+    if nO is not None and nI is not None:
+        model.initialize()
+    return model
 
 
 def init(model, X=None, Y=None):
-    pass
+    if X is not None:
+        model.set_dim("nI", get_width(X))
+    if Y is not None:
+        model.set_dim("nO", get_width(Y))
+    nO = model.get_dim("nO")
+    nI = model.get_dim("nI")
+    model.layers[0].set_dim("nO", nO * 4)
+    model.layers[0].set_dim("nI", nO + nI)
+    model.layers[0].initialize()
 
 
 def forward(model, prevstate_inputs, is_train):
     (cell_tm1, hidden_tm1), inputs = prevstate_inputs
+    weights = model.layers[0]
+    nI = model.get_dim("nI")
+    nO = model.get_dim("nO")
+    nB = inputs.shape[0]
+    X = model.ops.xp.hstack((inputs, hidden_tm1))
 
-    acts, bp_acts = _weights_forward(model, (inputs, hidden_tm1), is_train)
-    (cells, hiddens), bp_gates = _gates_forward(model, (acts, cell_tm1), is_train)
+    Y, bp_acts = weights(X, is_train)
+    acts = Y.reshape((nB, 4, nO)).transpose((1, 0, 2))
+    (cells, hiddens), bp_gates = _gates_forward(model.ops, acts, cell_tm1)
 
     def backprop(d_state_d_hiddens):
         (d_cells, d_hiddens), d_hiddens = d_state_d_hiddens
-        d_acts, d_cell_tm1 = bp_gates((d_cells, d_hiddens))
-        d_inputs, d_hidden_tm1 = bp_acts(d_acts)
-        return (d_cell_tm1, d_hidden_tm1), d_inputs
+        d_acts, d_cell_tm1 = bp_gates(d_cells, d_hiddens)
+        dY = d_acts.transpose((1, 0, 2)).reshape((nB, 4 * nO))
+        dX = bp_acts(dY)
+        return (d_cell_tm1, dX[:, nI:]), dX[:, :nI]
 
     return ((cells, hiddens), hiddens), backprop
 
 
-def _weights_forward(model, inputs_hidden, is_train):
-    inputs, hidden = inputs_hidden
-    nO = model.get_dim("nO")
-    nI = model.get_dim("nI")
-    W = model.get_param("W")
-    b = model.get_param("b")
-    forget_bias = model.get_param("forget_bias")
-    X = model.ops.xp.hstack([inputs, hidden])
-    acts = model.ops.gemm(X, W, trans2=True)
-    acts += b
-    acts = acts.reshape((acts.shape[0], 4, nO))
-    acts = model.ops.xp.ascontiguousarray(acts.transpose((1, 0, 2)))
-
-    acts[0] += forget_bias
-
-    def backprop_weights(d_acts):
-        model.inc_grad("forget_bias", d_acts[0].sum(axis=0))
-        dX = model.ops.gemm(d_acts, W)
-        model.inc_grad("W", model.ops.gemm(d_acts, X, trans1=True))
-        model.inc_grad("b", d_acts.sum(axis=0))
-        d_input = dX[:, :nI]
-        d_hidden = dX[:, nI:]
-        return d_input, d_hidden
-
-    return acts, backprop_weights
-
-
-def _gates_forward(model, acts_prev_cells, is_train):
-    acts, prev_cells = acts_prev_cells
-    new_cells = model.ops.allocate(prev_cells.shape)
-    new_hiddens = model.ops.allocate(prev_cells.shape)
-    model.ops.lstm(new_hiddens, new_cells, acts, prev_cells)
-    state = (new_cells, new_hiddens)
+def _gates_forward(ops, acts, prev_cells):
+    new_cells = ops.allocate(prev_cells.shape)
+    new_hiddens = ops.allocate(prev_cells.shape)
+    ops.lstm(new_hiddens, new_cells, acts, prev_cells)
     size = new_cells.shape[0]
 
-    def backprop_gates(d_state):
-        d_cells, d_hiddens = d_state
+    def backprop_gates(d_cells, d_hiddens):
         d_cells = d_cells[:size]
         d_hiddens = d_hiddens[:size]
-        d_acts = [model.ops.allocate(act.shape) for act in acts]
-        d_prev_cells = model.ops.allocate(prev_cells.shape)
-        model.ops.backprop_lstm(
-            d_cells, d_prev_cells, d_acts, d_hiddens, acts, new_cells, prev_cells
+        d_acts = ops.allocate(acts.shape)
+        d_prevcells = ops.allocate(prev_cells.shape)
+        ops.backprop_lstm(
+            d_cells, d_prevcells, d_acts, d_hiddens, acts, new_cells, prev_cells
         )
-        return d_acts, d_prev_cells
+        return d_acts, d_prevcells
 
-    return state, backprop_gates
+    return (new_cells, new_hiddens), backprop_gates
