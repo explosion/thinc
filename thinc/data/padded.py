7d3abede775f1572bb83772b5c72c2f7fa8a0c2a
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-04 16:17:21 +0100

    Move dataclasses into thinc.types

--- thinc/data/padded.py
+++ thinc/data/padded.py
@@ -2,12 +2,3 @@ from dataclasses import dataclass
 from ..types import Array
 
 
-@dataclass
-class Padded:
-    """A batch of padded sequences, sorted by decreasing length. The data array
-    is of shape (batch, step, ...). The auxiliary array size_at_t indicates the
-    length of the batch at each timestep, so you can do data[:size_at_t[t]] to
-    shrink the batch. 
-    """
-    data: Array
-    size_at_t: Array
--- thinc/data/ragged.py
+++ thinc/data/ragged.py
@@ -1,8 +1,2 @@
-from dataclasses import dataclass
-from ..types import Array
 
 
-@dataclass
-class Ragged:
-    data: Array
-    lengths: Array
