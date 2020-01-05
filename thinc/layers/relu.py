3d66c52a4a3b43773dd1cadc2a3e66528cb0ad8b
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-05 17:12:39 +0100

    Experiment with dimension vars in relu

--- thinc/layers/relu.py
+++ thinc/layers/relu.py
@@ -1,11 +1,17 @@
 from typing import Tuple, Callable, Optional, TypeVar
+from typing_extensions import Literal
 
 from ..model import Model, create_init
 from ..initializers import xavier_uniform_init, zero_init
-from ..types import Array, Floats2d
+from ..types import Array, Floats1d, Floats2d
 from .chain import chain
 from .layernorm import LayerNorm
 from .dropout import Dropout
+
+
+B = TypeVar("B", bound=int)
+O = TypeVar("O", bound=int)
+I = TypeVar("I", bound=int)
 
 
 def ReLu(
@@ -17,7 +23,7 @@ def ReLu(
     dropout: Optional[float] = None,
     normalize: bool = False,
 ) -> Model:
-    model = Model[Floats2d, Floats2d](
+    model = Model[Floats2d[B, O], Floats2d[B, O]](
         "relu",
         forward,
         init=create_init({"W": init_W, "b": init_b}),
@@ -33,15 +39,18 @@ def ReLu(
     return model
 
 
-def forward(model: Model, X: Floats2d, is_train: bool) -> Tuple[Floats2d, Callable]:
-    W = model.get_param("W")
-    b = model.get_param("b")
-
-    Y = model.ops.gemm(X, W, trans2=True)
+def forward(
+        model: Model,
+        X: Floats2d[B, I],
+        is_train: bool
+) -> Tuple[Floats2d[B, O], Callable]:
+    W: Floats2d[O, I] = model.get_param("W")
+    b: Floats1d[O] = model.get_param("b")
+    Y: Floats2d[B, O] = model.ops.gemm(X, W, trans2=True)
     Y += b
     model.ops.relu(Y, inplace=True)
 
-    def backprop(dY: Floats2d) -> Floats2d:
+    def backprop(dY: Floats2d[B, O]) -> Floats2d[B, I]:
         dY = model.ops.backprop_relu(dY, Y)
         model.inc_grad("b", dY.sum(axis=0))
         model.inc_grad("W", model.ops.gemm(dY, X, trans1=True))
