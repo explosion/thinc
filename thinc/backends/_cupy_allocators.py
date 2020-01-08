1646c28dbb1ff77f425a55db311f742ab50b54d1
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-08 15:37:06 +0100

    Fix type error in allocator

--- thinc/backends/_cupy_allocators.py
+++ thinc/backends/_cupy_allocators.py
@@ -1,4 +1,5 @@
-from typing import Any
+from typing import Any, cast
+from ..types import CupyArray
 from ..util import tensorflow2xp
 from ..util import torch2xp
 
@@ -13,7 +14,7 @@ except ImportError:
     pass
 
 try:
-    import MemoryPointer from cupy.cuda.memory as MemoryPointerT
+    from cupy.cuda.memory import MemoryPointer as MemoryPointerT
 except ImportError:
     MemoryPointerT = Any
 
@@ -24,9 +25,9 @@ def cupy_tensorflow_allocator(size_in_bytes: int) -> MemoryPointerT:
     together, as otherwise OOM errors can occur when there's available memory
     sitting in the other library's pool.
     """
-    tensor = tensorflow.zeros((size_in_bytes // 4,), dtype=tf.dtypes.float32)
+    tensor = tensorflow.zeros((size_in_bytes // 4,), dtype=tensorflow.dtypes.float32)
     # We convert to cupy via dlpack, so that we can get a memory pointer.
-    cupy_array = tensorflow2xp(tensor)
+    cupy_array = cast(CupyArray, tensorflow2xp(tensor))
     # Now return the array's memory pointer.
     return cupy_array.ptr
 
@@ -38,5 +39,5 @@ def cupy_pytorch_allocator(size_in_bytes: int) -> MemoryPointerT:
     sitting in the other library's pool.
     """
     torch_tensor = torch.zeros((size_in_bytes // 4,))
-    cupy_tensor = torch2xp(torch_tensor)
+    cupy_tensor = cast(CupyArray, torch2xp(torch_tensor))
     return cupy_tensor.data
