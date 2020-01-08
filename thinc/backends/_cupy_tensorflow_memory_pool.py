df2d451366884a5e8ad98346239b3d5d3edd7bdc
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-08 15:36:50 +0100

    Update allocator function

--- thinc/backends/_cupy_tensorflow_memory_pool.py
+++ thinc/backends/_cupy_tensorflow_memory_pool.py
@@ -8,8 +8,8 @@ except ImportError:
 
 def cupy_tensorflow_allocator(size_in_bytes: int):
     assert_tensorflow_is_installed()
-    tensor = tensorflow.zeros((size_in_bytes // 4,), dtype=tf.dtypes.float32)
+    tensor = tensorflow.zeros((size_in_bytes // 4,), dtype=tensorflow.dtypes.float32)
     # We convert to cupy via dlpack, so that we can get a memory pointer.
     cupy_array = tensorflow2xp(tensor)
     # Now return the array's memory pointer.
-    return cupy_array.ptr
+    return cupy_array.ptr # type: ignore
