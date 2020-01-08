0556a4ae9e8e6a8f0a682956f50890a23f3c98be
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-08 13:16:54 +0100

    Fix tensorflow naming

--- thinc/layers/tensorflow_wrapper.py
+++ thinc/layers/tensorflow_wrapper.py
@@ -1,6 +1,6 @@
 from typing import Callable, Tuple, Any
 from ..model import Model
-from ..shims import TensorFlowShim
+from ..shims import TensorflowShim
 from ..util import xp2tensorflow, tensorflow2xp
 from ..types import Array
 
@@ -11,7 +11,7 @@ except ImportError:
     has_tensorflow = False
 
 
-def TensorFlowWrapper(tensorflow_model: Any) -> Model:
+def TensorflowWrapper(tensorflow_model: Any) -> Model:
     """Wrap a TensorFlow model, so that it has the same API as Thinc models.
     To optimize the model, you'll need to create a Tensorflow optimizer and call
     optimizer.apply_gradients after each batch
@@ -19,7 +19,7 @@ def TensorFlowWrapper(tensorflow_model: Any) -> Model:
     assert has_tensorflow, "Tensorflow not found!"
     assert isinstance(tensorflow_model, tf.keras.models.Model), \
         "tensorflow_model must be an instance of tf.keras.models.Model"
-    return Model("tensorflow", forward, shims=[TensorFlowShim(tensorflow_model)])
+    return Model("tensorflow", forward, shims=[TensorflowShim(tensorflow_model)])
 
 
 def forward(model: Model, X: Array, is_train: bool) -> Tuple[Array, Callable]:
