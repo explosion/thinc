e1fdc100b2879968e4b88c868c3d5b73d193639f
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-08 15:05:44 +0100

    Fix cupy/pytorch alloc

--- thinc/backends/_cupy_torch_memory_pool.py
+++ thinc/backends/_cupy_torch_memory_pool.py
@@ -1,3 +1,5 @@
+from ..util import torch2xp
+
 try:
     import torch
     has_torch = True
@@ -5,36 +7,7 @@ except ImportError:
     has_torch = False
 
 
-try:
-    import cupy
-    import cupy.cuda.memory
-    import cupy.cuda.device
-    has_cupy = True
-except ImportError:
-    has_cupy = False
-
-
-class PyTorchMemory(cupy.cuda.memory.BaseMemory):
-    def __init__(self, size_in_bytes: int):
-        self._obj = torch.FloatStorage(size_in_bytes // 4)
-        self.size = size_in_bytes
-    
-    @property
-    def ptr(self) -> int:
-        return self._obj.data_ptr()
-
-    @property
-    def device_id(self) -> int:
-        return self._obj.device
-
-    @property
-    def device(self):
-        return cupy.cuda.device.Device(self.device_id)
-
-
 def cupy_pytorch_allocator(size_in_bytes: int):
-    # The pointer object will hold a reference to the `PyTorchMemory` object,
-    # which will hold a reference to the torch.FloatStorage object. When
-    # Python is ready to free the MemoryPointer, we'll also free the FloatStorage,
-    # releasing the memory back to PyTorch.
-    return cupy.cuda.memory.MemoryPointer(PyTorchMemory(size_in_bytes), 0)
+    torch_tensor = torch.zeros((size_in_bytes // 4,))
+    cupy_tensor = torch2xp(torch_tensor)
+    return cupy_tensor.data
