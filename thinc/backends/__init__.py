67dc194bac82b5d415ab1a94e0525b5115a76d8d
Ines Montani <ines@ines.io>
2020-01-08 18:21:28 +0100

    Tidy up

--- thinc/backends/__init__.py
+++ thinc/backends/__init__.py
@@ -7,7 +7,7 @@ from .numpy_ops import NumpyOps
 from ._cupy_allocators import cupy_tensorflow_allocator, cupy_pytorch_allocator
 from ..types import DeviceTypes
 from ..util import create_thread_local
-from ..util import assert_tensorflow_is_installed, assert_pytorch_is_installed
+from ..util import assert_tensorflow_installed, assert_pytorch_installed
 
 
 STATE = create_thread_local({"Ops": NumpyOps, "ops": NumpyOps()})
@@ -15,7 +15,7 @@ STATE = create_thread_local({"Ops": NumpyOps, "ops": NumpyOps()})
 
 def use_pytorch_for_gpu_memory() -> None:
     """Route GPU memory allocation via PyTorch.
-    
+
     This is recommended for using PyTorch and cupy together, as otherwise
     OOM errors can occur when there's available memory sitting in the other
     library's pool.
@@ -24,13 +24,14 @@ def use_pytorch_for_gpu_memory() -> None:
     (or vice versa), but do not currently have an implementation for it.
     """
     import cupy.cuda
-    assert_pytorch_is_installed()
+
+    assert_pytorch_installed()
     cupy.cuda.set_allocator(cupy_pytorch_allocator)
 
 
 def use_tensorflow_for_gpu_memory() -> None:
     """Route GPU memory allocation via TensorFlow.
-    
+
     This is recommended for using TensorFlow and cupy together, as otherwise
     OOM errors can occur when there's available memory sitting in the other
     library's pool.
@@ -39,7 +40,8 @@ def use_tensorflow_for_gpu_memory() -> None:
     well (or vice versa), but do not currently have an implementation for it.
     """
     import cupy.cuda
-    assert_tensorflow_is_installed()
+
+    assert_tensorflow_installed()
     cupy.cuda.set_allocator(cupy_tensorflow_allocator)
 
 
