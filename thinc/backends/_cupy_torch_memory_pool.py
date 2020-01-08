0e62538643f1b57f0ca9039b99b105ceeacc8b2e
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-08 15:29:32 +0100

    Refactor

--- thinc/backends/_cupy_torch_memory_pool.py
+++ thinc/backends/_cupy_torch_memory_pool.py
@@ -1,13 +1,4 @@
 from ..util import torch2xp
 
-try:
-    import torch
-    has_torch = True
-except ImportError:
-    has_torch = False
 
 
-def cupy_pytorch_allocator(size_in_bytes: int):
-    torch_tensor = torch.zeros((size_in_bytes // 4,))
-    cupy_tensor = torch2xp(torch_tensor)
-    return cupy_tensor.data
