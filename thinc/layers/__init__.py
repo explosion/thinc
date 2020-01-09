6de7a3cb9029671303b34412233f691be575c900
Matthw Honnibal <honnibal+gh@gmail.com>
2020-01-10 00:18:13 +0100

    Fix imports

--- thinc/layers/__init__.py
+++ thinc/layers/__init__.py
@@ -1,93 +1,93 @@
 # Weights layers
-#from .cauchysimilarity import CauchySimilarity
-#from .dropout import Dropout
-#from .embed import Embed
-#from .extractwindow import ExtractWindow
-#from .featureextractor import FeatureExtractor
-#from .hashembed import HashEmbed
-#from .layernorm import LayerNorm
-#from .linear import Linear
-#from .maxout import Maxout
-#from .mish import Mish
-#from .multisoftmax import MultiSoftmax
-#from .parametricattention import ParametricAttention
-#from .pytorchwrapper import PyTorchWrapper
-#from .relu import ReLu
-#from .residual import Residual
-#from .softmax import Softmax
-#from .sparselinear import SparseLinear
-#from .staticvectors import StaticVectors
-#from .lstm import BiLSTM, LSTM, PyTorchBiLSTM
-#from .tensorflowwrapper import TensorFlowWrapper
-#
-## Combinators
-#from .add import add
-#from .bidirectional import bidirectional
-#from .chain import chain
-#from .clone import clone
-#from .concatenate import concatenate
-#from .foreach import foreach
-#from .noop import noop
-#from .recurrent import recurrent
-#from .uniqued import uniqued
-#from .siamese import siamese
-#
-## Pooling
-#from .maxpool import MaxPool
-#from .meanpool import MeanPool
-#from .sumpool import SumPool
-#
-## Data-type transfers
-#from .list2array import list2array
-#from .list2ragged import list2ragged
-#from .ragged2list import ragged2list
-#from .strings2arrays import strings2arrays
-#from .with_list2array import with_list2array
-#from .with_list2padded import with_list2padded
-#from .with_reshape import with_reshape
-#from .with_getitem import with_getitem
-#
-#
-#__all__ = [
-#    "CauchySimilarity",
-#    "Linear",
-#    "Dropout",
-#    "Embed",
-#    "ExtractWindow",
-#    "HashEmbed",
-#    "LayerNorm",
-#    "Maxout",
-#    "Mish",
-#    "MultiSoftmax",
-#    "ParametricAttention",
-#    "PyTorchWrapper",
-#    "ReLu",
-#    "Residual",
-#    "Softmax",
-#    "SparseLinear",
-#    "StaticVectors",
-#    "BiLSTM",
-#    "LSTM",
-#    "PyTorchBiLSTM",
-#    "TensorFlowWrapper",
-#    "add",
-#    "bidirectional",
-#    "chain",
-#    "clone",
-#    "concatenate",
-#    "foreach",
-#    "noop",
-#    "recurrent",
-#    "uniqued",
-#    "siamese",
-#    "MaxPool",
-#    "MeanPool",
-#    "SumPool",
-#    "list2array",
-#    "list2ragged",
-#    "ragged2list",
-#    "with_list2array",
-#    "with_list2padded",
-#    "with_reshape",
-#    "with_getitem",
-#]
+from .cauchysimilarity import CauchySimilarity
+from .dropout import Dropout
+from .embed import Embed
+from .extractwindow import ExtractWindow
+from .featureextractor import FeatureExtractor
+from .hashembed import HashEmbed
+from .layernorm import LayerNorm
+from .linear import Linear
+from .maxout import Maxout
+from .mish import Mish
+from .multisoftmax import MultiSoftmax
+from .parametricattention import ParametricAttention
+from .pytorchwrapper import PyTorchWrapper
+from .relu import ReLu
+from .residual import Residual
+from .softmax import Softmax
+from .sparselinear import SparseLinear
+from .staticvectors import StaticVectors
+from .lstm import BiLSTM, LSTM, PyTorchBiLSTM
+from .tensorflowwrapper import TensorFlowWrapper
+
+# Combinators
+from .add import add
+from .bidirectional import bidirectional
+from .chain import chain
+from .clone import clone
+from .concatenate import concatenate
+from .foreach import foreach
+from .noop import noop
+from .recurrent import recurrent
+from .uniqued import uniqued
+from .siamese import siamese
+
+# Pooling
+from .maxpool import MaxPool
+from .meanpool import MeanPool
+from .sumpool import SumPool
+
+# Data-type transfers
+from .list2array import list2array
+from .list2ragged import list2ragged
+from .ragged2list import ragged2list
+from .strings2arrays import strings2arrays
+from .with_list2array import with_list2array
+from .with_list2padded import with_list2padded
+from .with_reshape import with_reshape
+from .with_getitem import with_getitem
+
+
+__all__ = [
+    "CauchySimilarity",
+    "Linear",
+    "Dropout",
+    "Embed",
+    "ExtractWindow",
+    "HashEmbed",
+    "LayerNorm",
+    "Maxout",
+    "Mish",
+    "MultiSoftmax",
+    "ParametricAttention",
+    "PyTorchWrapper",
+    "ReLu",
+    "Residual",
+    "Softmax",
+    "SparseLinear",
+    "StaticVectors",
+    "BiLSTM",
+    "LSTM",
+    "PyTorchBiLSTM",
+    "TensorFlowWrapper",
+    "add",
+    "bidirectional",
+    "chain",
+    "clone",
+    "concatenate",
+    "foreach",
+    "noop",
+    "recurrent",
+    "uniqued",
+    "siamese",
+    "MaxPool",
+    "MeanPool",
+    "SumPool",
+    "list2array",
+    "list2ragged",
+    "ragged2list",
+    "with_list2array",
+    "with_list2padded",
+    "with_reshape",
+    "with_getitem",
+]
