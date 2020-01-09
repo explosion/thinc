ad0c5e5d2e0efa43b06f33a485e542f7a4d7bb36
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-09 17:11:27 +0100

    Add new ArgsKwargs type

--- thinc/types.py
+++ thinc/types.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from typing import Union, Tuple, Iterator, Sized, Container, Any, TypeVar, Generic
-from typing import Optional, List
+from typing import Optional, List, Dict, Sequence, Iterable
 import numpy
 import sys
 
@@ -585,3 +585,45 @@ class Padded:
 
     data: Array
     size_at_t: Array
+
+
+@dataclass
+class ArgsKwargs:
+    """A tuple of (args, kwargs) that can be spread into some function f:
+
+        f(*args, **kwargs)
+    """
+    args: Tuple[Any, ...]
+    kwargs: Dict[str, Any]
+
+    @classmethod
+    def from_items(cls, items: Sequence[Tuple[Union[int, str], Any]]) -> "ArgsKwargs":
+        """Create an ArgsKwargs object from a sequence of (key, value) tuples,
+        such as produced by argskwargs.items(). Each key should be either a string
+        or an integer. Items with int keys are added to the args list, and
+        items with string keys are added to the kwargs list. The args list is
+        determined by sequence order, not the value of the integer.
+        """
+        args = []
+        kwargs = {}
+        for key, value in items:
+            if isinstance(key, int):
+                args.append(value)
+            else:
+                kwargs[key] = value
+        return cls(args=tuple(args), kwargs=kwargs)
+
+    def keys(self) -> Iterable[Union[int, str]]:
+        """Yield indices from self.args, followed by keys from self.kwargs."""
+        yield from range(len(self.args))
+        yield from self.kwargs.keys()
+
+    def values(self) -> Iterable[Any]:
+        """Yield elements of from self.args, followed by values from self.kwargs."""
+        yield from self.args
+        yield from self.kwargs.values()
+
+    def items(self) -> Iterable[Tuple[Union[int, str], Any]]:
+        """Yield enumerate(self.args), followed by self.kwargs.items()"""
+        yield from enumerate(self.args)
+        yield from self.kwargs.items()
