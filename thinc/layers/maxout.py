Fixing chain ini and maxout backprop (#199)
* prevent running predict if X is None

* more comprehensive error message for inc_grad

* even more informative error

* bugfix reshape for grad of b for maxout

* move inc_grad for b to increase readability

thinc/layers/maxout.py
@@ -55,9 +55,9 @@ def forward(model: Model[InT, OutT], X: InT, is_train: bool) -> Tuple[OutT, Call
 
     def backprop(d_best: OutT) -> InT:
         dY = model.ops.backprop_maxout(d_best, which, nP)
+        model.inc_grad("b", dY.sum(axis=0))
         dY = dY.reshape((dY.shape[0], nO * nP))
         model.inc_grad("W", model.ops.gemm(dY, X, trans1=True).reshape((nO, nP, nI)))
-        model.inc_grad("b", dY.sum(axis=0))
         return model.ops.gemm(dY, W.reshape((nO * nP, nI)))
 
     return best, backprop