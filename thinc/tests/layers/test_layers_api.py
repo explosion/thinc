184913d64c0c8ecdd397bc2b544ff6df697768a7
Ines Montani <ines@ines.io>
2020-01-12 19:56:22 +0100

    Add more tests and increase test coverage

--- thinc/tests/layers/test_layers_api.py
+++ thinc/tests/layers/test_layers_api.py
@@ -1,5 +1,5 @@
 from thinc.api import registry
-from thinc.types import Ragged
+from thinc.types import Ragged, Padded
 import numpy
 import pytest
 
@@ -9,12 +9,21 @@ class FakeDoc:
         return attr_ids
 
 
-array1duint = numpy.asarray([1, 2, 3], dtype="uint64")
+class FakeSpan:
+    doc = FakeDoc()
+    start = 0
+    end = -1
+
+
 array1d = numpy.asarray([1, 2, 3], dtype="f")
 array2d = numpy.asarray([[1, 2, 3, 4], [4, 5, 3, 4]], dtype="f")
 array2dint = numpy.asarray([[1, 2, 3], [4, 5, 6]], dtype="i")
+array3d = numpy.asarray([[1, 2, 3, 4], [4, 5, 3, 4], [1, 2, 3, 4]], dtype="f")
 ragged = Ragged(array2d, numpy.asarray([1, 1], dtype="i"))
+padded_data = (array3d, array1d, [1, 2, 3], [1, 2, 3])
+padded = Padded(array3d, array1d, [1, 2, 3], [1, 2, 3])
 doc = FakeDoc()
+span = FakeSpan()
 
 
 def assert_data_match(Y, out_data):
@@ -33,36 +42,46 @@ def assert_data_match(Y, out_data):
         pytest.fail(f"wrong output of {type(Y)}: {Y}")
 
 
-@pytest.mark.parametrize(
-    "name,kwargs,in_data,out_data",
-    [
-        # fmt: off
-        ("CauchySimilarity.v0", {}, (array2d, array2d), array1d),
-        ("Dropout.v0", {}, array2d, array2d),
-        ("Embed.v0", {}, array2dint, array2d),
-        ("ExtractWindow.v0", {}, array2d, array2d),
-        ("FeatureExtractor.v0", {"columns": [1, 2]}, [doc, doc, doc], [array2d, array2d, array2d]),
-        ("HashEmbed.v0", {"nO": 1, "nV": 2}, array2d, array2d),
-        ("LayerNorm.v0", {}, array2d, array2d),
-        ("Linear.v0", {}, array2d, array2d),
-        ("BiLSTM.v0", {}, [array2d, array2d], [array2d, array2d]),
-        ("LSTM.v0", {}, [array2d, array2d], [array2d, array2d]),
-        ("Maxout.v0", {}, array2d, array2d),
-        ("MaxPool.v0", {}, ragged, array2d),
-        ("MeanPool.v0", {}, ragged, array2d),
-        ("Mish.v0", {}, array2d, array2d),
-        ("MultiSoftmax.v0", {"nOs": (1, 3)}, array2d, array2d),
-        ("ParametricAttention.v0", {}, ragged, ragged),
-        ("ReLu.v0", {}, array2d, array2d),
-        ("Softmax.v0", {}, array2d, array2d),
-        ("SparseLinear.v0", {}, (array1duint, array1d, numpy.asarray([1, 1], dtype="i")), array2d),
-        ("SumPool.v0", {}, ragged, array2d),
-        # Currently doesn't work because of shape inference in chain
-        # ("Mish.v0", {"normalize": True, "dropout": 0.2}, array2d, array2d),
-        # ("ReLu.v0", {"normalize": True, "dropout": 0.2}, array2d, array2d),
-        # fmt: on
-    ],
-)
+TEST_CASES = [
+    # Array to array
+    ("Dropout.v0", {}, array2d, array2d),
+    ("Embed.v0", {}, array2dint, array2d),
+    ("Embed.v0", {"nO": 4}, array2dint, array2d),
+    ("ExtractWindow.v0", {}, array2d, array2d),
+    ("HashEmbed.v0", {"nO": 1, "nV": 2}, array2d, array2d),
+    ("LayerNorm.v0", {}, array2d, array2d),
+    ("Linear.v0", {}, array2d, array2d),
+    ("Maxout.v0", {}, array2d, array2d),
+    ("Mish.v0", {}, array2d, array2d),
+    ("Mish.v0", {"nO": 4, "nI": 4}, array2d, array2d),
+    ("MultiSoftmax.v0", {"nOs": (1, 3)}, array2d, array2d),
+    ("ReLu.v0", {}, array2d, array2d),
+    ("Softmax.v0", {}, array2d, array2d),
+    ("Softmax.v0", {"nO": 4, "nI": 4}, array2d, array2d),
+    # Currently doesn't work because of shape inference in chain:
+    # ("Mish.v0", {"normalize": True, "dropout": 0.2}, array2d, array2d),
+    # ("ReLu.v0", {"normalize": True, "dropout": 0.2}, array2d, array2d),
+    # Currently doesn't work because it requires spaCy:
+    # ("StaticVectors.v0", array2d, array2d),
+    # Ragged to array
+    ("MaxPool.v0", {}, ragged, array2d),
+    ("MeanPool.v0", {}, ragged, array2d),
+    ("SumPool.v0", {}, ragged, array2d),
+    # List to list
+    ("BiLSTM.v0", {}, [array2d, array2d], [array2d, array2d]),
+    ("LSTM.v0", {}, [array2d, array2d], [array2d, array2d]),
+    # Other
+    # fmt: off
+    ("CauchySimilarity.v0", {}, (array2d, array2d), array1d),
+    ("FeatureExtractor.v0", {"columns": [1, 2]}, [doc, doc, doc], [array2d, array2d, array2d]),
+    ("FeatureExtractor.v0", {"columns": [1, 2]}, [span, span], [array2d, array2d]),
+    ("ParametricAttention.v0", {}, ragged, ragged),
+    ("SparseLinear.v0", {}, (numpy.asarray([1, 2, 3], dtype="uint64"), array1d, numpy.asarray([1, 1], dtype="i")), array2d),
+    # fmt: on
+]
+
+
+@pytest.mark.parametrize("name,kwargs,in_data,out_data", TEST_CASES)
 def test_layers_from_config(name, kwargs, in_data, out_data):
     cfg = {"@layers": name, **kwargs}
     filled = registry.fill_config({"config": cfg})
