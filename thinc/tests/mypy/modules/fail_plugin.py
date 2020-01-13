ce1a620b2f1058f2cab92b192bae6d7eb8d433b9
Sebastián Ramírez <tiangolo@gmail.com>
2020-01-13 20:41:23 +0100

    :art: Format mypy test

--- thinc/tests/mypy/modules/fail_plugin.py
+++ thinc/tests/mypy/modules/fail_plugin.py
@@ -4,6 +4,10 @@ bad_model = chain(ReLu(10), MaxPool(), Softmax())
 
 bad_model2 = add(ReLu(10), MaxPool(), Softmax())
 
-bad_model_only_plugin = chain(ReLu(10), ReLu(10), ReLu(10), ReLu(10), MaxPool(), Softmax(), ReLu(10))
+bad_model_only_plugin = chain(
+    ReLu(10), ReLu(10), ReLu(10), ReLu(10), MaxPool(), Softmax()
+)
 
-bad_model_only_plugin2 = add(ReLu(10), ReLu(10), ReLu(10), ReLu(10), MaxPool(), Softmax(), ReLu(10))
+bad_model_only_plugin2 = add(
+    ReLu(10), ReLu(10), ReLu(10), ReLu(10), MaxPool(), Softmax()
+)
