e33a8db02129ae215cdbe28458966b9cab6a44d5
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-13 13:09:22 +0100

    Simplify regression test

--- thinc/tests/regression/test_issue208.py
+++ thinc/tests/regression/test_issue208.py
@@ -7,6 +7,5 @@ def test_issue208():
     layer1 = Linear(nO=9, nI=3)
     layer2 = Linear(nO=12, nI=9)
     layer3 = Linear(nO=5, nI=12)
-    with Model.define_operators({">>": chain}):
-        model = layer1 >> layer2 >> layer3
+    model = chain(layer1, chain(layer2, layer3))
     assert model.get_dim("nO") == 5
