7db1373fe1ee17acc082b25ee39eca006a87ee97
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-14 01:03:25 +0100

    Update layers tests

--- thinc/tests/layers/test_layers_api.py
+++ thinc/tests/layers/test_layers_api.py
@@ -49,13 +49,9 @@ def assert_data_match(Y, out_data):
         pytest.fail(f"wrong output of {type(Y)}: {Y}")
 
 
-TEST_CASES_SAME_TYPED = [
+TEST_CASES_SUMMABLE = [
     # Array to array
     ("Dropout.v0", {}, array2d, array2d),
-    ("Embed.v0", {}, array2dint, array2d),
-    ("Embed.v0", {"nO": 4}, array2dint, array2d),
-    ("ExtractWindow.v0", {}, array2d, array2d),
-    ("HashEmbed.v0", {"nO": 1, "nV": 2}, array2d, array2d),
     ("LayerNorm.v0", {}, array2d, array2d),
     ("Linear.v0", {}, array2d, array2d),
     ("Logistic.v0", {}, array2d, array2d),
@@ -65,7 +61,6 @@ TEST_CASES_SAME_TYPED = [
     ("Mish.v0", {}, array2d, array2d),
     ("Mish.v0", {"nO": 4, "nI": 4}, array2d, array2d),
     ("Mish.v0", {"normalize": True, "dropout": 0.2}, array2d, array2d),
-    ("MultiSoftmax.v0", {"nOs": (1, 3)}, array2d, array2d),
     ("ReLu.v0", {}, array2d, array2d),
     ("ReLu.v0", {"normalize": True, "dropout": 0.2}, array2d, array2d),
     ("Softmax.v0", {}, array2d, array2d),
@@ -73,14 +68,15 @@ TEST_CASES_SAME_TYPED = [
     # fmt: off
     # List to list
     ("LSTM.v0", {"bi": False}, [array2d, array2d], [array2d, array2d]),
-    ("LSTM.v0", {"bi": True}, [array2d, array2d], [array2d, array2d]),
     pytest.param("PyTorchLSTM.v0", {"bi": False, "nO": width, "nI": width}, [array2d, array2d], [array2d, array2d], marks=pytest.mark.skipif(not has_torch, reason="needs PyTorch")),
-    pytest.param("PyTorchLSTM.v0", {"bi": True, "nO": width * 2, "nI": width}, [array2d, array2d], [array2d, array2d], marks=pytest.mark.skipif(not has_torch, reason="needs PyTorch")),
     # fmt: on
 ]
 
 TEST_CASES = [
-    *TEST_CASES_SAME_TYPED,
+    *TEST_CASES_SUMMABLE,
+    pytest.param("PyTorchLSTM.v0", {"bi": True, "nO": width * 2, "nI": width}, [array2d, array2d], [array2d, array2d], marks=pytest.mark.skipif(not has_torch, reason="needs PyTorch")),
+    ("LSTM.v0", {"bi": True}, [array2d, array2d], [array2d, array2d]),
+
     # Currently doesn't work because it requires spaCy:
     # ("StaticVectors.v0", array2d, array2d),
     # Ragged to array
@@ -89,6 +85,11 @@ TEST_CASES = [
     ("SumPool.v0", {}, ragged, array2d),
     # fmt: off
     # Other
+    ("ExtractWindow.v0", {}, array2d, array2d),
+    ("Embed.v0", {}, array2dint, array2d),
+    ("Embed.v0", {"nO": 4}, array2dint, array2d),
+    ("HashEmbed.v0", {"nO": 1, "nV": 2}, array2d, array2d),
+    ("MultiSoftmax.v0", {"nOs": (1, 3)}, array2d, array2d),
     ("CauchySimilarity.v0", {}, (array2d, array2d), array1d),
     ("FeatureExtractor.v0", {"columns": [1, 2]}, [doc, doc, doc], [array2d, array2d, array2d]),
     ("FeatureExtractor.v0", {"columns": [1, 2]}, [span, span], [array2d, array2d]),
@@ -112,12 +113,13 @@ def test_layers_from_config(name, kwargs, in_data, out_data):
     assert_data_match(dX, in_data)
 
 
-@pytest.mark.xfail
-@pytest.mark.parametrize("name,kwargs,in_data,out_data", TEST_CASES_SAME_TYPED)
+@pytest.mark.parametrize("name,kwargs,in_data,out_data", TEST_CASES_SUMMABLE)
 def test_layers_with_residual(name, kwargs, in_data, out_data):
     cfg = {"@layers": "residual.v0", "layer": {"@layers": name, **kwargs}}
     filled = registry.fill_config({"config": cfg})
     model = registry.make_from_config(filled)["config"]
+    if "LSTM" in name:
+        model = with_padded(model)
     model.initialize(in_data, out_data)
     Y, backprop = model(in_data, is_train=True)
     assert_data_match(Y, out_data)
