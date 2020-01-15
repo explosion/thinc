ce1582aa1260d901b04f1fb83e665d9cd59085ef
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-15 02:46:57 +0100

    Adjust initializer functions to be Jax-compatible

--- thinc/initializers.py
+++ thinc/initializers.py
@@ -1,32 +1,23 @@
 from typing import Callable
 
+from .backends import Ops
 from .config import registry
-from .types import Array
-from .util import get_array_module, copy_array, partial
+from .types import Array, Shape
+from .util import copy_array, partial
 
 
-def xavier_uniform_init(data: Array, *, inplace: bool = False) -> Array:
-    xp = get_array_module(data)
-    scale = xp.sqrt(6.0 / (data.shape[0] + data.shape[1]))
-    if inplace:
-        copy_array(data, xp.random.uniform(-scale, scale, data.shape))
-        return data
-    else:
-        return xp.random.uniform(-scale, scale, data.shape)
+def xavier_uniform_init(ops: Ops, shape: Shape) -> Array:
+    scale = ops.xp.sqrt(6.0 / (shape[0] + shape[1]))
+    return ops.xp.random.uniform(-scale, scale, shape).astype("float32")
 
 
 @registry.initializers("xavier_uniform_init.v0")
-def configure_xavier_uniform_init(*, inplace: bool = False) -> Callable[[Array], Array]:
+def configure_xavier_uniform_init(*, inplace: bool = False) -> Callable[[Shape], Array]:
     return partial(xavier_uniform_init, inplace=inplace)
 
 
-def zero_init(data: Array, *, inplace: bool = False) -> Array:
-    if inplace:
-        data.fill(0.0)
-        return data
-    else:
-        xp = get_array_module(data)
-        return xp.zeros_like(data)
+def zero_init(ops: Ops, shape: Shape) -> Array:
+    return ops.alloc(*shape)
 
 
 @registry.initializers("zero_init.v0")
@@ -35,15 +26,10 @@ def configure_zero_init(*, inplace: bool = False) -> Callable[[Array], Array]:
 
 
 def uniform_init(
-    data: Array, *, lo: float = -0.1, hi: float = 0.1, inplace: bool = False
+        ops: Ops, shape: Shape, *, lo: float = -0.1, hi: float = 0.1
 ) -> Array:
-    xp = get_array_module(data)
-    values = xp.random.uniform(lo, hi, data.shape)
-    if inplace:
-        copy_array(data, values)
-        return data
-    else:
-        return values.astype(data.dtype)
+    values = ops.xp.random.uniform(lo, hi, shape)
+    return values.astype(data.dtype)
 
 
 @registry.initializers("uniform_init.v0")
@@ -53,19 +39,14 @@ def configure_uniform_init(
     return partial(uniform_init, lo=lo, hi=hi, inplace=inplace)
 
 
-def normal_init(data: Array, *, fan_in: int = -1, inplace: bool = False) -> Array:
+def normal_init(ops: Ops, shape: Shape, *, fan_in: int = -1, inplace: bool = False) -> Array:
     if fan_in == -1:
-        fan_in = data.shape[1]
-    xp = get_array_module(data)
-    scale = xp.sqrt(1.0 / fan_in)
-    size = int(xp.prod(data.shape))
-    inits = xp.random.normal(scale=scale, size=size)
+        fan_in = shape[1]
+    scale = ops.xp.sqrt(1.0 / fan_in)
+    size = int(ops.xp.prod(data.shape))
+    inits = ops.xp.random.normal(scale=scale, size=size).astype("float32")
     inits = inits.reshape(data.shape)
-    if inplace:
-        copy_array(data, inits)
-        return data
-    else:
-        return inits
+    return inits
 
 
 @registry.initializers("normal_init.v0")
