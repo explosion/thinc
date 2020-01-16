58b5cdf16aa673e1e0916b015039f2c5257ff78d
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-16 14:34:02 +0100

    Move LSTM from cupy to Ops

--- thinc/backends/cupy_ops.py
+++ thinc/backends/cupy_ops.py
@@ -129,28 +129,3 @@ class CupyOps(Ops):
     def position_encode(self, N, D, period=10000, out=None):
         positions = NumpyOps().position_encode(N, D, period=period, out=out)
         return self.asarray(positions)
-
-    def backprop_lstm(
-        self,
-        d_cells: Array2d,
-        d_prev: Array2d,
-        d_gates: Array3d,
-        d_output: Array2d,
-        gates: Array3d,
-        cells: Array2d,
-        prev: Array2d,
-    ) -> None:
-        (hf, hi, ho, hc) = (0, 1, 2, 3)
-        cells_tanh = self.xp.tanh(cells)
-        # Gradient for ho and c in h = sigmoid(ho) * tanh(c)
-        d_gates[ho] = cells_tanh
-        d_gates[ho] *= d_output * self.dsigmoid(gates[ho])
-        d_prevcells = gates[ho] * d_output * self.dtanh(cells_tanh)
-        d_prevcells += d_cells  # Carry gradient from timestep
-        # Gradient for hf, hi, hc, prev[i]
-        # in c = sigmoid(hf) * prev[i] + sigmoid(hi) * tanh(hc)
-        d_gates[hf] = self.dsigmoid(gates[hf]) * d_prevcells * prev
-        d_gates[hi] = self.dsigmoid(gates[1]) * d_prevcells * gates[hc]
-        d_gates[hc] = self.dtanh(gates[hc]) * d_prevcells * gates[hi]
-        d_prev[:] = d_prevcells * gates[hf]
-        copy_array(d_cells, d_prevcells)
