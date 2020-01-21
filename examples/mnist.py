c49bc9b4c9c369a505ff6af7562969949177c451
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-21 17:18:05 +0100

    Update mnist example

--- examples/mnist.py
+++ examples/mnist.py
@@ -3,7 +3,7 @@ PyTorch version: https://github.com/pytorch/examples/blob/master/mnist/main.py
 TensorFlow version: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/tutorials/mnist/mnist.py
 """
 # pip install thinc ml_datasets typer
-from thinc.api import Model, chain, ReLu, Softmax, Adam, minibatch
+from thinc.api import Model, chain, ReLu, Softmax, Adam, minibatch, Pairs
 from thinc.api import evaluate_model_on_arrays
 import ml_datasets
 from wasabi import msg
@@ -26,16 +26,19 @@ def main(
     model.initialize(X=train_X[:5], Y=train_Y[:5])
     # Create the optimizer.
     optimizer = Adam(0.001)
-    # Train the model
-    indices = model.ops.xp.arange(train_X.shape[0], dtype="i")
     for i in range(n_iter):
-        model.ops.xp.random.shuffle(indices)
-        for idx_batch in minibatch(tqdm.tqdm(indices, leave=False)):
-            Yh, backprop = model.begin_update(train_X[idx_batch])
-            backprop(Yh - train_Y[idx_batch])
+        for X, Y in model.ops.minibatch(batch_size, train_X, train_Y, shuffle=True):
+            Yh, backprop = model.begin_update(X)
+            backprop(Yh - Y)
             model.finish_update(optimizer)
         # Evaluate and print progress
-        score = evaluate_model_on_arrays(model, dev_X, dev_Y, batch_size=batch_size)
+        correct = 0
+        total = 0
+        for X, Y in model.ops.minibatch(batch_size, dev_X, dev_Y):
+            Yh = model.predict(X)
+            corrct += (Yh.argmax(axis=0) == Y.argmax(axis=0)).sum()
+            total += Yh.shape[0]
+        score = correct / total
         msg.row((i, f"{score:.3f}"), widths=(3, 5))
 
 
