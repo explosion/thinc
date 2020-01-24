f54df0ba25429a820453361aee24e67f940f9e1b
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2020-01-24 15:39:23 +0100

    Update thinc/backends/cupy_ops.py
    Co-Authored-By: Ines Montani <ines@ines.io>

--- thinc/backends/cupy_ops.py
+++ thinc/backends/cupy_ops.py
@@ -36,7 +36,7 @@ class CupyOps(Ops):
             return data.get()
 
     def gemm(self, x, y, out=None, trans1=False, trans2=False):
-        if type(x) == numpy.ndarray or type(y) == numpy.ndarray:
+        if isinstance(x, numpy.ndarray) or isinstance(y, numpy.ndarray):
             raise ValueError("Encountered a numpy array when processing with cupy. "
                              "This is likely an error in thinc, so feel free to open "
                              "an issue on the GitHub tracker.")
