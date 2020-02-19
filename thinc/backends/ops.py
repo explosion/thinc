71e67c0e2a8814419c34ee0b4e2e213e358069c4
Matthew Honnibal <honnibal+gh@gmail.com>
2020-02-19 01:59:08 +0100

    Fix lint

--- thinc/backends/ops.py
+++ thinc/backends/ops.py
@@ -4,7 +4,7 @@ import numpy
 import itertools
 
 from ..types import Xp, Shape, DTypes, DTypesInt, DTypesFloat, List2d, ArrayXd
-from ..types import Array2d, Array3d, Floats1d, Floats2d, Floats3d, Floats4d
+from ..types import Array3d, Floats1d, Floats2d, Floats3d, Floats4d
 from ..types import FloatsXd, Ints1d, Ints2d, Ints3d, Ints4d, IntsXd, _Floats
 from ..types import DeviceTypes, Generator, Padded, Batchable, SizedGenerator
 from ..util import get_array_module, is_xp_array
