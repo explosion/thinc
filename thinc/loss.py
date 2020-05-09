629945a1208e20720052f52d8af0918ed4cc40df
Matthw Honnibal <honnibal+gh@gmail.com>
2020-05-09 14:54:46 +0200

    Improve missing value handling for CCE and Sequence CCE

--- thinc/loss.py
+++ thinc/loss.py
@@ -34,9 +34,10 @@ class Loss(Generic[GuessT, TruthT, GradT, LossT]):  # pragma: no cover
 
 
 class CategoricalCrossentropy(Loss):
-    def __init__(self, *, normalize: bool = True, names: Optional[List[str]] = None):
+    def __init__(self, *, normalize: bool = True, names: Optional[List[str]] = None, missing_value: Optional[Union[str, int]]=None):
         self.normalize = normalize
         self.names = names
+        self.missing_value = missing_value
         if self.names is not None:
             self._name_to_i = {name: i for i, name in enumerate(names)}
         else:
@@ -45,8 +46,11 @@ class CategoricalCrossentropy(Loss):
     def convert_truths(self, truths, guesses: Floats2d) -> Floats2d:
         n_classes = guesses.shape[-1]
         xp = get_array_module(guesses)
+        missing = []
+        missing_value = self.missing_value
         # Convert list of ints or list of strings
         if isinstance(truths, list):
+            truths = list(truths)
             if len(truths) and isinstance(truths[0], str):
                 if self.names is None:
                     msg = (
@@ -56,31 +60,37 @@ class CategoricalCrossentropy(Loss):
                         "e.g. CategoricalCrossentropy(names=['dog', 'cat'])"
                     )
                     raise ValueError(msg)
+                for i, value in enumerate(truths):
+                    if value == missing_value:
+                        truths[i] = self.names[0]
+                        missing.append(i)
                 truths = [self._name_to_i[name] for name in truths]
             truths = xp.asarray(truths, dtype="i")
+        else:
+            for i, value in enumerate(truths):
+                if value == missing_value:
+                    truths[i] = 0
+                    missing.append(i)
         if truths.ndim != guesses.ndim:
             # transform categorical values to one-hot encoding
             truths = to_categorical(cast(Ints1d, truths), n_classes=guesses.shape[-1])
-        return truths
+        mask = _make_mask(missing, guesses)
+        return truths, mask
 
     def __call__(
         self,
         guesses: Floats2d,
         truths: IntsOrFloatsOrStrs,
-        *,
-        missing: Optional[IntsOrFloats] = None,
     ) -> Tuple[Floats2d, float]:
-        d_truth = self.get_grad(guesses, truths, missing=missing)
+        d_truth = self.get_grad(guesses, truths)
         return (d_truth, self._get_loss_from_grad(d_truth))
 
     def get_grad(
         self,
         guesses: Floats2d,
         truths: IntsOrFloatsOrStrs,
-        *,
-        missing: Optional[IntsOrFloats] = None,
     ) -> Floats2d:
-        target = self.convert_truths(truths, guesses)
+        target, mask = self.convert_truths(truths, guesses)
         if guesses.shape != target.shape:  # pragma: no cover
             err = f"Cannot calculate CategoricalCrossentropy loss: mismatched shapes: {guesses.shape} vs {target.shape}."
             raise ValueError(err)
@@ -91,9 +101,7 @@ class CategoricalCrossentropy(Loss):
             err = f"Cannot calculate CategoricalCrossentropy loss with truth values outside the [0,1] interval."
             raise ValueError(err)
         difference = guesses - target
-        if missing is not None:
-            mask = _make_mask(missing, guesses.shape)
-            difference *= 1 - mask
+        difference *= mask
         if self.normalize:
             difference = difference / guesses.shape[0]
         return difference
@@ -102,9 +110,8 @@ class CategoricalCrossentropy(Loss):
         self,
         guesses: Floats2d,
         truths: IntsOrFloats,
-        missing: Optional[IntsOrFloats] = None,
     ) -> float:
-        d_truth = self.get_grad(guesses, truths, missing=missing)
+        d_truth = self.get_grad(guesses, truths)
         return self._get_loss_from_grad(d_truth)
 
     def _get_loss_from_grad(self, d_truth: Floats2d) -> float:
@@ -116,14 +123,15 @@ class CategoricalCrossentropy(Loss):
 
 @registry.losses("CategoricalCrossentropy.v1")
 def configure_CategoricalCrossentropy(
-    *, normalize: bool = True, names: Optional[List[str]] = None
+        *, normalize: bool = True, names: Optional[List[str]] = None, missing_value: Optional[Union[str, int]]=None
 ) -> CategoricalCrossentropy:
-    return CategoricalCrossentropy(normalize=normalize, names=names)
+    return CategoricalCrossentropy(normalize=normalize, names=names, missing_value=missing_value)
 
 
 class SequenceCategoricalCrossentropy(Loss):
-    def __init__(self, *, normalize: bool = True, names: Optional[List[str]] = None):
-        self.cc = CategoricalCrossentropy(normalize=normalize, names=names)
+    def __init__(self, *, normalize: bool = True, names: Optional[List[str]] = None, missing_value: Optional[Union[str, int]]=None):
+        self.cc = CategoricalCrossentropy(normalize=False, names=names, missing_value=missing_value)
+        self.normalize = normalize
 
     def __call__(
         self, guesses: List[Floats2d], truths: List[Union[Ints1d, Floats2d]]
@@ -136,18 +144,22 @@ class SequenceCategoricalCrossentropy(Loss):
         err = "Cannot calculate SequenceCategoricalCrossentropy loss: guesses and truths must be same length"
         if len(guesses) != len(truths):  # pragma: no cover
             raise ValueError(err)
+        n = sum(yh.shape[0] for yh in guesses)
         d_scores = []
         for yh, y in zip(guesses, truths):
-            d_scores.append(self.cc.get_grad(yh, y))
+            d_yh = self.cc.get_grad(yh, y)
+            if self.normalize:
+                d_yh /= n
+            d_scores.append(d_yh)
         return d_scores
 
     def get_loss(
         self, guesses: List[Floats2d], truths: List[Union[Ints1d, Floats2d]]
-    ) -> List[float]:
-        losses = []
-        for yh, y in zip(guesses, truths):
-            losses.append(self.cc.get_loss(yh, y))
-        return losses
+    ) -> float:
+        loss = 0.
+        for grad in self.get_grad(guesses, truths):
+            loss += self.cc._get_loss_from_grad(grad)
+        return loss
 
 
 @registry.losses("SequenceCategoricalCrossentropy.v1")
@@ -261,14 +273,11 @@ def configure_CosineDistance(
     return CosineDistance(normalize=normalize, ignore_zeros=ignore_zeros)
 
 
-def _make_mask(missing, shape: Tuple[int, int]) -> Floats2d:
-    if missing.dtype == "f":
-        return cast(Floats2d, missing)
-    else:
-        xp = get_array_module(missing)
-        mask = xp.zeros(shape, dtype="f")
-        mask[missing] = 1
-        return mask
+def _make_mask(missing, guesses) -> Floats2d:
+    xp = get_array_module(guesses)
+    mask = xp.ones(guesses.shape, dtype="f")
+    mask[missing] = 0
+    return mask
 
 
 __all__ = [
