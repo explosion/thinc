4300ee85f6ad992477fc16eb2b34dc51a7d5ea1d
justindujardin <justin@dujardinconsulting.com>
2020-05-11 13:36:04 -0700

    drop old tf wrapper optimizer test

--- thinc/tests/layers/test_tensorflow_wrapper.py
+++ thinc/tests/layers/test_tensorflow_wrapper.py
@@ -217,27 +217,6 @@ def test_tensorflow_wrapper_keras_subclass_decorator_compile_args():
 
 
 @pytest.mark.skipif(not has_tensorflow, reason="needs TensorFlow")
-def test_tensorflow_wrapper_keras_subclass_compile_optimizer():
-    import tensorflow as tf
-
-    @keras_subclass(
-        "TestModel", X=numpy.array([0.0, 0.0]), Y=numpy.array([0.5]), input_shape=(2,)
-    )
-    class TestModel(tf.keras.Model):
-        def call(self, inputs):
-            return inputs
-
-    optimizer = tf.keras.optimizers.Adam(lr=3e-18)
-    model = TensorFlowWrapper(TestModel(), optimizer=optimizer)
-    weights_model = model.shims[0]._optimizer.get_weights()
-    for w1, w2 in zip(optimizer.get_weights(), weights_model):
-        assert numpy.array_equal(w1, w2)
-    lr_key = "learning_rate"
-    assert optimizer._get_hyper(lr_key) == 3e-18
-    assert optimizer._get_hyper(lr_key) == model.shims[0]._optimizer._get_hyper(lr_key)
-
-
-@pytest.mark.skipif(not has_tensorflow, reason="needs TensorFlow")
 def test_tensorflow_wrapper_keras_subclass_decorator():
     import tensorflow as tf
 
