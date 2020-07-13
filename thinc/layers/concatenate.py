7afb55e04ab8b8795c3da9c280ae90f05dc1bcba
svlandeg <sofie.vanlandeghem@gmail.com>
2020-07-13 22:11:08 +0200

    remove asarray calls in _list_forward

--- thinc/layers/concatenate.py
+++ thinc/layers/concatenate.py
@@ -80,12 +80,12 @@ def _list_forward(
         d_output = model.ops.xp.concatenate(d_output, axis=0)
         dY = model.ops.as_contig(d_output[:, : widths[0]])
         # We want to generalize unflatten later.
-        dY = model.ops.asarray(model.ops.unflatten(dY, lengths))  # type: ignore
+        dY = model.ops.unflatten(dY, lengths)  # type: ignore
         dX = callbacks[0](dY)
         start = widths[0]
         for bwd, width in zip(callbacks[1:], widths[1:]):
             dY = model.ops.as_contig(d_output[:, start : start + width])
-            dY = model.ops.asarray(model.ops.unflatten(dY, lengths))  # type: ignore
+            dY = model.ops.unflatten(dY, lengths)  # type: ignore
             dX += bwd(dY)
             start += width
         return dX
