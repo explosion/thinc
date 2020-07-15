7e296d9eb29dbc9f0f0511c3bca8e2a571d1a38e
Matthew Honnibal <honnibal+gh@gmail.com>
2020-07-15 19:31:40 +0200

    Remove blocking

--- thinc/backends/_ray_remote_params.py
+++ thinc/backends/_ray_remote_params.py
@@ -28,10 +28,8 @@ class RayProxy:
     def get_param(self, model_id: int, name: str):
         """Get a parameter from the connection."""
         key = (model_id, name)
-        self.wait_key(key)
         version, param = self.ray.get(self.conn.get_param.remote(key))
         self._param_versions[key] = version
-        self._futures[key] = []
         return param
 
     def set_param(self, model_id: int, name: str, value):
