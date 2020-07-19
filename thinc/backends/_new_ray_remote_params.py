a19e9006f3bec004ee939cdfac15754407d6ca3b
Matthew Honnibal <honnibal+gh@gmail.com>
2020-07-19 16:50:47 +0200

    Try using gradient futures

--- thinc/backends/_new_ray_remote_params.py
+++ thinc/backends/_new_ray_remote_params.py
@@ -2,7 +2,7 @@ from typing import Dict, Tuple, Any
 from timeit import default_timer as timer
 import threading
 from dataclasses import dataclass
-from collections import Counter
+from collections import Counter, defaultdict
 import time
 import threading
 from collections import UserDict
@@ -40,19 +40,16 @@ class ManyTimer:
             self.timers[key] = Timer(key)
         return self.timers[key]
 
-def thread_function(fetch_event, next_params, ray_, conn, poll):
+def thread_function(next_params, ray_, conn, poll):
     _last_update = 0
     while True:
         time.sleep(poll)
-        # fetch_event.wait()
         updates = ray_.get(
             conn.get_updated_params.remote(_last_update)
         )
         new_time = timer()
         _last_update = new_time
         next_params.update(updates)
-        print('fetched')
-        # fetch_event.clear()
 
 class RayProxy:
     """Proxy for the 'head' worker that owns the optimizer and pushes
@@ -64,32 +61,38 @@ class RayProxy:
     _next_params: Dict
     _versions: Dict
 
-    def __init__(self, connection, *, ray=None, poll_freq=0.1, use_thread=False):
+    def __init__(self, connection, *, ray=None, use_thread=False, poll_every=0.1):
         if ray is None:
             import ray # type: ignore
         # Pass in 'ray' so that we can test with a mock object.
         self.ray = ray
         # This 'connection' object will usually be a ray remote.
         self.conn = connection
+        self._poll_every = poll_every
         self._last_update = 0
-        self._poll_freq = poll_freq
+        self._next_params = {}
         self._params = {}
         self._versions = {}
-        self._next_params = {}
+        self._grad_futures = defaultdict(list)
         self.timers = ManyTimer()
-
         self.use_thread = use_thread
         if self.use_thread:
-            print('starting thread')
-            self.fetch_event = threading.Event()
-            self.thread = threading.Thread(target=thread_function, args=(
-                self.fetch_event, self._next_params, self.ray, self.conn, self._poll_freq), daemon=True)
+            args = (
+                self._next_params,
+                self.ray,
+                self.conn,
+                self._poll_every
+            )
+            self.thread = threading.Thread(
+                target=thread_function,
+                args=args,
+                daemon=True
+            )
 
     def set_param(self, model_id: int, name: str, value: FloatsXd) -> None:
         """Set a parameter to the connection."""
         key = make_key(model_id, name)
         self._params[key] = value
-        self._next_params[key] = None
         self._versions[key] = self.ray.get(
             self.conn.set_param.remote(
                 key,
@@ -100,59 +103,69 @@ class RayProxy:
     def get_param(self, model_id: int, name: str) -> FloatsXd:
         """Get a parameter from the connection."""
         key = make_key(model_id, name)
+        if not self.use_thread and (timer() - self._last_update) >= self._poll_every:
+            self._refresh_nexts()
         self._maybe_update_param(key)
         return self._params[key]
 
     def set_grad(self, model_id: int, name: str, value: FloatsXd) -> None:
         """Set a gradient to the connection."""
         key = make_key(model_id, name)
-        self.conn.set_grad.remote(
-            key,
-            self._versions[key],
-            value
+        self._grad_futures[key].append(
+            self.conn.set_grad.remote(
+                key,
+                self._versions[key],
+                value
+            )
         )
 
     def inc_grad(self, model_id: int, name: str, value: FloatsXd) -> None:
         """Increment a gradient to the connection."""
         key = make_key(model_id, name)
-        # with self.timers(f"inc_grad:"):
-        self._begin_params_pull()
-        self.conn.inc_grad.remote(
-            key,
-            self._versions[key],
-            value
+        self._grad_futures[key].append(
+            self.conn.inc_grad.remote(
+                key,
+                self._versions[key],
+                value
+            )
         )
 
-    def _begin_params_pull(self):
-        new_time = timer()
-        if self.use_thread:
-            if not self.thread.is_alive():
-                self.thread.start()
-                print('threadstarted')
-            # self.fetch_event.set()
-        else:
-            if (new_time - self._last_update) >= self._poll_freq:
-                updates = self.ray.get(
-                    self.conn.get_updated_params.remote(self._last_update)
-                )
-                self._last_update = new_time
-                self._next_params.update(updates)
+    def _refresh_nexts(self):
+        self._await_grads()
+        now_time = timer()
+        self._next_params.update(
+            self.ray.get(
+                self.conn.get_updated_params.remote(self._last_update)
+            )
+        )
+        self._last_update = now_time
 
+    def _await_grads(self):
+        futures = []
+        for g in self._grad_futures.values():
+            futures.extend(g)
+        self.ray.get(futures)
+        self._grad_futures = defaultdict(list)
+        
     def _maybe_update_param(self, key):
-        if self._next_params.get(key) is None:
-            return False
-        else:
-            version, param = self._next_params.pop(key)
-            self._params[key] = param
-            self._versions[key] = version
-            return True
+        if key in self._next_params:
+            self._versions[key], self._params[key] = self._next_params.pop(key)
+        if key in self._grad_futures:
+            self.ray.get(self._grad_futures.pop(key))
+            maybe_param = self.ray.get(
+                self.conn.get_param_if_updated.remote(key, self._versions[key])
+            )
+            if maybe_param is not None:
+                self._versions[key], self._params[key] = maybe_param
+                return True
+        return False
 
     def _sync_params(self):
+        self._await_grads()
         self._params = {}
         self._versions = {}
         self._next_params = {}
         params = self.ray.get(self.conn.get_updated_params.remote(0))
-        self._last_update = timer()
         for key, (version, param) in params.items():
             self._params[key] = param
             self._versions[key] = version
@@ -182,6 +195,8 @@ class SharedOptimizer:
         self.optimizer = optimizer
         self._params = {}
         self._progress = Counter()
+        self._n_kept = 0
+        self._n_dropped = 0
         self._n_updates = 0
         self.write_lock = threading.Lock()
 
@@ -205,6 +220,21 @@ class SharedOptimizer:
 
     def get_param(self, key):
         return (self._params[key].version, self._params[key].value)
+
+    def get_percent_dropped(self):
+        total = self._n_dropped + self._n_kept
+        if total == 0:
+            return total
+        else:
+            return self._n_dropped / total
+
+    def get_param_if_updated(self, key, version):
+        if key not in self._params:
+            raise KeyError("wat")
+        elif self._params[key].version == version:
+            return None
+        else:
+            return (self._params[key].version, self._params[key].value)
 
     def get_updated_params(self, since: float) -> Dict:
         """Return a dict with params that have changed since a given timestamp.
@@ -248,12 +278,15 @@ class SharedOptimizer:
             if key not in self._params:
                 return None
             elif tid != self._params[key].version:
+                self._n_dropped += 1
                 return None
             elif self._params[key].grads is None:
+                self._n_kept += 1
                 self._params[key].grads = value.copy()
                 self._params[key].grad_count = 1
                 self._update_if_quorum(key)
             else:
+                self._n_kept += 1
                 self._params[key].grads += value
                 self._params[key].grad_count += 1
                 self._update_if_quorum(key)
@@ -262,7 +295,8 @@ class SharedOptimizer:
         if key not in self._params:
             return
         if self._params[key].grad_count >= self.quorum:
-
+            # The optimizer call changes internal state, so we need to worry
+            # about concurrency on it.
             params, _ = self.optimizer(
                 key,
                 self._params[key].value.copy(),
@@ -276,6 +310,5 @@ class SharedOptimizer:
                 grad_count=0,
                 timestamp=timer()
             )
-            # atomic assignment
             self._params[key] = new_param
             self._n_updates += 1
