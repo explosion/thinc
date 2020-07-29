3ef2e94dee17ea95a9a9e9e4bb2dfb1117c40a17
Matthew Honnibal <honnibal+gh@gmail.com>
2020-07-29 13:33:39 +0200

    Fix flatten when 0 arrays

--- thinc/backends/ops.py
+++ thinc/backends/ops.py
@@ -216,7 +216,8 @@ class Ops:
             return self.alloc((0,) * ndim_if_empty, dtype=dtype or "f")
         xp = get_array_module(X[0])
         shape_if_empty = X[0].shape
-        if len([x for x in X if x.size != 0]) == 0:
+        X = [x for x in X if x.size != 0]
+        if len(X) == 0:
             return self.alloc(shape_if_empty, dtype=dtype or "f")
         if int(pad) >= 1:
             padded = []
