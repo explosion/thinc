36c9171efbded6f314111cb8003ddd13341ff031
Matthew Honnibal <honnibal+gh@gmail.com>
2021-01-21 19:58:22 +1100

    Fix types

--- thinc/layers/sigmoid_activation.py
+++ thinc/layers/sigmoid_activation.py
@@ -1,11 +1,11 @@
-from typing import TypeVar, Tuple, Callable
+from typing import TypeVar, Tuple, Callable, cast
 
 from ..model import Model
 from ..config import registry
 from ..types import FloatsXd
 
 
-InT = TypeVar(bound=FloatsXd)
+InT = TypeVar("InT", bound=FloatsXd)
 
 
 @registry.layers("sigmoid_activation.v1")
@@ -17,6 +17,6 @@ def forward(model: Model[InT, InT], X: InT, is_train: bool) -> Tuple[InT, Callab
     Y = model.ops.sigmoid(X, inplace=False)
 
     def backprop(dY: InT) -> InT:
-        return dY * model.ops.dsigmoid(Y, inplace=False)
+        return dY * model.ops.dsigmoid(Y, inplace=False) # type: ignore
 
     return Y, backprop
