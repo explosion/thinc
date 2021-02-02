5191d2c6cf3904f744218580bddb1b76d6bb6dea
justindujardin <justin@dujardinconsulting.com>
2021-02-01 17:05:21 -0800

    chore: rename TestShim to avoid angering the pytest gods

--- thinc/tests/layers/test_shim.py
+++ thinc/tests/layers/test_shim.py
@@ -3,7 +3,7 @@ from thinc.shims.shim import Shim
 from ..util import make_tempdir
 
 
-class TestShim(Shim):
+class MockShim(Shim):
     def __init__(self, data: List[int]):
         super().__init__(None, config=None, optimizer=None)
         self.data = data
@@ -11,15 +11,15 @@ class TestShim(Shim):
     def to_bytes(self):
         return bytes(self.data)
 
-    def from_bytes(self, data: bytes) -> "TestShim":
-        return TestShim(data=list(data))
+    def from_bytes(self, data: bytes) -> "MockShim":
+        return MockShim(data=list(data))
 
 
 def test_shim_can_roundtrip_with_path():
 
     with make_tempdir() as path:
         shim_path = path / "cool_shim.data"
-        shim = TestShim([1, 2, 3])
+        shim = MockShim([1, 2, 3])
         shim.to_disk(shim_path)
         copy_shim = shim.from_disk(shim_path)
     assert copy_shim.to_bytes() == shim.to_bytes()
@@ -27,7 +27,7 @@ def test_shim_can_roundtrip_with_path():
 
 def test_shim_can_roundtrip_with_path_subclass(pathy_fixture):
     shim_path = pathy_fixture / "cool_shim.data"
-    shim = TestShim([1, 2, 3])
+    shim = MockShim([1, 2, 3])
     shim.to_disk(shim_path)
     copy_shim = shim.from_disk(shim_path)
     assert copy_shim.to_bytes() == shim.to_bytes()
