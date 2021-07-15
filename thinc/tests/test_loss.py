823d93bc65efacfce7a7c73a1b0b60ef35b604f7
svlandeg <sofie.vanlandeghem@gmail.com>
2021-07-15 20:08:01 +0200

    add test for loss on missing and negated string values

--- thinc/tests/test_loss.py
+++ thinc/tests/test_loss.py
@@ -117,6 +117,39 @@ def test_sequence_categorical_crossentropy(guesses, labels, names):
     assert loss == pytest.approx(1.09, eps)
 
 
+@pytest.mark.parametrize(
+    "guesses, labels, names",
+    [
+        ([guesses1], [["A", "!A", "", "!C"]], ["A", "B", "C"]),
+    ],
+)
+def test_sequence_categorical_missing_negative(guesses, labels, names):
+    d_scores = SequenceCategoricalCrossentropy(normalize=False, names=names, neg_prefix="!", missing_value="").get_grad(
+        guesses, labels
+    )
+    d_scores0 = d_scores[0]
+
+    # [0.1, 0.5, 0.6] should be A
+    assert d_scores0[0][0] == pytest.approx(-0.9, eps)
+    assert d_scores0[0][1] == pytest.approx(0.5, eps)
+    assert d_scores0[0][2] == pytest.approx(0.6, eps)
+
+    # [0.4, 0.6, 0.3] should NOT be A
+    assert d_scores0[1][0] == pytest.approx(0.4, eps)
+    assert d_scores0[1][1] == pytest.approx(0.0, eps)
+    assert d_scores0[1][2] == pytest.approx(0.0, eps)
+
+    # [1, 1, 1] has missing gold label
+    assert d_scores0[2][0] == pytest.approx(0.0, eps)
+    assert d_scores0[2][1] == pytest.approx(0.0, eps)
+    assert d_scores0[2][2] == pytest.approx(0.0, eps)
+
+    # [0.0, 0.0, 0.0] should NOT be C
+    assert d_scores0[3][0] == pytest.approx(0.0, eps)
+    assert d_scores0[3][1] == pytest.approx(0.0, eps)
+    assert d_scores0[3][2] == pytest.approx(0.0, eps)
+
+
 def test_L2():
     # L2 loss = 2²+4²=20 (or normalized: 1²+2²=5)
     vec1 = numpy.asarray([[1, 2], [8, 9]])
