8d8888100c6898b83a3b324aa0767c25a8a2e1e6
Connor Brinton <connor@brintonium.com>
2021-08-06 11:44:38 -0400

    🧪 Add regression test for Mypy plugin crash
    These changes add a regression test checking whether or not
    type-checking succeeds when applied to a file with variadic arguments
    passed to a Thinc API function.

---
+++ thinc/tests/regression/issue519/program.py
@@ -0,0 +1,20 @@
+from thinc.api import chain, concatenate, Relu, Softmax
+from thinc.model import Model
+from thinc.types import Floats2d
+
+n_hidden = 32
+dropout = 0.2
+
+model1 = chain(
+    Relu(nO=n_hidden, dropout=dropout),
+    Relu(nO=n_hidden, dropout=dropout),
+    Softmax()
+)
+
+model2 = chain(
+    Relu(nO=n_hidden, dropout=dropout),
+    Relu(nO=n_hidden, dropout=dropout),
+    Softmax()
+)
+
+model3: Model[Floats2d, Floats2d] = concatenate(*[model1, model2])
---
+++ thinc/tests/regression/issue519/test_issue519.py
@@ -0,0 +1,23 @@
+import importlib.resources
+import subprocess
+
+import pytest
+from thinc.api import chain, Linear
+
+
+@pytest.mark.slow
+def test_issue519():
+    """
+    Test ability of Thinc mypy plugin to handle variadic arguments.
+
+    This test usually takes around 45 seconds (at least on my machine), so
+    I've marked it as slow 🙂
+    """
+    # Determine the name of the parent module (which contains the test program)
+    parent_module_name = __name__[:__name__.rfind(".")]
+
+    # Load test program that calls a Thinc API with variadic arguments
+    program_text = importlib.resources.read_text(parent_module_name, "program.py")
+
+    # Ask Mypy to type-check the loaded program text
+    subprocess.run(["mypy", "--command", program_text], check=True)
