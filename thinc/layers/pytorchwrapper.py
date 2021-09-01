6cb74b0b9b4d116791dbb0f10930a31034323c07
Daniël de Kok <me@danieldk.eu>
2021-09-01 16:08:34 +0200

    Add PyTorchWrapper.v2 for wrapper with mixed-precision support

--- thinc/layers/pytorchwrapper.py
+++ thinc/layers/pytorchwrapper.py
@@ -34,6 +34,46 @@ def PyTorchWrapper(
     pytorch_model,
     convert_inputs: Optional[Callable] = None,
     convert_outputs: Optional[Callable] = None,
+) -> Model[Any, Any]:
+    """Wrap a PyTorch model, so that it has the same API as Thinc models.
+    To optimize the model, you'll need to create a PyTorch optimizer and call
+    optimizer.step() after each batch. See examples/wrap_pytorch.py
+
+    Your PyTorch model's forward method can take arbitrary args and kwargs,
+    but must return either a single tensor as output or a tuple. You may find the
+    PyTorch register_forward_hook helpful if you need to adapt the output.
+
+    The convert functions are used to map inputs and outputs to and from your
+    PyTorch model. Each function should return the converted output, and a callback
+    to use during the backward pass. So:
+
+        Xtorch, get_dX = convert_inputs(X)
+        Ytorch, torch_backprop = model.shims[0](Xtorch, is_train)
+        Y, get_dYtorch = convert_outputs(Ytorch)
+
+    To allow maximum flexibility, the PyTorchShim expects ArgsKwargs objects
+    on the way into the forward and backward passed. The ArgsKwargs objects
+    will be passed straight into the model in the forward pass, and straight
+    into `torch.autograd.backward` during the backward pass.
+    """
+    if convert_inputs is None:
+        convert_inputs = convert_pytorch_default_inputs
+    if convert_outputs is None:
+        convert_outputs = convert_pytorch_default_outputs
+    return Model(
+        "pytorch",
+        forward,
+        attrs={"convert_inputs": convert_inputs, "convert_outputs": convert_outputs},
+        shims=[PyTorchShim(pytorch_model)],
+        dims={"nI": None, "nO": None},
+    )
+
+
+@registry.layers("PyTorchWrapper.v2")
+def PyTorchWrapper_v2(
+    pytorch_model,
+    convert_inputs: Optional[Callable] = None,
+    convert_outputs: Optional[Callable] = None,
     mixed_precision: bool = False,
     grad_scaler: Optional[PyTorchGradScaler] = None,
 ) -> Model[Any, Any]:
@@ -57,6 +97,14 @@ def PyTorchWrapper(
     on the way into the forward and backward passed. The ArgsKwargs objects
     will be passed straight into the model in the forward pass, and straight
     into `torch.autograd.backward` during the backward pass.
+
+    mixed_precision:
+        Enable mixed-precision. This changes whitelisted ops to run
+        in half precision for better performance and lower memory use.
+    grad_scaler:
+        The gradient scaler to use for mixed-precision training. If this
+        argument is set to "None" and mixed precision is enabled, a gradient
+        scaler with the default configuration is used.
     """
     if convert_inputs is None:
         convert_inputs = convert_pytorch_default_inputs
