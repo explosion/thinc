3b2eb1f7d4bb392892cef34fcc2d890f2d4289f7
Daniël de Kok <me@danieldk.eu>
2021-10-19 13:24:20 +0200

    test_mxnet_wrapper_to_gpu: apply forward pass
    MXNet models require initialization using a forward pass before
    parameters can be accessed.
    
    Also guard the test with has_cupy.

--- thinc/tests/layers/test_mxnet_wrapper.py
+++ thinc/tests/layers/test_mxnet_wrapper.py
@@ -5,7 +5,7 @@ import pytest
 from thinc.api import Adam, ArgsKwargs, Model, Ops, MXNetWrapper
 from thinc.api import get_current_ops, mxnet2xp, xp2mxnet
 from thinc.types import Array2d, Array1d, IntsXd
-from thinc.util import has_mxnet, to_categorical
+from thinc.util import has_cupy, has_mxnet, to_categorical
 
 from ..util import check_input_converters, make_tempdir
 
@@ -157,11 +157,10 @@ def test_mxnet_wrapper_to_cpu(mx_model, X: Array2d):
     model.to_cpu()
 
 
-@pytest.mark.skipif(not has_mxnet, reason="needs MXNet")
+@pytest.mark.skipif(not has_mxnet or not has_cupy, reason="needs MXNet")
 def test_mxnet_wrapper_to_gpu(model: Model[Array2d, Array2d], X: Array2d):
-    # Raises while failing to import cupy
-    with pytest.raises(ImportError):
-        model.to_gpu(0)
+    model.predict(X)
+    model.to_gpu(0)
 
 
 @pytest.mark.skipif(not has_mxnet, reason="needs MXNet")
