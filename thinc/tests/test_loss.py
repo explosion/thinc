5d97ec6a68fa0482626276ff01709e0150915100
Daniël de Kok <me@danieldk.eu>
2022-02-13 20:49:22 +0100

    Add test for incorrect categorical crossentropy guesses

--- thinc/tests/test_loss.py
+++ thinc/tests/test_loss.py
@@ -65,17 +65,28 @@ def test_categorical_crossentropy(guesses, labels):
     assert loss == pytest.approx(0.239375, eps)
 
 
-def test_crossentropy_incorrect_scores():
+def test_crossentropy_incorrect_scores_targets():
     labels = numpy.asarray([2])
 
     guesses_neg = numpy.asarray([[-0.1, 0.5, 0.6]])
-    with pytest.raises(ValueError, match=r"Cannot calculate"):
+    with pytest.raises(ValueError, match=r"Cannot calculate.*guesses"):
         CategoricalCrossentropy(normalize=True).get_grad(guesses_neg, labels)
 
     guesses_larger_than_one = numpy.asarray([[1.1, 0.5, 0.6]])
-    with pytest.raises(ValueError, match=r"Cannot calculate"):
+    with pytest.raises(ValueError, match=r"Cannot calculate.*guesses"):
         CategoricalCrossentropy(normalize=True).get_grad(
             guesses_larger_than_one, labels
+        )
+
+    guesses_ok = numpy.asarray([[0.1, 0.4, 0.5]])
+    targets_neg = numpy.asarray([[-0.1, 0.5, 0.6]])
+    with pytest.raises(ValueError, match=r"Cannot calculate.*truth"):
+        CategoricalCrossentropy(normalize=True).get_grad(guesses_ok, targets_neg)
+
+    targets_larger_than_one = numpy.asarray([[2.0, 0.5, 0.6]])
+    with pytest.raises(ValueError, match=r"Cannot calculate.*truth"):
+        CategoricalCrossentropy(normalize=True).get_grad(
+            guesses_ok, targets_larger_than_one
         )
 
 
