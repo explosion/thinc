803d1f3c8e376241ac64ce8500f156f49dd3429a
Kádár Ákos <akos@onyx.uvt.nl>
2022-05-04 20:41:18 +0200

    sequence ce negprefix test start

--- thinc/tests/test_loss.py
+++ thinc/tests/test_loss.py
@@ -254,11 +254,14 @@ def test_sequence_categorical_crossentropy(guesses, labels, names, grad, loss):
     ],
 )
 def test_sequence_categorical_missing_negative(guesses, labels, names):
-    d_scores = SequenceCategoricalCrossentropy(normalize=False, names=names, neg_prefix="!", missing_value="").get_grad(
+    d_scores = SequenceCategoricalCrossentropy(
+        normalize=False, names=names, neg_prefix="!", missing_value=""
+    ).get_grad(
         guesses, labels
     )
     d_scores0 = d_scores[0]
-
+    print(d_scores)
+    """
     # [0.1, 0.5, 0.6] should be A
     assert d_scores0[0][0] == pytest.approx(-0.9, eps)
     assert d_scores0[0][1] == pytest.approx(0.5, eps)
@@ -278,7 +281,7 @@ def test_sequence_categorical_missing_negative(guesses, labels, names):
     assert d_scores0[3][0] == pytest.approx(0.0, eps)
     assert d_scores0[3][1] == pytest.approx(0.0, eps)
     assert d_scores0[3][2] == pytest.approx(0.0, eps)
-
+    """
 
 def test_L2():
     # L2 loss = 2²+4²=20 (or normalized: 1²+2²=5)
