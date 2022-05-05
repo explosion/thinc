de0856a6a20691e2238f995298f7f0de0ed76385
Daniël de Kok <me@danieldk.eu>
2022-05-05 17:26:46 +0200

    reduce_{mean,sum}: explicitly handle length == 0 for clarity

--- thinc/backends/cpu_kernels.hh
+++ thinc/backends/cpu_kernels.hh
@@ -141,8 +141,13 @@ void cpu_reduce_mean(A* means__bo, const A* X__to, const L* lengths__b,
     static_assert(std::is_integral<L>::value, "Array length should be integral");
 
     for (const L* length = lengths__b; length < lengths__b + B; ++length) {
-        if (*length < 0)
+        if (*length < 0) {
             throw std::invalid_argument(std::string("all sequence lengths must be >= 0, was: ") + std::to_string(*length));
+        }
+        else if (length == 0) {
+            means__bo += O;
+            continue;
+        }
         else if (*length > T) {
             throw std::out_of_range("lengths must sum up to the number of rows");
         }
@@ -222,8 +227,13 @@ void cpu_reduce_sum(A* sums__bo, const A* X__to, const L* lengths__b,
     static_assert(std::is_integral<L>::value, "Array length should be integral");
 
     for (const L* length = lengths__b; length < lengths__b + B; ++length) {
-        if (*length < 0)
+        if (*length < 0) {
             throw std::invalid_argument(std::string("all sequence lengths must be >= 0, was: ") + std::to_string(*length));
+        }
+        else if (length == 0) {
+            sums__bo += O;
+            continue;
+        }
         else if (*length > T) {
             throw std::out_of_range("lengths must sum up to the number of rows");
         }
