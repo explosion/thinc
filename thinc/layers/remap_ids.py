9371cea753121aa6093fde05c7a4207898599784
kadarakos <kadar.akos@gmail.com>
2022-07-13 15:35:25 +0000

    work with cupy arrays and 2d arrays

--- thinc/layers/remap_ids.py
+++ thinc/layers/remap_ids.py
@@ -1,17 +1,22 @@
-from typing import Tuple, Callable, Sequence, Dict, Any
+from typing import Tuple, Callable, Sequence
+from typing import Dict, Any, Union, Optional
 
 from ..model import Model
 from ..config import registry
-from ..types import Ints2d, DTypes
+from ..types import Ints2d, DTypes, Array2d
+from ..util import is_cupy_array, is_xp_array
 
-
-InT = Sequence[Any]
+InT = Union[Sequence[Any], Array2d]
 OutT = Ints2d
 
 
 @registry.layers("remap_ids.v1")
 def remap_ids(
-    mapping_table: Dict[Any, int] = {}, default: int = 0, dtype: DTypes = "i"
+    mapping_table: Dict[Any, int] = {},
+    default: int = 0,
+    dtype: DTypes = "i",
+    *,
+    column: Optional[int] = None
 ) -> Model[InT, OutT]:
     """Remap string or integer inputs using a mapping table, usually as a
     preprocess before embeddings. The mapping table can be passed in on input,
@@ -21,7 +26,12 @@ def remap_ids(
     return Model(
         "remap_ids",
         forward,
-        attrs={"mapping_table": mapping_table, "dtype": dtype, "default": default},
+        attrs={
+            "mapping_table": mapping_table,
+            "dtype": dtype,
+            "default": default,
+            "column": column
+        },
     )
 
 
@@ -31,11 +41,21 @@ def forward(
     table = model.attrs["mapping_table"]
     default = model.attrs["default"]
     dtype = model.attrs["dtype"]
+    column = model.attrs["column"]
+    if column is not None:
+        inputs = inputs[:, column]
+    # elements of cupy arrays are 0-dimensional arrays
+    # not the integers stored in the original mapper.
+    if is_cupy_array(inputs) and inputs.dtype.kind == 'i':
+        inputs = map(int, inputs)
     values = [table.get(x, default) for x in inputs]
     arr = model.ops.asarray2i(values, dtype=dtype)
     output = model.ops.reshape2i(arr, -1, 1)
 
     def backprop(dY: OutT) -> InT:
-        return []
+        if is_xp_array(inputs):
+            return model.ops.asarray([])
+        else:
+            return []
 
     return output, backprop
