ac5a893b76ad91eff6e15dae4286a856d221762e
kadarakos <kadar.akos@gmail.com>
2022-07-27 11:08:51 +0200

    Update thinc/layers/remap_ids.py
    Co-authored-by: Daniël de Kok <me@github.danieldk.eu>

--- thinc/layers/remap_ids.py
+++ thinc/layers/remap_ids.py
@@ -47,8 +47,7 @@ def forward(
         inputs = inputs[:, column]
     # elements of cupy arrays are 0-dimensional arrays
     # not the integers stored in the original mapper.
-    if is_cupy_array(inputs):  # type: ignore
-        inputs = to_numpy(inputs)  # type: ignore
+    inputs = to_numpy(inputs)  # type: ignore
     values = [table.get(x, default) for x in inputs]
     arr = model.ops.asarray2i(values, dtype=dtype)
     output = model.ops.reshape2i(arr, -1, 1)
