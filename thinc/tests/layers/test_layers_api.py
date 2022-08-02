ddc8ae55e233aa4f77240aa66cd0cffe655dca45
kadarakos <kadar.akos@gmail.com>
2022-08-02 12:50:07 +0000

    adjust test

--- thinc/tests/layers/test_layers_api.py
+++ thinc/tests/layers/test_layers_api.py
@@ -126,8 +126,8 @@ TEST_CASES = [
     # ("CauchySimilarity.v1", {}, (array2d, array2d), array1d),
     ("ParametricAttention.v1", {}, ragged, ragged),
     ("SparseLinear.v1", {}, (numpy.asarray([1, 2, 3], dtype="uint64"), array1d, numpy.asarray([1, 1], dtype="i")), array2d),
-    ("remap_ids.v1", {"dtype": "f"}, ["a", 1, 5.0], array2dint),
-    ("remap_ids.v1", {"dtype": "f", "column": 1}, numpy.array([[1, 2, 3], [4, 5, 6]]).T, array2dint)
+    ("remap_ids.v1", {"mapping_table": {}}, ["a", 1, 5.0], array2dint),
+    ("remap_ids.v1", {"mapping_table": {}, "column": 1}, numpy.array([[1, 2, 3], [4, 5, 6]]).T, array2dint)
     # fmt: on
 ]
 
