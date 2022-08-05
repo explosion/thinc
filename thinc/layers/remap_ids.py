ba730c3d5fc4814d54246f33ef8f23333c450f05
kadarakos <kadar.akos@gmail.com>
2022-08-05 11:00:48 +0000

    legacy

--- thinc/layers/remap_ids.py
+++ thinc/layers/remap_ids.py
@@ -17,7 +17,7 @@ OutT_legacy = Ints2d
 @registry.layers("remap_ids.v1")
 def remap_ids_legacy(
     mapping_table: Dict[Any, int] = {}, default: int = 0, dtype: DTypes = "i"
-) -> Model[InT, OutT]:
+) -> Model[InT_legacy, OutT_legacy]:
     """Remap string or integer inputs using a mapping table, usually as a
     preprocess before embeddings. The mapping table can be passed in on input,
     or updated after the layer has been created. The mapping table is stored in
@@ -31,7 +31,7 @@ def remap_ids_legacy(
 
 
 def forward_legacy(
-    model: Model[InT, OutT], inputs: InT, is_train: bool
+    model: Model[InT_legacy, OutT_legacy], inputs: InT_legacy, is_train: bool
 ) -> Tuple[OutT, Callable]:
     table = model.attrs["mapping_table"]
     default = model.attrs["default"]
@@ -40,13 +40,13 @@ def forward_legacy(
     arr = model.ops.asarray2i(values, dtype=dtype)
     output = model.ops.reshape2i(arr, -1, 1)
 
-    def backprop(dY: OutT) -> InT:
+    def backprop(dY: OutT_legacy) -> InT:
         return []
 
     return output, backprop
 
 
-@registry.layers("remap_ids.v1")
+@registry.layers("remap_ids.v2")
 def remap_ids(
     mapping_table: Optional[Union[Dict[int, int], Dict[str, int]]] = None,
     default: int = 0,
