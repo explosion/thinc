64967ebb821cfb05ee014e99032259766205948c
Madeesh Kannan <shadeMe@users.noreply.github.com>
2022-08-17 17:37:52 +0200

    Remove `thinc.extra.search` module and related tests (moved to spaCy) (#743)

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ MOD_NAMES = [
     "thinc.backends.cblas",
     "thinc.backends.linalg",
     "thinc.backends.numpy_ops",
-    "thinc.extra.search",
     "thinc.layers.sparselinear",
 ]
 COMPILE_OPTIONS = {
@@ -106,7 +105,9 @@ def setup_package():
         ext = Extension(name, [mod_path], language="c++", include_dirs=include_dirs)
         ext_modules.append(ext)
     print("Cythonizing sources")
-    ext_modules = cythonize(ext_modules, compiler_directives=COMPILER_DIRECTIVES, language_level=2)
+    ext_modules = cythonize(
+        ext_modules, compiler_directives=COMPILER_DIRECTIVES, language_level=2
+    )
 
     setup(
         name="thinc",
