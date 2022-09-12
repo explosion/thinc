968644e4ee3fa4e57b414cf4e35a899809fb7599
Will Frey <jfrey89@gmail.com>
2022-09-12 15:27:43 -0400

    ♻️ expose {floats,int}_getitem in thinc.api

--- thinc/api.py
+++ thinc/api.py
@@ -39,7 +39,8 @@ from .layers import with_array, with_array2d
 from .layers import with_padded, with_list, with_ragged, with_flatten
 from .layers import with_reshape, with_getitem, strings2arrays, list2array
 from .layers import list2ragged, ragged2list, list2padded, padded2list, remap_ids
-from .layers import array_getitem, with_cpu, with_debug, with_nvtx_range
+from .layers import array_getitem, floats_getitem, ints_getitem
+from .layers import with_cpu, with_debug, with_nvtx_range
 from .layers import with_signpost_interval
 from .layers import tuplify
 
