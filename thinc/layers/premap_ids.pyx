daf9911f085e9ad6e05523efb3434a719e1c70b3
kadarakos <kadar.akos@gmail.com>
2022-12-07 15:39:05 +0000

    layer to strictly map from ints to ints

---
+++ thinc/layers/premap_ids.pyx
@@ -0,0 +1,85 @@
+# cython: infer_types=True
+cimport cython
+import numpy as np
+from preshed.maps cimport PreshMap
+from preshed.maps import PreshMap
+from typing import Dict, Union, Optional, cast, Callable, Tuple, Mapping
+from ..types import Ints1d, Ints2d
+from ..config import registry
+from ..model import Model
+from ..util import to_numpy
+
+
+InT = Union[Ints1d, Ints2d]
+OutT = Ints2d
+
+
+cdef lookup(PreshMap mapping, long[:] keys, long default):
+    """
+    Faster dict.get(keys, default) for the case when 
+    the "dict" is a Dict[int, int] converted to PreshMap
+    and the "keys" is a numpy integer vector.
+    """
+    cdef int maxi = len(keys)
+    result = np.empty(maxi, dtype="int")
+    cdef long[:] result_view = result
+    for i in range(maxi):
+        v = mapping[keys[i]]
+        if v is None:
+            result_view[i] = default
+        else:
+            result_view[i] = v
+    return result
+
+
+@cython.binding(True)
+@registry.layers("premap_ids.v1")
+def premap_ids(
+    mapping_table: Mapping[int, int],
+    default: int = 0,
+    *,
+    column: Optional[int] = None
+):
+    """Remap integer inputs to integers a mapping table, usually as a
+    preprocess before embeddings."""
+    if isinstance(mapping_table, PreshMap):
+        mapper = mapping_table
+    else:
+        mapper = PreshMap(initial_size=len(mapping_table))
+        for k, v in mapping_table.items():
+            if not (isinstance(k, int) and isinstance(v, int)):
+                raise ValueError(
+                    "mapping_table has to be of type Mapping[int, int], "
+                    f"but found {k}, {type(k)} and {v}, {type(v)}"
+                )
+            mapper[k] = v
+    return Model(
+        "premap_ids",
+        forward,
+        attrs={
+            "mapping_table": mapper, "default": default, "column": column
+        }
+    )
+
+
+@cython.binding(True)
+def forward(
+    model: Model, inputs: InT, is_train: bool
+) -> Tuple[OutT, Callable]:
+    table = model.attrs["mapping_table"]
+    default = model.attrs["default"]
+    column = model.attrs["column"]
+    # Have to convert to numpy anyways, because
+    # cupy ints don't work together with Python ints.
+    if column is None:
+        idx = to_numpy(inputs)
+    else:
+        idx = to_numpy(cast(Ints2d, inputs)[:, column])
+    result = lookup(table, idx, default) 
+    arr = model.ops.asarray2i(result)
+    output = model.ops.reshape2i(arr, -1, 1)
+
+    def backprop(dY: OutT) -> InT:
+        return model.ops.xp.empty(dY.shape)  # type: ignore
+
+    return output, backprop
