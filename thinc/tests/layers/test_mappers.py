90e02f18cc1acad8e94f73f3a2216e0455a33788
kadarakos <kadar.akos@gmail.com>
2023-01-30 13:05:52 +0000

    add assert_equal

--- thinc/tests/layers/test_mappers.py
+++ thinc/tests/layers/test_mappers.py
@@ -53,5 +53,9 @@ def test_column(keys, mapper):
     idx[:, 3] = keys
     remap_v2 = remap_ids_v2(mapper, column=3)
     premap = premap_ids(mapper, column=3)
-    assert all(remap_v2(idx, False)[0].squeeze() == numpy.asarray(range(len(keys))))
-    assert all(premap(idx, False)[0].squeeze() == numpy.asarray(range(len(keys))))
+    numpy.testing.assert_equal(
+        remap_v2(idx, False)[0].squeeze(), numpy.asarray(range(len(keys)))
+    )
+    numpy.testing.assert_equal(
+        premap(idx, False)[0].squeeze(), numpy.asarray(range(len(keys)))
+    )
