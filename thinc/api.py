529fc82f3ddabea59106f3b65675004a09a7c1c8
kadarakos <kadar.akos@gmail.com>
2023-02-28 13:44:33 +0100

    Premap: Mapping layer from ints to ints (#815)
    * layer to strictly map from ints to ints
    
    * layer to strictly map from ints to ints
    
    * mini rough speed test
    
    * premap imports
    
    * tests for remap_ids and premap_ids
    
    * import fix
    
    * Update api.py
    
    * test with Embed
    
    * test with Embed
    
    * add hashembed test
    
    * binding=True in preamble
    
    * change all to numpy assert_equal
    
    * turn functions to fixtures
    
    * np to numpy and remove binding decorator
    
    * remove preshmap as possible input type
    
    * add assert_equal
    
    * all tests with assert_equal
    
    * add context manager for timing
    
    * use context manager for timing
    
    * Update thinc/tests/layers/test_layers_api.py
    
    Co-authored-by: Adriane Boyd <adrianeboyd@gmail.com>
    
    * remove time_context from util
    
    * black
    
    * revert changes to util
    
    * revert changes to util
    
    ---------
    
    Co-authored-by: Adriane Boyd <adrianeboyd@gmail.com>

--- thinc/api.py
+++ thinc/api.py
@@ -39,7 +39,8 @@ from .layers import map_list
 from .layers import with_array, with_array2d
 from .layers import with_padded, with_list, with_ragged, with_flatten
 from .layers import with_reshape, with_getitem, strings2arrays, list2array
-from .layers import list2ragged, ragged2list, list2padded, padded2list, remap_ids
+from .layers import list2ragged, ragged2list, list2padded, padded2list
+from .layers import remap_ids, remap_ids_v2, premap_ids
 from .layers import array_getitem, with_cpu, with_debug, with_nvtx_range
 from .layers import with_signpost_interval
 from .layers import tuplify, with_flatten_v2
@@ -101,7 +102,8 @@ __all__ = [
     "with_array", "with_array2d",
     "with_padded", "with_list", "with_ragged", "with_flatten",
     "with_reshape", "with_getitem", "strings2arrays", "list2array",
-    "list2ragged", "ragged2list", "list2padded", "padded2list", "remap_ids",
+    "list2ragged", "ragged2list", "list2padded", "padded2list", 
+    "remap_ids", "remap_ids_v2", "premap_ids",
     "array_getitem", "with_cpu", "with_debug", "with_nvtx_range",
     "with_signpost_interval",
     "tuplify", "with_flatten_v2",
